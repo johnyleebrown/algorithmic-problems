6a36679e1a23d9a966aa1aac276ad7f4df997970
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-08-30 10:50:23 +0300

    112

---
+++ java/src/Tree/PathSum.java
@@ -0,0 +1,24 @@
+package Tree;
+
+/**
+ * 112
+ * Given a binary tree and a sum, determine if the tree
+ * has a root-to-leaf path such that adding up all the
+ * values along the path equals the given sum.
+ */
+public class PathSum {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    // Space complexity: O(1)
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.right == null && root.left == null && sum - root.val == 0) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}

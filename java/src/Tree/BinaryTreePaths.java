c583594e3e4efe2b3ebdaad34c9b70da3089097a
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-04 10:50:04 +0300

    257

---
+++ java/src/Tree/BinaryTreePaths.java
@@ -0,0 +1,56 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 257
+ * Given a binary tree, return all root-to-leaf paths.
+ * ["1->2->5", "1->3"]
+ */
+public class BinaryTreePaths {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    // Solution 1
+    List<String> list = new LinkedList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) return new LinkedList<>();
+        binaryTreePathsHelper(root, "");
+        return list;
+    }
+
+    public void binaryTreePathsHelper(TreeNode root, String str) {
+        if (root.left == null && root.right == null) list.add(str + root.val);
+        if (root.left != null) binaryTreePathsHelper(root.left, str + root.val + "->") ;
+        if (root.right != null) binaryTreePathsHelper(root.right, str + root.val + "->") ;
+    }
+
+    // Solution 2
+    public List<String> binaryTreePaths2(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        if (root.left == null &&  root.right == null) {
+            List<String> temp = new ArrayList<>();
+            temp.add(root.val + "");
+            return temp;
+        }
+
+        List<String> left = binaryTreePaths(root.left);
+        for (int i = 0; i < left.size(); i++) left.set(i, root.val + "->" + left.get(i));
+
+        List<String> right = binaryTreePaths(root.right);
+        for (int i = 0; i < right.size(); i++) right.set(i, root.val + "->" + right.get(i));
+
+        if (left.isEmpty() || right.isEmpty())    return left.isEmpty() ? right : left;
+
+        left.addAll(right);
+        return left;
+    }
+}

6346640153be8f0e4848925940dafa280a499fb1
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-05 07:11:55 +0300

    501

---
+++ java/src/SQL/CombineTwoTables.sql
@@ -0,0 +1,33 @@
+--
+-- 175
+--
+-- Write a SQL query for a report that provides
+-- the following information for each person in the Person table,
+-- regardless if there is an address for each of those people:
+-- FirstName, LastName, City, State
+
+-- Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+
+-- Address
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+
+SELECT    FirstName, LastName, City, State
+FROM      Person
+LEFT JOIN Address
+ON        Person.PersonId = Address.PersonId;

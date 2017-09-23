dd2e53f8e4190c82aa496a42c6bc3ee903b4481c
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-23 05:53:36 +0300

    485

---
+++ java/src/Easy/Array/MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package Easy.Array;
+
+/**
+ * 485
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
+ */
+public class MaxConsecutiveOnes {
+    // O(n)
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int index = 0;
+        int result = 0;
+        while (index < nums.length) {
+            while (index < nums.length && nums[index] == 0) index++;
+            int start = index;
+            while (index < nums.length && nums[index] == 1) index++;
+            result = Math.max(result, index - start);
+        }
+        return result;
+    }
+}

3f20513052cf6831b6e62e56a7fe0d2e445124cd
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-25 12:47:40 +0300

    520

--- java/src/Easy/DP/ClimbingStairs.java
+++ java/src/Easy/DP/ClimbingStairs.java
@@ -20,4 +20,51 @@ public class ClimbingStairs {
         }
     }
 
+    // Fibonacci Number
+    // O(n) ; O(1)
+    class Solution2 {
+        public int climbStairs(int n) {
+            if (n == 1) return 1;
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++) {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+
+    // Brute Force
+    // O(n^2) Size of recursion tree will be 2^n
+    // O(n)
+    public class Solution3 {
+        public int climbStairs(int n) {
+            return climbRecursive(0, n);
+        }
+
+        public int climbRecursive(int i, int n) {
+            if (i > n) return 0;
+            if (i == n) return 1;
+            return climbRecursive(i + 1, n) + climbRecursive(i + 2, n);
+        }
+    }
+
+    // Recursion with memorization
+    // O(n) ; O(n)
+    class Solution4 {
+        public int climbStairs(int n) {
+            int memo[] = new int[n + 1];
+            return climbRecursive(0, n, memo);
+        }
+
+        public int climbRecursive(int i, int n, int memo[]) {
+            if (i > n) return 0;
+            if (i == n) return 1;
+            if (memo[i] > 0) return memo[i];
+            memo[i] = climbRecursive(i + 1, n, memo) + climbRecursive(i + 2, n, memo);
+            return memo[i];
+        }
+    }
 }

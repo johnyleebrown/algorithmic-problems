a54df276f132e98de1fd64405bb2d49db2164d5a
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-26 06:38:50 +0300

    53

---
+++ java/src/Easy/DP/MaximumSubarray.java
@@ -0,0 +1,46 @@
+package Easy.DP;
+
+/**
+ * 53
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ */
+public class MaximumSubarray {
+
+    // Kadane's Algorithm
+    class Solution{
+        public int maxSubArray(int[] A) {
+            int maxSoFar = A[0], maxEndingHere = A[0];
+            for (int i = 1; i < A.length; ++i) {
+                maxEndingHere = Math.max(maxEndingHere + A[i], A[i]);
+                maxSoFar = Math.max(maxSoFar, maxEndingHere);
+            }
+            return maxSoFar;
+        }
+    }
+
+    class Solution2 {
+        public int maxSubArray(int[] A) {
+            int max = Integer.MIN_VALUE, sum = 0;
+            for (int i = 0; i < A.length; i++) {
+                if (sum < 0) sum = A[i];
+                else sum += A[i];
+                if (sum > max) max = sum;
+            }
+            return max;
+        }
+    }
+
+    class Solution3 {
+        public int maxSubArray(int[] nums) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 1; i <= nums.length; i++) {
+                for (int j = 0; j < nums.length && j + i <= nums.length; j++) {
+                    int temp = 0;
+                    for (int k = j; k < j + i; k++) temp += nums[k];
+                    max = Math.max(max, temp);
+                }
+            }
+            return max;
+        }
+    }
+}

71a09ce611f5dd2edcdc34a21ce67ca893e89a53
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-26 08:18:16 +0300

    118

---
+++ java/src/Easy/Array/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package Easy.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 118
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * [
+ *     [1],
+ *    [1,1],
+ *   [1,2,1],
+ *  [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ */
+public class PascalsTriangle {
+    public class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> list = new ArrayList<>();
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int i = 0; i < numRows; i++) {
+                row.add(0, 1);
+                for (int j = 1; j < row.size() - 1; j++)
+                    row.set(j, row.get(j) + row.get(j + 1));
+                list.add(new ArrayList<>(row));
+            }
+            return list;
+        }
+    }
+}

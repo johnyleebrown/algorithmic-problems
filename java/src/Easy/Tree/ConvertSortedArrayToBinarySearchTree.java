6bf473d9c582afccdfc4ad4c815294a3a8731142
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-09-27 08:08:55 +0300

    108

---
+++ java/src/Easy/Tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,24 @@
+package Easy.Tree;
+
+import Helpers.TreeNode;
+
+/**
+ * 108
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return makeTree(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode makeTree(int[] a, int lo, int hi) {
+            if (hi < lo) return null;
+            int mid = lo + (hi - lo) / 2;
+            TreeNode node = new TreeNode(a[mid]);
+            node.left = makeTree(a, lo, mid - 1);
+            node.right = makeTree(a, mid + 1, hi);
+            return node;
+        }
+    }
+}

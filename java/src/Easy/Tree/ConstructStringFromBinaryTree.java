440bf373b1a557c786f296163360ca40d1c8a0f2
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-02 07:49:08 +0300

    606

--- java/src/Easy/Tree/ConstructStringFromBinaryTree.java
+++ java/src/Easy/Tree/ConstructStringFromBinaryTree.java
@@ -8,16 +8,17 @@ import Helpers.TreeNode;
  * The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
  */
 public class ConstructStringFromBinaryTree {
-    public class Solution {
+    // O(n), O(n)
+    public String tree2str(TreeNode t) {
+        if (t == null) return "";
+        if (t.left == null && t.right == null) return t.val + "";
+        if (t.right == null) return t.val + "(" + tree2str(t.left) + ")";
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+
+    class Solution2 {
         public String tree2str(TreeNode t) {
-            if (t == null) return "";
-            String result = t.val + "";
-            String left = tree2str(t.left);
-            String right = tree2str(t.right);
-            if (left == "" && right == "") return result;
-            if (left == "") return result + "()" + "(" + right + ")";
-            if (right == "") return result + "(" + left + ")";
-            return result + "(" + left + ")" + "(" + right + ")";
+            return t == null ? "" : t.val + (t.left != null ? "(" + tree2str(t.left) + ")" : t.right != null ? "()" : "") + (t.right != null ? "(" + tree2str(t.right) + ")" : "");
         }
     }
 }

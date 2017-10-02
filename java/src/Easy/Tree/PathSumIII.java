fd572632f4b5d7d0fb76405ab8638644a6d7dfac
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-02 09:56:55 +0300

    437

--- java/src/Easy/Tree/PathSumIII.java
+++ java/src/Easy/Tree/PathSumIII.java
@@ -1,5 +1,10 @@
 package Easy.Tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import Helpers.TreeNode;
+
 /**
  * 437
  * You are given a binary tree in which each node contains an integer value.
@@ -9,5 +14,22 @@ package Easy.Tree;
  * The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
  */
 public class PathSumIII {
+    class Solution {
+        public int pathSum(TreeNode root, int sum) {
+            if (root == null) return 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            return findPathSum(root, 0, sum, map);
+        }
 
+        private int findPathSum(TreeNode curr, int sum, int target, Map<Integer, Integer> map) {
+            if (curr == null) return 0;
+            sum += curr.val;
+            int numPathToCurr = map.getOrDefault(sum - target, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            int res = numPathToCurr + findPathSum(curr.left, sum, target, map) + findPathSum(curr.right, sum, target, map);
+            map.put(sum, map.get(sum) - 1);
+            return res;
+        }
+    }
 }

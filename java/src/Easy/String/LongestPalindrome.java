f24e7d169b32b6ef4e2b4ecd9c2e21683d30cbf6
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-14 20:57:02 +0300

    409

--- java/src/Easy/String/LongestPalindrome.java
+++ java/src/Easy/String/LongestPalindrome.java
@@ -8,21 +8,42 @@ import java.util.HashSet;
  This is case sensitive, for example "Aa" is not considered a palindrome here.
  */
 public class LongestPalindrome {
-    private class Solution{
+
+    //O(n), O(n)
+    class Solution {
         public int longestPalindrome(String s) {
-            if(s==null || s.length()==0) return 0;
-            HashSet<Character> hs = new HashSet<Character>();
+            char chars[] = s.toCharArray();
+            int lowerCount[] = new int[26];
+            int upperCount[] = new int[26];
+            int odds = 0;
+            int n = s.length();
+            for (int i = 0; i < n; i++) {
+                if (chars[i] < 'a') upperCount[chars[i] - 'A']++;
+                else lowerCount[chars[i] - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                if (lowerCount[i] % 2 == 1) odds++;
+                if (upperCount[i] % 2 == 1) odds++;
+            }
+            if (odds == 0) return n;
+            return n - odds + 1;
+        }
+    }
+
+    //O(n), O(n)
+     class Solution2 {
+        public int longestPalindrome(String s) {
+            if (s == null || s.length() == 0) return 0;
+            HashSet<Character> hs = new HashSet<>();
             int count = 0;
-            for(int i=0; i<s.length(); i++){
-                if(hs.contains(s.charAt(i))){
+            for (int i = 0; i < s.length(); i++) {
+                if (hs.contains(s.charAt(i))) {
                     hs.remove(s.charAt(i));
                     count++;
-                }else{
-                    hs.add(s.charAt(i));
-                }
+                } else hs.add(s.charAt(i));
             }
-            if(!hs.isEmpty()) return count*2+1;
-            return count*2;
+            if (!hs.isEmpty()) return count * 2 + 1;
+            return count * 2;
         }
     }
 }

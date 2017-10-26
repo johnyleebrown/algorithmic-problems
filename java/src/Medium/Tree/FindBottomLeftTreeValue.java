21694a98e939195fcd60af0f799a538f6d35c18a
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-26 09:53:41 +0300

    513

--- java/src/Medium/Tree/FindBottomLeftTreeValue.java
+++ java/src/Medium/Tree/FindBottomLeftTreeValue.java
@@ -11,8 +11,24 @@ import Helpers.TreeNode;
  */
 public class FindBottomLeftTreeValue {
 
+    public class Solution {
+        public int findBottomLeftValue(TreeNode root) {
+            return findBottomLeftValue(root, 1, new int[]{0, 0});
+        }
+
+        public int findBottomLeftValue(TreeNode root, int depth, int[] res) {
+            if (res[1] < depth) {
+                res[0] = root.val;
+                res[1] = depth;
+            }
+            if (root.left != null) findBottomLeftValue(root.left, depth + 1, res);
+            if (root.right != null) findBottomLeftValue(root.right, depth + 1, res);
+            return res[0];
+        }
+    }
+
     // O(n), O(n)
-    class Solution {
+    class Solution2 {
         public int findBottomLeftValue(TreeNode root) {
             Queue<TreeNode> queue = new LinkedList<>();
             queue.add(root);

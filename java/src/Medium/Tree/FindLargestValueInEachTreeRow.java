44547b55f653d8d06e4a6657527704aee546c2f1
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-26 12:14:35 +0300

    515

---
+++ java/src/Medium/Tree/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,52 @@
+package Medium.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import Helpers.TreeNode;
+
+/**
+ * 515
+ * You need to find the largest value in each row of a binary tree.
+ */
+public class FindLargestValueInEachTreeRow {
+
+    class Solution {
+        public List<Integer> largestValues(TreeNode root) {
+            if (root == null) return new ArrayList<>();
+            List<Integer> res = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                int max = queue.peek().val;
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = queue.poll();
+                    max = Math.max(max, curr.val);
+                    if (curr.left != null) queue.offer(curr.left);
+                    if (curr.right != null) queue.offer(curr.right);
+                }
+                res.add(max);
+            }
+            return res;
+        }
+    }
+
+    public class Solution2 {
+        public List<Integer> largestValues(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            helper(root, res, 0);
+            return res;
+        }
+
+        private void helper(TreeNode root, List<Integer> res, int d) {
+            if (root == null) return;
+            if (d == res.size()) res.add(root.val);
+            else res.set(d, Math.max(res.get(d), root.val));
+            helper(root.left, res, d + 1);
+            helper(root.right, res, d + 1);
+        }
+    }
+}

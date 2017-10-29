f4d9c34a1db193c2fa496523f6f72c999b92e7b4
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-10-29 21:02:44 +0300

    717

---
+++ java/src/Easy/Array/OnebitAnd2bitCharacters.java
@@ -0,0 +1,18 @@
+package Easy.Array;
+
+/**
+ * 717
+ * We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+ * Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+ */
+public class OnebitAnd2bitCharacters {
+    class Solution {
+        public boolean isOneBitCharacter(int[] bits) {
+            int i = 0;
+            while (i < bits.length - 1) {
+                i += bits[i] + 1;
+            }
+            return i == bits.length - 1;
+        }
+    }
+}

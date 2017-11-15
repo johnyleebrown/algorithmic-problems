347d069813d45545bacf029112d70cc58cad1c4a
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-11-15 11:56:50 +0300

    461

--- java/src/Easy/BitManipulation/NumberComplement.java
+++ java/src/Easy/BitManipulation/NumberComplement.java
@@ -2,6 +2,8 @@ package Easy.BitManipulation;
 
 /**
  * 476
+ * Given a positive integer, output its complement number.
+ * The complement strategy is to flip the bits of its binary representation.
  */
 public class NumberComplement {
     class Solution {

997f467130b17066b4855ee7b9910d97ad717c23
Grigorii Shevchenko <gregsh9.5@gmail.com>
2017-11-29 09:22:33 +0300

    342

---
+++ java/src/Easy/BitManipulation/PowerOfFour.java
@@ -0,0 +1,12 @@
+package Easy.BitManipulation;
+
+/**
+ * 342
+ */
+public class PowerOfFour {
+    class Solution {
+        public boolean isPowerOfFour(int num) {
+            return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        }
+    }
+}

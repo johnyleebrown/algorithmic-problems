b190dd4a70fc360eefc9052841cb2693a663077b
Grigorii <gregsh9.5@gmail.com>
2018-01-31 06:57:31 +0300

    UnboundedKnapsack

--- java/src/Medium/DP/Knapsack01.java
+++ java/src/Medium/DP/Knapsack01.java
@@ -14,17 +14,17 @@ public class Knapsack01 {
         /**
          * @param values array of values of items in the knapsack
          * @param weights array of weights of items in the knapsack, indexes correspond to indexes in {@code val}
-         * @param cap knapsack capacity
+         * @param capacity knapsack capacity
          * @return maximum value of items
          */
-        public int Knapsack01(int[] values, int[] weights, int cap) {
-            /**
-             * Algorithm:
-             * Building a 2d array ixj
-             * It will hold all of the best solutions to the sub problems
-             * by finding the maximum profit of each item at each weight
-             * a[i][j] = Math.max(a[i-1][j], val[i] + a[i-1][j-w[i]]
-             * The value in the bottom right of the array is the maximum profit
+        public int Knapsack01(int[] values, int[] weights, int capacity) {
+            /*
+              Algorithm:
+              Building a 2d array ixj
+              It will hold all of the best solutions to the sub problems
+              by finding the maximum profit of each item at each weight
+              a[i][j] = Math.max(a[i-1][j], val[i] + a[i-1][j-w[i]]
+              The value in the bottom right of the array is the maximum profit
              */
             int i, w;
             int k[][] = new int[values.length + 1][weights.length + 1];
@@ -36,7 +36,7 @@ public class Knapsack01 {
                     else k[i][w] = k[i - 1][w];
                 }
             }
-            return k[values.length][cap];
+            return k[values.length][capacity];
         }
     }
 }
--- java/src/Medium/DP/UnboundedKnapsack.java
+++ java/src/Medium/DP/UnboundedKnapsack.java
@@ -4,5 +4,13 @@ package Medium.DP;
  * Repetition of items allowed
  */
 public class UnboundedKnapsack {
-
+    private int Solution(int[] values, int[] weights, int capacity) {
+        int dp[] = new int[capacity + 1];
+        for (int i = 0; i <= capacity; i++) {
+            for (int j = 0; j < values.length; j++) {
+                if (weights[j] <= i) dp[i] = Math.max(dp[i], dp[i - weights[j]] + values[j]);
+            }
+        }
+        return dp[capacity];
+    }
 }

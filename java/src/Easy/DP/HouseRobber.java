a0bcdb813a001de0e1a362a2133b1dea98386c19
Grigorii <gregsh9.5@gmail.com>
2018-02-07 05:52:59 +0300

    HouseRobber

--- java/src/Easy/DP/HouseRobber.java
+++ java/src/Easy/DP/HouseRobber.java
@@ -10,25 +10,13 @@ package Easy.DP;
  * amount of money you can rob tonight without alerting the police.
  */
 public class HouseRobber {
-    class Solution {
-        public int rob(int[] nums) {
-            int currYes = 0, currNo = 0;
-            for (int x : nums) {
-                int prevNo = currNo;
-                currNo = Math.max(prevNo, currYes);
-                currYes = prevNo + x;
-            }
-            return Math.max(currNo, currYes);
-        }
-    }
-
     class Solution2 {
         public int rob(int[] nums) {
             if (nums.length == 0) return 0;
             int[] dp = new int[nums.length + 1];
             dp[0] = 0;
             dp[1] = nums[0];
-            for (int i = 2; i < nums.length + 1; i++)
+            for (int i = 2; i <= nums.length; i++)
                 dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
             return dp[nums.length];
         }
---
+++ java/src/Medium/DP/DecodeWays.java
@@ -0,0 +1,22 @@
+package Medium.DP;
+
+/**
+ *
+ */
+public class DecodeWays  {
+    /**
+     * Time complexity: O()
+     * Space complexity: O()
+     */
+    public static void solution() {
+        //65..90
+        //1..26
+        //a[n] =
+        //1 2 3 .. n-2 n-1 n
+        //a[1] = 1
+
+    }
+
+    public static void main(String[] args) {
+    }
+}

cfe1fe5cca06e47df45c1de8e6d12f6dfb0e415a
Grigorii <gregsh9.5@gmail.com>
2018-02-14 20:55:34 +0300

    GlobalAndLocalInversions

--- java/src/Medium/Array/GlobalAndLocalInversions.java
+++ java/src/Medium/Array/GlobalAndLocalInversions.java
@@ -10,4 +10,14 @@ package Medium.Array;
  */
 public class GlobalAndLocalInversions {
 
+    // O(n), O(1)
+    public boolean isIdealPermutation(int[] A) {
+        int max = -1;
+        for(int i = 0; i < A.length-2; i++) {
+            max = Math.max(max, A[i]);
+            if(max > A[i+2])
+                return false;
+        }
+        return true;
+    }
 }

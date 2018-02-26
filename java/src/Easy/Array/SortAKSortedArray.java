6348f2de860ca45533605630aa2e3e72ff1b9fde
Grigorii <gregsh9.5@gmail.com>
2018-02-26 07:06:55 +0300

    DominoAndTrominoTiling

---
+++ java/src/Easy/Array/SortAKSortedArray.java
@@ -0,0 +1,33 @@
+package Easy.Array;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Given an array of n elements, where each element
+ * is at most k away from its target position, devise
+ * an algorithm that sorts in O(n log k) time.
+ * For example, let us consider k is 2, an element at
+ * index 7 in the sorted array, can be at indexes
+ * 5, 6, 7, 8, 9 in the given array.
+ */
+public class SortAKSortedArray {
+    /**
+     * Time complexity: O()
+     * Space complexity: O()
+     */
+    public static int[] solution(int[] a, int k) {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int i = 0; i < k; i++)
+            treeMap.put(a[i], i);
+        for (int i = 0; i < a.length - k - 1; i++) {
+            Map.Entry<Integer, Integer> entry = treeMap.pollFirstEntry();
+            int val = entry.getValue();
+            int idx = entry.getKey();
+            a[i] = val;
+            treeMap.put(a[i + k + 1], i + k);
+        }
+        return a;
+    }
+}
--- java/src/Medium/DP/DominoAndTrominoTiling.java
+++ java/src/Medium/DP/DominoAndTrominoTiling.java
@@ -2,25 +2,28 @@ package Medium.DP;
 
 /**
  * 790
+ * We have two types of tiles: a 2x1 domino shape,
+ * and an "L" tromino shape. These shapes may be rotated.
  */
 public class DominoAndTrominoTiling {
     /**
-     * XX tile and  XX tile
-     *              X
-     *
-     * Time complexity: O()
-     * Space complexity: O()
+     * dp[n]=dp[n-1]+dp[n-2] + 2*(dp[n-3]+…+d[0]) = dp[n-1]+dp[n-3]+dp[n-1] = 2*dp[n-1]+dp[n-3]
+     * Time complexity: O(n)
+     * Space complexity: O(n)
      */
-    public static int solution(int N) {
+    private static final int MOD = 1000000007;
+
+    public int numTilings(int N) {
+        if (N < 0) return 0;
+        if (N == 0) return 1;
+        if (N < 3) return N;
         int[] dp = new int[N + 1];
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-            int sum = 0;
-            for (int j = 3; j <= i; j++)
-                sum += dp[i - j];
-            dp[i] += 2 * sum;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = 2 * dp[i - 1] % MOD + dp[i - 3] % MOD;
+            dp[i] %= MOD;
         }
         return dp[N];
     }

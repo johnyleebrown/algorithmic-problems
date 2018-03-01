7881f86cd08f7377b2726cd8499825a514dc4c04
Grigorii <gregsh9.5@gmail.com>
2018-03-01 22:18:49 +0300

    UniqueBinarySearchTrees

---
+++ java/src/Medium/DP/UniqueBinarySearchTrees.java
@@ -0,0 +1,15 @@
+package Medium.DP;
+
+/**
+ * 98
+ */
+public class UniqueBinarySearchTrees {
+    /**
+     * Time complexity: O()
+     * Space complexity: O()
+     */
+    public static int solution() {
+// recurrence : dp[i] = sum(dp[2]..dp[i-1])*2
+        return 0;
+    }
+}

33440271c17785b062d079f6c909d1cf2b0b735f
Grigorii <gregsh9.5@gmail.com>
2018-03-05 21:46:27 +0300

    NumberOfSubarraysWithBoundedMaximum

--- java/src/Medium/Array/NumberOfMatchingSubsequences.java
+++ java/src/Medium/Array/NumberOfMatchingSubsequences.java
@@ -1,49 +1,30 @@
 package Medium.Array;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 792
+ * Given string S and a dictionary of words words,
+ * find the number of words[i] that is a subsequence of S.
  */
 public class NumberOfMatchingSubsequences {
     /**
-     * Time complexity: O(S.length + sum(words[i].length))
+     * Time complexity: O()
      * Space complexity: O(1)
      */
     public static int solution(String S, String[] words) {
-        int ans = 0;
-        ArrayList<Node>[] heads = new ArrayList[26];
-        for (int i = 0; i < 26; ++i)
-            heads[i] = new ArrayList<Node>();
-
-        for (String word: words)
-            heads[word.charAt(0) - 'a'].add(new Node(word, 0));
-
-        for (char c: S.toCharArray()) {
-            ArrayList<Node> old_bucket = heads[c - 'a'];
-            heads[c - 'a'] = new ArrayList<Node>();
-
-            for (Node node: old_bucket) {
-                node.index++;
-                if (node.index == node.word.length()) {
-                    ans++;
-                } else {
-                    heads[node.word.charAt(node.index) - 'a'].add(node);
-                }
-            }
-            old_bucket.clear();
+        List<Integer[]>[] waiting = new List[128];
+        for (int c = 0; c <= 'z'; c++)
+            waiting[c] = new ArrayList();
+        for (int i = 0; i < words.length; i++)
+            waiting[words[i].charAt(0)].add(new Integer[]{i, 1});
+        for (char c : S.toCharArray()) {
+            List<Integer[]> advance = new ArrayList(waiting[c]);
+            waiting[c].clear();
+            for (Integer[] a : advance)
+                waiting[a[1] < words[a[0]].length() ? words[a[0]].charAt(a[1]++) : 0].add(a);
         }
-        return ans;
-    }
-
-}
-
-class Node {
-    String word;
-    int index;
-    public Node(String w, int i) {
-        word = w;
-        index = i;
+        return waiting[0].size();
     }
 }
-

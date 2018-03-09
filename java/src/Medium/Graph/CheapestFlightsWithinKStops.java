a9f55d5d308cef34c8d5f532cbf3aebf615d0ec4
Grigorii <gregsh9.5@gmail.com>
2018-03-09 21:23:04 +0300

    CheapestFlightsWithinKStops

--- java/src/Medium/Graph/CheapestFlightsWithinKStops.java
+++ java/src/Medium/Graph/CheapestFlightsWithinKStops.java
@@ -2,6 +2,7 @@ package Medium.Graph;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -96,6 +97,180 @@ public class CheapestFlightsWithinKStops {
         }
     }
 
+    /**
+     * Find Shortest Path
+     * Non-negative edge weight
+     * Relaxation - update the cost of getting to the point if found a shorter path
+     * distTo[w] <= distTo[v] + e.weight()
+     */
+    public static class Dijkstras {
+        private static int[] distTo;
+        private static MinPQ<Integer> pq;
+
+        public static int solution(int n, int[][] flights, int src, int dst, int K) {
+            EWDI graph = new EWDI(n, flights);
+            distTo = new int[n];
+            Arrays.fill(distTo, Integer.MAX_VALUE);
+            distTo[src] = 0;
+
+            pq = new MinPQ<>(n);
+            pq.insert(src, 0);
+
+            while (!pq.isEmpty()) {
+                int v = pq.delMin();
+                for (DWE edge : graph.adj(v))
+                    relax(edge);
+            }
+
+            return distTo[dst] == Integer.MAX_VALUE ? -1 : distTo[dst];
+        }
+
+        private static void relax(DWE edge) {
+            int from = edge.getV();
+            int to = edge.getW();
+            int cost = edge.getWeight();
+            if (distTo[to] > distTo[from] + cost) {
+                distTo[to] = distTo[from] + cost;
+                if (pq.contains(to)) pq.decreaseKey(to, distTo[to]);
+                else pq.insert(to, distTo[to]);
+            }
+        }
+
+        public static class EWDI {
+            private int V;
+            private int E;
+            private LinkedList<DWE>[] adj;
+
+            public EWDI(int n, int[][] graph) {
+                this.V = n;
+                adj = (LinkedList<DWE>[]) new LinkedList[n];
+                for (int v = 0; v < V; v++) adj[v] = new LinkedList<>();
+                for (int[] edge : graph) {
+                    int from = edge[0];
+                    int to = edge[1];
+                    int cost = edge[2];
+                    adj[from].add(new DWE(from, to, cost));
+                }
+            }
+
+            public int outdegree(int v) {
+                return adj[v].size();
+            }
+
+            public Iterable<DWE> adj(int v) {
+                return adj[v];
+            }
+        }
+
+        public static class DWE {
+            private final int v;
+            private int w;
+            private int weight;
+
+            public int getWeight() {
+                return weight;
+            }
+
+            public DWE(int v, int w, int weight) {
+                this.v = v;
+                this.w = w;
+                this.weight = weight;
+            }
+
+            public int getV() {
+                return v;
+            }
+
+            public int getW() {
+                return w;
+            }
+        }
+
+        public static class MinPQ<Key extends Comparable<Key>> {
+            private int maxN;        // maximum number of elements on PQ
+            private int n;           // number of elements on PQ
+            private int[] pq;        // binary heap using 1-based indexing
+            private int[] qp;        // inverse of pq - qp[pq[i]] = pq[qp[i]] = i
+            private Key[] keys;      // keys[i] = priority of i
+
+            public MinPQ(int maxN) {
+                if (maxN < 0) throw new IllegalArgumentException();
+                this.maxN = maxN;
+                n = 0;
+                keys = (Key[]) new Comparable[maxN + 1];
+                pq   = new int[maxN + 1];
+                qp   = new int[maxN + 1];
+                for (int i = 0; i <= maxN; i++) qp[i] = -1;
+            }
+
+            public int delMin() {
+                int min = pq[1];
+                exch(1, n--);
+                sink(1);
+                qp[min] = -1;        // delete
+                keys[min] = null;    // to help with garbage collection
+                pq[n+1] = -1;        // not needed
+                return min;
+            }
+
+            public boolean isEmpty() {
+                return n == 0;
+            }
+
+            public boolean contains(int i) {
+                return qp[i] != -1;
+            }
+
+            public int size() {
+                return n;
+            }
+
+            public void insert(int i, Key key) {
+                n++;
+                qp[i] = n;
+                pq[n] = i;
+                keys[i] = key;
+                swim(n);
+            }
+
+            // Decrease the key associated with index i to the specified value.
+            public void decreaseKey(int i, Key key) {
+                keys[i] = key;
+                swim(qp[i]);
+            }
+
+            private boolean greater(int i, int j) {
+                return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
+            }
+
+            private void exch(int i, int j) {
+                int swap = pq[i];
+                pq[i] = pq[j];
+                pq[j] = swap;
+                qp[pq[i]] = i;
+                qp[pq[j]] = j;
+            }
+
+            private void swim(int k) {
+                while (k > 1 && greater(k/2, k)) {
+                    exch(k, k/2);
+                    k = k/2;
+                }
+            }
+
+            private void sink(int k) {
+                while (2*k <= n) {
+                    int j = 2*k;
+                    if (j < n && greater(j, j+1)) j++;
+                    if (!greater(k, j)) break;
+                    exch(k, j);
+                    k = j;
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[][] a = new int[][] {
                 {3,4,200},
@@ -107,5 +282,6 @@ public class CheapestFlightsWithinKStops {
         };
         System.out.println(solution2(5, a, 0, 4,1));
         System.out.println(solution1(5, a, 0, 4,1));
+        System.out.println(Dijkstras.solution(5, a, 0, 4,1));
     }
 }

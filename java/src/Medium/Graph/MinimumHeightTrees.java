ec4ffcb3c090ed6e8e6803a91cfe084218a2839b
Grigorii <gregsh9.5@gmail.com>
2018-03-13 08:14:01 +0300

    MinimumHeightTrees

--- java/src/Medium/Graph/MinimumHeightTrees.java
+++ java/src/Medium/Graph/MinimumHeightTrees.java
@@ -2,10 +2,12 @@ package Medium.Graph;
 
 import static Helpers.Helper.replaceBracets;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 310
@@ -17,59 +19,57 @@ import java.util.List;
  */
 public class MinimumHeightTrees {
     /**
-     * Time complexity: O()
-     * Space complexity: O()
+     * We start from every vertex of degree 1. We let the pointers move the same speed.
+     * When two pointers meet, we keep only one of them, until the last two pointers meet
+     * or one step away we then find the roots. It is easy to see that the last two pointers
+     * are from the two ends of the longest path in the graph.
+     *
+     * Remove the leaves, update the degrees of inner vertexes. Then remove the new leaves.
+     * Doing so level by level until there are 2 or 1 nodes left.
+     *
+     * Basically, the idea is to eat up all the leaves at the same time, until one/two leaves are left
+     *
+     * Time complexity: O(n)
+     * Space complexity: O(n)
      */
     public static List<Integer> solution1(int n, int[][] edges) {
-        List<Integer> l = new LinkedList<>();
-        if (edges.length == 0){
-            if (n == 1) {
-                l.add(0);
-            } else if (n == 2){
-                l.add(0);
-                l.add(1);
+        if (n == 1) return Collections.singletonList(0);
+
+        // init graph
+        List<Set<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i)
+            adj.add(new HashSet<>());
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);// O(1)
+            adj.get(edge[1]).add(edge[0]);// O(1)
+        }
+
+        // find v with degree 1
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; ++i)
+            if (adj.get(i).size() == 1)
+                leaves.add(i);
+
+        // until 2 leaves are left on a tree
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves) {
+                // going to the next leave
+                int j = adj.get(i).iterator().next();
+                // removing connection to prev leave
+                adj.get(j).remove(i);
+                if (adj.get(j).size() == 1)
+                    // add if the node doesn't have any other edges
+                    newLeaves.add(j);
             }
-            return l;
+            leaves = newLeaves;
         }
-        if (n == 0) return null;
-        if (n == 1) {
-            l.add(0);
-            return l;
-        }
-        if (n == 2) {
-            l.add(edges[0][0]);
-            l.add(edges[0][1]);
-            return l;
-        }
-
-        int[] edgesCount = new int[n];
-        int[] positions = new int[n];
-
-        for (int i = 0; i < edges.length; i++) {
-            positions[edges[i][0]] = positions[edges[i][1]] = i;
-            edgesCount[edges[i][0]]++;
-            edgesCount[edges[i][1]]++;
-        }
-
-        for (int i = 0; i < edgesCount.length; i++) {
-            if (edgesCount[i] == 1 && positions[getSibling(positions[i], i, edges)] != -1) {
-                int sib = getSibling(positions[i], i, edges);
-                l.add(sib);
-                positions[sib] = -1;
-            }
-        }
-
-        return l;
-    }
-
-    private static int getSibling(int i, int a, int[][] edges) {
-        if (edges[i][0] == a)   return edges[i][1];
-        else                    return edges[i][0];
+        return leaves;
     }
 
     public static void main(String[] args) {
         String s = "[[0,1],[0,2],[0,3],[3,4],[4,5]]";
-        //System.out.println(Arrays.toString(solution1(6, replaceBracets(s)).toArray()));
-        System.out.println(-2147483646 - Integer.MAX_VALUE);
+        System.out.println(Arrays.toString(solution1(6, replaceBracets(s)).toArray()));
     }
 }

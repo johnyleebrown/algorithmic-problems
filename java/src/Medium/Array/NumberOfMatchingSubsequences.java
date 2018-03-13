9626f315d3a611522d8bb516dfd39c34a892f28b
Grigorii <gregsh9.5@gmail.com>
2018-03-13 10:06:54 +0300

    NumberOfMatchingSubsequences

--- java/src/Medium/Array/NumberOfMatchingSubsequences.java
+++ java/src/Medium/Array/NumberOfMatchingSubsequences.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 /**
  * 792
+ *
  * Given string S and a dictionary of words words,
  * find the number of words[i] that is a subsequence of S.
  */
 public class NumberOfMatchingSubsequences {
     /**
+     * Go through S once, and while doing that, move through all words accordingly.
+     * Track of how much of each word I’ve already seen, and with each letter of S,
+     * advance the words waiting for that letter. To quickly find the words waiting
+     * for a certain letter, store each word (and its progress) in a list of words
+     * waiting for that letter. Then for each of the lucky words whose current letter
+     * just occurred in S, I update their progress and store them in the list for their
+     * next letter.
+     * by {https://leetcode.com/stefanpochmann}
+     *
      * Time complexity: O(n)
      * Space complexity: O(1)
      */

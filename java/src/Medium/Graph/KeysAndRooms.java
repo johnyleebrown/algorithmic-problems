9c2ff5a70207f1b81c64e8357b5212b04ab69955
Grigorii <gregsh9.5@gmail.com>
2018-05-23 00:15:21 +0300

    841

---
+++ java/src/Medium/Graph/KeysAndRooms.java
@@ -0,0 +1,14 @@
+package Medium.Graph;
+
+/**
+ *
+ */
+public class KeysAndRooms {
+    /**
+     * Time complexity: O()
+     * Space complexity: O()
+     */
+    public static int solution() {
+
+    }
+}

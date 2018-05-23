231e6506a42925393bf2b589d5d813cc83abda57
Grigorii <gregsh9.5@gmail.com>
2018-05-24 00:15:21 +0300

    841
    MaximizeDistanceToClosestPerson
    
    MaximizeDistanceToClosestPerson

--- java/src/Medium/Graph/KeysAndRooms.java
+++ java/src/Medium/Graph/KeysAndRooms.java
@@ -1,14 +1,34 @@
 package Medium.Graph;
 
+import java.util.List;
+
 /**
+ * 841
  *
+ * There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
+ * Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+ * Initially, all the rooms start locked (except for room 0).
+ * You can walk back and forth between rooms freely.
+ * Return true if and only if you can enter every room.
  */
 public class KeysAndRooms {
     /**
-     * Time complexity: O()
-     * Space complexity: O()
+     * Connected Components
+     *
+     * Time complexity: O(n)
+     * Space complexity: O(n)
      */
-    public static int solution() {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] marked = new boolean[rooms.size()];
+        return helper(1, rooms.get(0), rooms, 0, marked) == rooms.size();
+    }
 
+    int helper(int count, List<Integer> room, List<List<Integer>> rooms, int roomN, boolean[] marked) {
+        marked[roomN] = true;
+
+        for (int key : room)
+            if (!marked[key]) count = helper(++count, rooms.get(key), rooms, key, marked);
+
+        return count;
     }
 }
---
+++ java/src/Medium/String/ShiftingLetters.java
@@ -0,0 +1,38 @@
+package Medium.String;
+
+/**
+ * 848
+ *
+ * We have a string S of lowercase letters, and an integer array shifts.
+ *
+ * Call the shift of a letter, the next letter in the alphabet, (wrapping around so that 'z' becomes 'a').
+ *
+ * For example, shift('a') = 'b', shift('t') = 'u', and shift('z') = 'a'.
+ *
+ * Now for each shifts[i] = x, we want to shift the first i+1 letters of S, x times.
+ *
+ * Return the final string after all such shifts to S are applied.
+ */
+public class ShiftingLetters {
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    class Solution {
+        public String shiftingLetters(String S, int[] shifts) {
+            StringBuilder sb = new StringBuilder();
+            int[] cumShift = new int[shifts.length];
+            cumShift[shifts.length - 1] = shifts[shifts.length - 1];
+
+            for (int i = shifts.length - 2; i >= 0; i--)
+                cumShift[i] = (cumShift[i + 1] + shifts[i]) % 26;
+
+            for (int i = 0 ; i < shifts.length; i++) {
+                int x = S.charAt(i) + cumShift[i];
+                sb.append((char) (((x - 'a') % 26) + 'a'));
+            }
+
+            return sb.toString();
+        }
+    }
+}

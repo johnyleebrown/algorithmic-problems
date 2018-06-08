9b4bd910fc5ef9fdd323e7e68136d01dd0b44648
Grigorii <gregsh9.5@gmail.com>
2018-06-08 09:31:20 +0300

    851

---
+++ java/src/Medium/DFS/LoudAndRich.java
@@ -0,0 +1,60 @@
+package Medium.DFS;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * 851
+ *
+ * Topics: DFS, GRAPH
+ *
+ * In a group of N people (labelled 0, 1, 2, ..., N-1), each person has different amounts of money, and different levels of quietness.
+ * For convenience, we'll call the person with label x, simply "person x".
+ * We'll say that richer[i] = [x, y] if person x definitely has more money than person y.  Note that richer may only be a subset of valid observations.
+ * Also, we'll say quiet[x] = q if person x has quietness q.
+ * Now, return answer, where answer[x] = y if y is the least quiet person (that is, the person y with the smallest value of quiet[y]), among all people who definitely have equal to or more money than person x.
+ */
+public class LoudAndRich {
+    /**
+     * TLE
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    class Solution {
+        public int[] loudAndRich(int[][] richer, int[] quiet) {
+            HashMap<Integer, LinkedList<Integer>> map = new HashMap<>();
+            for (int[] r : richer) {
+                map.putIfAbsent(r[1], new LinkedList<>());
+                map.get(r[1]).add(r[0]);
+            }
+
+            int[] answer = new int[quiet.length];
+            for (int i = 0 ; i < quiet.length; i++) {
+                int[] x = helper(map, quiet, i, quiet[i]);
+                answer[i] = x[0];
+            }
+
+            return answer;
+        }
+
+        int[] helper(HashMap<Integer, LinkedList<Integer>> map, int[] quiet, int i, int minQuite) {
+            int[] m = new int[]{i, minQuite};
+
+            for (int k : map.getOrDefault(i, new LinkedList<>())) {
+                if (m[1] > quiet[k]) {
+                    m[0] = k;
+                    m[1] = quiet[k];
+                }
+
+                if (map.containsKey(k)) {
+                    int[] x = helper(map, quiet, k, m[1]);
+                    if (m[1] > x[1]) {
+                        m = x;
+                    }
+                }
+            }
+
+            return m;
+        }
+    }
+}

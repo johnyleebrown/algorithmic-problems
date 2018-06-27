5f222305cf5197806e9ec0d7e3df26288f090b0f
Grigorii <gregsh9.5@gmail.com>
2018-06-27 18:28:23 +0300

    82

--- java/src/Medium/LinkedList/RemoveDuplicatesFromSortedListII.java
+++ java/src/Medium/LinkedList/RemoveDuplicatesFromSortedListII.java
@@ -1,14 +1,26 @@
 package Medium.LinkedList;
 
+import Helpers.ListNode;
+
 /**
+ * 82
  *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
  */
 public class RemoveDuplicatesFromSortedListII {
     /**
-     * Time complexity: O()
-     * Space complexity: O()
+     * Time complexity: O(n)
+     * Space complexity: O(1)
      */
-    public static int solution() {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        if (head.next != null && head.val == head.next.val) return helper(head.val, head);
+        else return helper(null, head);
+    }
 
+    ListNode helper(Integer prev, ListNode head) {
+        boolean flag = head.next != null && head.val == head.next.val;
+        if (head.next != null) head.next = helper(head.val, head.next);
+        return prev != null && head.val == prev || flag ? head.next : head;
     }
 }

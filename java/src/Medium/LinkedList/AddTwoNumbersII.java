8520e38a4ae29d6c45600ec35f057576db9a6b8b
Grigorii <gregsh9.5@gmail.com>
2018-07-06 12:47:54 +0300

    445

--- java/src/Medium/LinkedList/AddTwoNumbersII.java
+++ java/src/Medium/LinkedList/AddTwoNumbersII.java
@@ -13,17 +13,12 @@ import Helpers.ListNode;
 public class AddTwoNumbersII {
     /**
      * Solution 1: modifying
-     * Time complexity: O()
-     * Space complexity: O()
+     * Time complexity: O(n)
+     * Space complexity: O(n)
      */
     class Solution1 {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            // reverse l1, l2
-            ListNode head1 = reverse(l1);
-            ListNode head2 = reverse(l2);
-
-            // reverse sum
-            return reverse(addLists(head1, head2));
+            return reverse(addLists(reverse(l1), reverse(l2)));
         }
 
         ListNode reverse(ListNode l) {
@@ -61,10 +56,56 @@ public class AddTwoNumbersII {
 
     /**
      * Solution 2: w/o modifying
+     * Changed : creation of new ListNode
+     * Beats 99,89%
      * Time complexity: O()
      * Space complexity: O()
      */
-   /* public static int solution2() {
+    class Solution2 {
+        public  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            // reverse l1, l2
+            ListNode head1 = reverse(l1);
+            ListNode head2 = reverse(l2);
 
-    }*/
+            // reverse sum
+            return reverse(addLists(head1, head2));
+        }
+
+        ListNode reverse(ListNode l){
+            ListNode prev = null;
+            ListNode cur = new ListNode(l.val);
+            ListNode pointer = l;
+            ListNode next = null;
+            while (cur != null) {
+                if (pointer.next != null) {
+                    cur.next = new ListNode(pointer.next.val);
+                    pointer = pointer.next;
+                }
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
+            return prev;
+        }
+
+        ListNode addLists(ListNode l1, ListNode l2){
+            ListNode result = new ListNode(0);
+            ListNode head = result;
+            int carry = 0;
+            ListNode i = l1, j = l2;
+            while (i != null || j != null) {
+                int i1 = (i != null) ? i.val : 0;
+                int j1 = (j != null) ? j.val : 0;
+                int res = carry + i1 + j1;
+                result.next = new ListNode(res % 10);
+                result = result.next;
+                carry = res/10;
+                if (i != null) i = i.next;
+                if (j != null) j = j.next;
+            }
+            if (carry > 0) result.next = new ListNode(carry);
+            return head.next;
+        }
+    }
 }

c3db78612435ee54cebd9336de5ad86ec055cdd0
Grigorii <gregsh9.5@gmail.com>
2019-07-29 07:07:20 +0300

    144

--- medium/tree/BinaryTreePreorderTraversal.java
+++ medium/tree/BinaryTreePreorderTraversal.java
@@ -10,38 +10,56 @@ import util.TreeNode;
  * 144
  * Given a binary tree, return the preorder traversal of its nodes' values.
  */
-public class BinaryTreePreorderTraversal {
+public class BinaryTreePreorderTraversal 
+{
+	// recursive
+	class Solution {
+		private List<Integer> list = new ArrayList<>();
 
-    class Solution {
-        public List<Integer> preorderTraversal(TreeNode root) {
-            List<Integer> list = new LinkedList<>();
-            Stack<TreeNode> st = new Stack<>();
-            TreeNode x = root;
-            while (x != null || !st.isEmpty()) {
-                while (x != null) {
-                    st.push(x.right);
-                    list.add(x.val);
-                    x = x.left;
-                }
-                x = st.pop();
-            }
-            return list;
-        }
-    }
+		public List<Integer> preorderTraversal(TreeNode root) {
+			if (root == null) return new ArrayList<>();
+			helper(root);
+			return list;
+		}
+		
+		private void helper(TreeNode root)
+		{
+			if (root == null) return;
+			list.add(root.val);
+			helper(root.left);
+			helper(root.right);
+		}
+	}
 
-    class Solution2 {
-        public List<Integer> preorderTraversal(TreeNode root) {
-            List<Integer> list = new LinkedList<>();
-            if (root == null) return list;
-            Stack<TreeNode> st = new Stack<>();
-            st.push(root);
-            while (!st.isEmpty()) {
-                TreeNode x = st.pop();
-                list.add(x.val);
-                if (x.right != null) st.push(x.right);
-                if (x.left != null) st.push(x.left);
-            }
-            return list;
-        }
-    }
+	// iterative
+	// using stack to store right nodes
+	class Solution2 
+	{    
+		public List<Integer> preorderTraversal(TreeNode root) 
+		{
+			if (root == null) return new ArrayList<>();
+			List<Integer> list = new ArrayList<>();
+			Stack<TreeNode> st = new Stack<>();
+			
+			while (root != null || !st.isEmpty())
+			{
+				if (root != null)
+				{
+					list.add(root.val);
+					if (root.right != null) st.push(root.right);
+					root = root.left;    
+				}
+				else if (!st.isEmpty())
+				{ 
+					TreeNode r = st.pop();
+					list.add(r.val);
+					root = r.left;
+					if (r.right != null) st.push(r.right);
+				}
+			}
+			
+			return list;
+		}
+	}
 }
+

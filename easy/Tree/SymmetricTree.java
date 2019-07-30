d1757f6a94812dd2c60956223ce7e7322ac43d25
Grigorii <gregsh9.5@gmail.com>
2019-07-30 07:47:21 +0300

    101

--- easy/Tree/SymmetricTree.java
+++ easy/Tree/SymmetricTree.java
@@ -16,15 +16,15 @@ public class SymmetricTree {
     // Space complexity: O(n)
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-        return isSymmetric2(root.left, root.right);
+        return helper(root.left, root.right);
     }
 
-    public boolean isSymmetric2(TreeNode left, TreeNode right) {
-        if (left == null && right == null) return true;
-        return left != null && right != null
-                && left.val == right.val
-                && isSymmetric2(left.left, right.right)
-                && isSymmetric2(left.right, right.left);
+    public boolean helper(TreeNode left, TreeNode right) {
+        return (left == null && right == null)
+			|| (left != null && right != null
+            	&& left.val == right.val
+            	&& helper(left.left, right.right)
+                && helper(left.right, right.left));
     }
 
     // Non-Recursive

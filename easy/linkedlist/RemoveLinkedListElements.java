3036182c71d5f92ade399518d784cc70a21b2cfb
Grigorii <gregsh9.5@gmail.com>
2019-08-10 20:13:41 +0300

    203

---
+++ easy/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,26 @@
+// 203
+public class RemoveLinkedListElements
+{
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode newhead = null;
+        ListNode prev = head;
+        while (head != null)
+        {
+            // System.out.println(head.val);
+            while (head != null && head.val == val)
+            {
+                head = head.next;
+            }
+            if (newhead == null) newhead = head;
+            if (prev.val != val && prev != head) prev.next = head;
+            prev = head;
+            if (head != null) head = head.next;
+        }
+        return newhead;
+    }
+}
+/*
+[1,2,6,3,4,5,6]
+6
+*/
+
--- medium/linkedlist/RemoveNthNodeFromEndOfList.java
+++ medium/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -10,84 +10,18 @@ import Helpers.ListNode;
  * Could you do this in one pass?
  */
 public class RemoveNthNodeFromEndOfList {
-    /**
-     * 75%
-     * Time complexity: O(n)
-     * Space complexity: O(1)
-     */
-    class Solution1 {
-        public ListNode removeNthFromEnd(ListNode head, int n) {
-            if (head == null) return null;
-            if (n == 0) return head;
-
-            ListNode p = head;
-            int size = 0;
-
-            // get the size of a list
-            while (p != null && p.next != null) {
-                p = p.next.next;
-                size += 2;
-            }
-            if (p != null) size++;
-
-            if (n == size) return head.next;
-
-            p = head;
-            while (size != n + 1) {
-                p = p.next;
-                size--;
-            }
-            p.next = p.next.next;
-
-            return head;
-        }
-    }
-
-    /**
-     * One pass
-     * Idea is super simple: keep the distance btw
-     * 2 pointers n and get the faster one to the end
-     *
-     * Time complexity: O(n)
-     * Space complexity: O(1)
-     */
-    class Solution2 {
-        public ListNode removeNthFromEnd(ListNode head, int n) {
-            ListNode headNode = new ListNode(0);
-            headNode.next = head;
-            ListNode fast = headNode;
-            ListNode slow = headNode;
-
-            while (fast.next != null) {
-                if (n <= 0) slow = slow.next;
-                fast = fast.next;
-                n--;
-            }
-            if (slow.next != null) slow.next = slow.next.next;
-
-            return headNode.next;
-        }
-    }
+	class Solution0 {
+		public ListNode removeNthFromEnd(ListNode head, int n) {
+			if (head == null) return null;
+			ListNode first = head, second = head;
+			while (first != null)
+			{
+				if (n-- < 0) second = second.next;
+				first = first.next;
+			}
+			if (n == 0) return head.next;
+			second.next = second.next.next;
+			return head;
+		}
+	}
 }
-/*
-[1,2,3,4,5]
-1
-[1,2,3,4,5]
-2
-[1,2,3,4,5]
-3
-[1,2,3,4,5]
-4
-[1,2,3,4,5]
-5
-[1,2,3,4]
-1
-[1,2,3,4]
-2
-[1,2,3,4]
-3
-[1,2,3,4]
-4
-[1,2,3,4]
-5
- */

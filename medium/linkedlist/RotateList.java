10762f609955a1d5750eb356b01983b9e49e808b
Grigorii <gregsh9.5@gmail.com>
2019-08-11 12:16:27 +0300

    61

--- medium/linkedlist/RotateList.java
+++ medium/linkedlist/RotateList.java
@@ -3,97 +3,41 @@ package Medium.LinkedList;
 import Helpers.Helper;
 import Helpers.ListNode;
 
-/**
- * 61
- *
- * Given a list, rotate the list to the right by k places, where k is non-negative.
- */
-public class RotateList {
-    /**
-     * Time complexity: O(n)
-     * Space complexity: O(1)
-     */
-    public static ListNode solution1(ListNode head, int k) {
-        if ((head == null && k == 0) || head == null) return null;
-        if (k == 0) return head;
-
-        ListNode pointer = head;
-        ListNode last = null;
-        int size = 0;
-
-        while (pointer != null) {
-            size++;
-            last = pointer;
-            pointer = pointer.next;
-        }
-
-        pointer = head;
-        k %= size;
-
-        int i = 0;
-        while (i != size - k - 1) {
-            i++;
-            pointer = pointer.next;
-        }
-
-        last.next = head;
-        head = pointer.next;
-        pointer.next = null;
-
-        return head;
-    }
-
-    /**
-     * Fast runner, Slow runner
-     * Time complexity: O(n)
-     * Space complexity: O(1)
-     */
-    public ListNode solution2(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
-        ListNode node = head;
-        int size = 1;
-
-        while (node.next != null) {
-            size++;
-            node = node.next;
-        }
-
-        k %= size;
-        if (k == 0) return head;
-
-        ListNode fast = head, slow = head;
-
-        while (k-- > 0) {
-            fast = fast.next;
-        }
-
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-
-        ListNode dummy = new ListNode(0);
-        node = dummy;
-        node.next = slow.next;
-        slow.next = null;
-        fast.next = head;
-
-        return dummy.next;
-    }
-
-    public static void main(String[] args) {
-
-        ListNode listNode = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        ListNode listNode3 = new ListNode(3);
-        ListNode listNode4 = new ListNode(4);
-        ListNode listNode5 = new ListNode(5);
-        listNode.next = listNode2;
-        listNode2.next = listNode3;
-        listNode3.next = listNode4;
-        listNode4.next = listNode5;
-
-        ListNode sol = solution1(listNode, 2);
-        Helper.printList(sol);
-    }
+// 61
+public class RotateList 
+{
+	// we need to connect the end to the first, return the new head, remove the cycle
+	class Solution 
+	{
+		public ListNode rotateRight(ListNode head, int k) 
+		{
+			if (head == null) return null;
+			// find length
+			int len = 0;
+			ListNode x = head;
+			while (x != null)
+			{
+				len++;
+				// connect end to head
+				if (x.next == null) 
+				{
+					x.next = head;
+					x = x.next;
+					break;
+				}
+				x = x.next;
+			}
+			// go to the future end
+			int r = k % len;
+			while (--len != r)
+			{
+				x = x.next;
+			}
+			// save the next, disconnect, return saved stf
+			ListNode ans = x.next;
+			x.next = null;
+			return ans;
+		}
+	}
 }
+

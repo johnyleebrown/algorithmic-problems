2f6107cc4dcd5c90a8ed7a375ddbdacdcfe8219f
Grigorii <gregsh9.5@gmail.com>
2019-08-11 11:29:53 +0300

    138

--- medium/linkedlist/CopyListWithRandomPointer.java
+++ medium/linkedlist/CopyListWithRandomPointer.java
@@ -3,74 +3,53 @@ package Medium.LinkedList;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 138
- * A linked list is given such that each node contains
- * an additional random pointer which could point to
- * any node in the list or null. Return a deep copy of the list.
- */
+// 138
 public class CopyListWithRandomPointer {
-    /**
-     * The main problem that arises is that we don't have the random nodes in the first iteration
-     * So we put all the original list nodes into hashmap with new nodes
-     *
-     * Time complexity: O(n)
-     * Space complexity: O(n)
-     */
-    public static RandomListNode solution(RandomListNode head) {
-        if (head == null) return null;
-        Map<RandomListNode, RandomListNode> map = new HashMap<>();
-
-        // loop 1. copy all the nodes
-        RandomListNode node = head;
-        while (node != null) {
-            map.put(node, new RandomListNode(node.label));
-            node = node.next;
-        }
-
-        // loop 2. assign next and random pointers
-        node = head;
-        while (node != null) {
-            map.get(node).next = map.get(node.next);
-            map.get(node).random = map.get(node.random);
-            node = node.next;
-        }
-
-        return map.get(head);
-    }
-
-    static class RandomListNode {
-        int label;
-        RandomListNode next, random;
-
-        RandomListNode(int x) {
-            this.label = x;
-        }
-    }
-
-    static void printLL(RandomListNode head) {
-        while (head != null){
-            System.out.println(head.label);
-            if (head.random != null) {
-                System.out.println("random: " + head.random.label);
-            } else {
-                System.out.println("random: null");
-            }
-            head = head.next;
-        }
-    }
-
-    public static void main(String[] args) {
-        RandomListNode randomListNode1 = new RandomListNode(1);
-        RandomListNode randomListNode2 = new RandomListNode(2);
-        RandomListNode randomListNode3 = new RandomListNode(3);
-        RandomListNode randomListNode4 = new RandomListNode(4);
-        randomListNode1.next = randomListNode2;
-        randomListNode2.next = randomListNode3;
-        randomListNode3.next = randomListNode4;
-        randomListNode1.random = randomListNode3;
-        randomListNode2.random = randomListNode3;
-
-        printLL(solution(randomListNode1));
-    }
+	// 1st solution - use map
+	// 2nd sol, but there we change origin next links, so this don't count
+	// 3nd mod - next pointer of the or node is the new node
+	class Solution 
+	{
+		public Node copyRandomList(Node head) 
+		{
+			// go through the list, set origin nexts to new nodes, set new nodes rands to origins
+			
+			Node newHead = null;
+			Node newPrev = null;
+			
+			Node x = head;
+			while (x != null)
+			{
+				// saved the next so we could go there
+				Node temp = x.next;
+				
+				// create a new node
+				Node newNode = new Node(x.val);
+				// if there is a prev defined, link it to a new node
+				if (newPrev != null) newPrev.next = newNode;
+				// if there is no head yet, assign one
+				if (newHead == null) newHead = newNode;
+				
+				// origin next is new node
+				x.next = newNode;
+				// new rand is origin node
+				newNode.random = x;
+				
+				// going there
+				x = temp;
+			}
+			
+			// go throught the origin again and use rands
+			x = newHead;
+			while (x != null)
+			{   
+				Node newN = x.random.random.next;
+				x.random = newN;
+				x = x.next;
+			}
+			
+			return newHead;
+		}
+		
+	}
 }

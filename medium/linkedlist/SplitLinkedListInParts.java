f271c8da9a0e6c3402b2adaf2769059a56f83bdf
Grigorii <gregsh9.5@gmail.com>
2019-08-13 10:51:02 +0300

    725

--- medium/linkedlist/SplitLinkedListInParts.java
+++ medium/linkedlist/SplitLinkedListInParts.java
@@ -1,69 +1,34 @@
-package Medium.LinkedList;
+// 725
+public class SplitLinkedListInParts 
+{
+	class Solution 
+	{
+		public ListNode[] splitListToParts(ListNode root, int k) 
+		{
+			ListNode[] arr = new ListNode[k];
+			if (root == null) return arr;
+			int len = getLen(root);
+			int x = len <= k ? 1 : len/k;
+			int leftOver = len <= k ? 0 : len%k;
 
-import Helpers.ListNode;
+			for (int i = 0; i < k && root != null; i++)
+			{
+				arr[i] = root;
+				int y = leftOver-- > 0 ? x + 1 : x;
+				while (--y > 0) root = root.next;
+				ListNode next = root.next;
+				root.next = null;
+				root = next;
+			}
 
-/**
- * 725
- *
- * Given a (singly) linked list with head node root, write a function to split the linked list into k consecutive linked list "parts".
- * The length of each part should be as equal as possible: no two parts should have a size differing by more than 1. This may lead to some parts being null.
- * The parts should be in order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal parts occurring later.
- * Return a List of ListNode's representing the linked list parts that are formed.
- * Examples 1->2->3->4, k = 5 // 5 equal parts [ [1], [2], [3], [4], null ]
- */
-public class SplitLinkedListInParts {
+			return arr;
+		}
 
-    // O(n + k), O(max(n, k))
-    class Solution {
-        public ListNode[] splitListToParts(ListNode root, int k) {
-            ListNode cur = root;
-            int N = 0;
-            while (cur != null) {
-                cur = cur.next;
-                N++;
-            }
-            int width = N / k, rem = N % k;
-            ListNode[] ans = new ListNode[k];
-            cur = root;
-            for (int i = 0; i < k; ++i) {
-                ListNode head = new ListNode(0), write = head;
-                for (int j = 0; j < width + (i < rem ? 1 : 0); ++j) {
-                    write = write.next = new ListNode(cur.val);
-                    if (cur != null) cur = cur.next;
-                }
-                ans[i] = head.next;
-            }
-            return ans;
-        }
-    }
+		private int getLen(ListNode root)
+		{
+			int c = 0; while (root != null) { c++; root = root.next; } return c;
+		}
+	}
 
-    // O(n + k), O(k)
-    class Solution2 {
-        public ListNode[] splitListToParts(ListNode root, int k) {
-            ListNode cur = root;
-            int N = 0;
-            while (cur != null) {
-                cur = cur.next;
-                N++;
-            }
-
-            int width = N / k, rem = N % k;
-
-            ListNode[] ans = new ListNode[k];
-            cur = root;
-            for (int i = 0; i < k; ++i) {
-                ListNode head = cur;
-                for (int j = 0; j < width + (i < rem ? 1 : 0) - 1; ++j) {
-                    if (cur != null) cur = cur.next;
-                }
-                if (cur != null) {
-                    ListNode prev = cur;
-                    cur = cur.next;
-                    prev.next = null;
-                }
-                ans[i] = head;
-            }
-            return ans;
-        }
-    }
 }
+

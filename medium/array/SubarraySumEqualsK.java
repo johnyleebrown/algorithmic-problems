6c464d5377f178308ac9fa3faea97de32e8e2ac7
Grigorii <gregsh9.5@gmail.com>
2019-08-23 15:32:59 +0300

    560

---
+++ medium/array/SubarraySumEqualsK.java
@@ -0,0 +1,21 @@
+// 560
+public class SubarraySumEqualsK
+{
+	class Solution 
+	{
+		public int subarraySum(int[] nums, int k) 
+		{
+			int sum = 0, count = 0;
+			Map<Integer, Integer> map = new HashMap<>();
+			map.put(0, 1);
+			for (int i = 0; i < nums.length; i++)
+			{
+				sum+=nums[i];
+				if (map.contains(sum - k)) count+=map.get(sum-k);
+				map.put(sum, maps.getOrDefault(sum, 0) + 1);
+			}
+			return count;	
+		}
+	}
+}
+

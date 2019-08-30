3f1c9018e513ea26c9797c2a14d174f2a6fb571f
Grigorii <gregsh9.5@gmail.com>
2019-08-30 19:43:05 +0300

    658

---
+++ medium/array/bs/FindKClosestElements.java
@@ -0,0 +1,25 @@
+// 658
+class Solution 
+{
+    public List<Integer> findClosestElements(int[] arr, int k, int x) 
+	{
+		List<Integer> ans = new ArrayList<>();
+		
+		return ans;		
+    }
+}
+/*
+-----------------
+[1,2,3,4,5]
+4
+3
+
+[1,2,3,4]
+-----------------
+[1,2,3,4,5,6]
+3
+4
+
+[3,4,5]
+-----------------
+*/

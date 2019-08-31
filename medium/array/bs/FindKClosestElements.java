b805ed9bd1d7bbc13dd3d302c6e8df3d4846a380
Grigorii <gregsh9.5@gmail.com>
2019-08-31 19:34:17 +0300

    658

--- medium/array/bs/FindKClosestElements.java
+++ medium/array/bs/FindKClosestElements.java
@@ -3,23 +3,26 @@ class Solution
 {
     public List<Integer> findClosestElements(int[] arr, int k, int x) 
 	{
-		List<Integer> ans = new ArrayList<>();
-		
-		return ans;		
+		int lo = 0;
+        int hi = arr.length - k - 1;
+
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x)
+            {
+                lo = mid + 1;
+            }
+            else
+            {
+                hi = mid - 1;
+            }
+        }
+
+        List<Integer> res = new ArrayList(k);
+        for (int i = 0; i < k; i++) res.add(arr[lo + i]);
+        return res;
     }
 }
-/*
------------------
-[1,2,3,4,5]
-4
-3
 
-[1,2,3,4]
------------------
-[1,2,3,4,5,6]
-3
-4
-
-[3,4,5]
------------------
-*/

935df9532ac10f8677d7f216c5be1835607671d6
Grigorii <gregsh9.5@gmail.com>
2019-09-04 10:29:12 +0300

    575

---
+++ easy/hashtable/DistributeCandies.java
@@ -0,0 +1,29 @@
+package easy.hashtable;
+
+import java.util.HashSet;
+
+// 575
+// company:amazon
+public class DistributeCandies 
+{
+	class Solution 
+	{
+		public int distributeCandies(int[] candies) 
+		{
+			boolean[] types = new boolean[200001];
+			int res = 0;
+			for (int i = 0; i < candies.length; i++)
+			{
+				if(!types[candies[i]+100000])
+				{
+					types[candies[i]+100000] = true;
+					res++;
+					if(res == candies.length/2) return res;
+				}
+			}
+
+			return res;
+		}
+	}
+}
+

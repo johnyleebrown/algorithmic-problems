f467579aa8ff0043519bac78db494f5446ed4e83
Grigorii <gregsh9.5@gmail.com>
2019-09-07 19:58:50 +0300

    222

--- medium/bfs/tree/BinaryTreeRightSideView.java
+++ medium/bfs/tree/BinaryTreeRightSideView.java
@@ -1,32 +1,32 @@
 package medium.bfs;
 
-/*
- * 199 Binary Tree Right Side View
- */
+// 199
 public class BinaryTreeRightSideView
 {
- 	/* 
-	 * Level order traversal but we insert to the list only the first element of the level if it is not null
+	/* 
+	 * Level order traversal but we insert to the list only 
+	 * the first element of the level if it is not null.
 	 */
-	class Solution {
+	class Solution 
+	{
 		public List<Integer> rightSideView(TreeNode root) 
 		{
 			List<Integer> resultList = new LinkedList<>();
 			if (root == null) return resultList;
-			
+
 			Queue<TreeNode> queue = new LinkedList<>();
 			queue.add(root);
-			
+
 			while (!queue.isEmpty())
 			{
 				resultList.add(q.peek().val);
-				
+
 				int size = queue.size();
 				while (--size >= 0)
 				{
 					TreeNode node = queue.poll();
 					if (node == null) continue;
-					
+
 					if (node.right != null) q.add(node.right);
 					if (node.left != null) q.add(node.left); 
 				}
@@ -36,3 +36,4 @@ public class BinaryTreeRightSideView
 		}
 	}
 }
+

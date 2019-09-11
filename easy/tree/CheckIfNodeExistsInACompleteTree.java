2cfa9f2ef0abef1131ceaadaef882dfb936e287e
Grigorii <gregsh9.5@gmail.com>
2019-09-11 10:13:48 +0300

    complete tree

---
+++ easy/tree/CheckIfNodeExistsInACompleteTree.java
@@ -0,0 +1,38 @@
+/*
+ * Check if node index exists in complete binary tree.
+ * 
+ * google phone interview.
+ */
+{
+	public boolean ifExists(TreeNode root, int target)
+	{
+		if (root == null || target < 0) return false;
+		
+		Deque<Integer> pathFromTarget = new ArrayDeque<>();
+		while (target > 1)
+		{
+			pathFromTarget.push(target);
+			target/=2;
+		}
+	
+		while (!pathFromTarget.isEmpty())
+		{
+			int val = pathFromTarget.pop();
+			if (root.left != null && root.left.val == val)
+			{
+				root = root.left;
+			}
+			else if (root.right != null && root.right.val == val)
+			{
+				root = root.right;
+			}
+			else 
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+

b7e55397525323f13bb5312b9187a7c4e7030322
Grigorii <gregsh9.5@gmail.com>
2019-09-12 23:23:16 +0300

    236 cleanup

--- medium/tree/LowestCommonAncestorOfABinaryTree.java
+++ medium/tree/LowestCommonAncestorOfABinaryTree.java
@@ -1,17 +1,12 @@
-package Medium.Tree;
+package medium.tree;
 
 import util.TreeNode;
 
 // 236
 public class LowestCommonAncestorOfABinaryTree 
 {
-    /*
-	 * firstly, if p and q are in the same subtree, we need to return the first that we meet
-	 * secondly, if they are in the diff subtrees, then we will return the root
-	 *
-     * O(log n) / O(1)
-     */
-	class Solution {
+	class Solution 
+	{
 		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
 		{
 			if (root == p || root == q || root == null) return root;
@@ -19,9 +14,10 @@ public class LowestCommonAncestorOfABinaryTree
 			TreeNode left = lowestCommonAncestor(root.left, p, q);
 			TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-			if (left == null) return right;
-			else if (right == null) return left;
-			else return root;
+			if (left == null && right == null) return root;
+			else if (left == null) return right;
+			else return left;
 		}
 	}
 }
+

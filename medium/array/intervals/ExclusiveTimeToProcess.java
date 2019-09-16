ac4b5eb0ec69fed6bef9cadef2c4bdce4b119aa6
Grigorii <gregsh9.5@gmail.com>
2019-09-16 18:35:28 +0300

    intervals

--- medium/array/intervals/ExclusiveTimeToProcess.java
+++ medium/array/intervals/ExclusiveTimeToProcess.java
@@ -27,48 +27,43 @@ public class ExclusiveTimeToProcess
 		TreeMap<Integer, int[]> map = new TreeMap<>();
 		for (int[] p: processes)
 		{
-			int ind = p[0];
-			// count, index, isStart
-			map.put(p[1], new int[]{-1, ind, 1});
-			map.put(p[2], new int[]{-1, ind, 0});
+			int count = 1;
+			if (map.containsKey(p[1]))
+			{
+				count += map.get(p[1])[2];
+			}
+			map.put(p[1], new int[]{p[0], 0, count});
+			
+			count = -1;
+			if (map.containsKey(p[2]))
+			{
+				count -= map.get(p[2])[2];
+			}
+			map.put(p[2], new int[]{p[0], 1, count});
 		}
 
+		// set the indexes of the processes answers
 		int n = processes.length;
 		int[][] ans = new int[n + 1][2];
 		for (int i = 0; i < n + 1; i++) ans[i][0] = i + 1;
-		int countOngoingProcesses = 1;
-		int[] firstValue = map.get(map.firstKey());
+
+		int count = 0;
 		int prevDuration = map.firstKey();
-		sout(prevDuration, firstValue[1]);
 		List<Integer> l = new ArrayList<>();
-		l.add(firstValue[1]);
 
-		for (int key: map.keySet())
+		for (int curDuration: map.keySet())
 		{
-			int[] val = map.get(key);
-			
-			int curDuration = key;
-			int curInd = val[1];
-			int isStart = val[2];
-		
-			sout(countOngoingProcesses, (l.size() > 0 ? l.get(0) - 1 : -1),
-					(curDuration - prevDuration));	
+			int[] val = map.get(curDuration);
+			int curInd = val[0], start = val[1], c = val[2];
 
-			if (countOngoingProcesses == 1)
-			{
-				ans[l.get(0) - 1][1] += curDuration - prevDuration;
-			}
+			sout(curInd, start, c);
 
-			if (isStart == 0)
-			{
-				countOngoingProcesses++;
-				l.add(curInd);
-			}
-			else
-			{
-				countOngoingProcesses--;	
-				l.remove(new Integer(curInd));
-			}
+			count+=c;
+
+			if (start == 0) l.add(curInd);
+			else l.remove(new Integer(curInd));
+
+			if (count == 1) ans[l.get(0) - 1][1] += curDuration - prevDuration;
 
 			prevDuration = curDuration;
 		}		

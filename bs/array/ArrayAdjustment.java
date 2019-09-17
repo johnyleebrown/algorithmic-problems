75058ee789adb67d7da6cdf12f44f77f16357b11
Grigorii <gregsh9.5@gmail.com>
2019-09-17 23:53:48 +0300

    google

--- bs/array/ArrayAdjustment.java
+++ bs/array/ArrayAdjustment.java
@@ -6,41 +6,96 @@
  */
 public class ArrayAdjustment
 {
-    private static int f(int[] a, int m)
-    {
-        int lo = m/a.length, hi = m, mid = -1;
-        while (lo <= hi)
-        {
-            mid = lo + (hi - lo)/2;
-            if (exceeds(a, mid, m))
-            {
-                hi = mid - 1;
-            }
-            else
-            {
-                lo = mid + 1;
-            }
-        }
-        return mid;
+	/*
+	 * Find the value that is less then max.
+	 * Just change the exceeds method.
+	 */
+	public class Variant1
+	{
+		public static void main(String[] args) 
+		{
+			int[] a = new int[]{10, 5, 20, 30};
+			int max = 40;
+
+			System.out.println(f(a, max));
+		}
 	}
 
-    private static boolean exceeds(int[] a, int mid, int max)
-    {
-        int sum = 0;
-        for (int i: a)
-        {
-            sum += Math.min(i, mid);
-            if (sum > max) return true;
-        }
-        return false;
-    }
-
-    public static void main(String[] args) 
+	/*
+	 * Equals piece.
+	 */
+	public class Variant2
 	{
-        int[] a = new int[]{10, 5, 20, 30};
-	    int max = 40;
-        int m = f(a, max);
-        System.out.println(m);
-    }
+		private static int f(int[] a, int m)
+		{
+			// check if we need to search for anything
+			int s = 0, max = Integer.MIN_VALUE;
+			for (int i: a) 
+			{
+				s += i;
+				max = Math.max(max, i);
+			}
+			if (s == m) return max;
+			//////////////////////
+
+			int lo = 1, hi = m, mid = -1;
+			while (lo <= hi)
+			{
+				mid = lo + (hi - lo)/2;
+				int x = e(a, mid, m);
+				if (x == 0) return mid;
+				if (x == 1)
+				{
+					hi = mid - 1;
+				}
+				else
+				{
+					lo = mid + 1;
+				}
+			}
+			return mid;
+		}
+
+		private static int e(int[] a, int mid, int max)
+		{
+			int sum = 0;
+			for (int i: a)
+			{
+				sum += Math.min(i, mid);
+			}
+			return sum != max ? sum > max ? 1 : -1 : 0;
+		}
+
+		public static void main(String[] args) 
+		{
+			int[] a = new int[]{100, 300, 200, 400};
+			int max = 800;
+			System.out.println(f(a, max)); // 250
+
+			a = new int[]{100, 300, 200, 400};
+			max = 400;
+			System.out.println(f(a, max)); // 100
+
+			a = new int[]{100};
+			max = 100;
+			System.out.println(f(a, max)); // 100
+
+			a = new int[]{100, 100, 100, 100, 200};
+			max = 500;
+			System.out.println(f(a, max)); // 100
+
+			a = new int[]{100, 100, 500, 300, 200, 400};
+			max = 1500;
+			System.out.println(f(a, max)); // 400
+
+			a = new int[]{100, 100, 100, 100, 200};
+			max = 600;
+			System.out.println(f(a, max)); // 200
+
+			a = new int[]{100, 300, 200, 400};
+			max = 999;
+			System.out.println(f(a, max)); // 399
+		}
+	}	
 }
 

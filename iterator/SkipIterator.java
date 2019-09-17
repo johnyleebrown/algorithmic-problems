6579bc7ee99e758fbe87ae6f2dec71d40cc923a4
Grigorii <gregsh9.5@gmail.com>
2019-09-17 08:59:25 +0300

    google

---
+++ iterator/SkipIterator.java
@@ -0,0 +1,95 @@
+package iterator;
+
+import java.util.*;
+import static test.Out.sout;
+
+// Company=Google
+public class SkipIterator implements Iterator<Integer>
+{
+	// map to count collection integers
+	private Map<Integer, Integer> skipItems;
+	private Iterator<Integer> it;
+	private Integer nextElement;
+
+	public SkipIterator(Iterator<Integer> it)
+	{
+		skipItems = new HashMap<>();
+		this.it = it;
+
+		prefill();
+	}
+
+	public boolean hasNext()
+	{
+		return nextElement != null;
+	}
+
+	public Integer next()
+	{
+		if (!hasNext()) throw new RuntimeException();
+
+		Integer element = nextElement;
+		prefill();
+
+		return element;
+	}
+
+	public void skip(int x)
+	{
+		if (!hasNext()) throw new RuntimeException();
+
+		// since we are prefilling next element
+		// we need to check if our prepared element is not the next next
+		if (nextElement == x)
+		{
+			prefill();
+		}
+		else
+		{
+			skipItems.put(x, skipItems.getOrDefault(x, 0) + 1);
+		}
+	}
+
+	private void prefill()
+	{
+		nextElement = null;
+		while (nextElement == null && it.hasNext())
+		{
+			Integer element = it.next();
+			if (skipItems.containsKey(element) && skipItems.get(element) != 0)
+			{
+				skipItems.put(element, skipItems.get(element) - 1);	
+			}
+			else
+			{
+				nextElement = element;
+			}
+		}
+	}
+
+	public static void main(String[] s)
+	{
+		List<Integer> l = new ArrayList<>();
+		l.add(2);
+		l.add(3);
+		l.add(5);
+		l.add(6);
+		l.add(5);
+		l.add(7);
+		l.add(5);
+		l.add(-1);
+		l.add(5);
+		l.add(10);
+
+		SkipIterator sk = new SkipIterator(l.iterator());
+		sout(sk.hasNext());
+		sout(sk.next());
+		
+		sk.skip(5);
+		
+		sout(sk.next());
+		sout(sk.next());
+		sout(sk.hasNext());
+	}
+}
+
--- medium/array/ChampagneTower.java
+++ medium/array/ChampagneTower.java
@@ -1,37 +1,27 @@
-package Medium.Array;
+package medium.array;
 
-/**
- * 799
- * We stack glasses in a pyramid, where the first row has 1 glass, the
- * second row has 2 glasses, and so on until the 100th row. Each glass
- * holds one cup (250ml) of champagne. Then, some champagne is poured
- * in the first glass at the top.  When the top most glass is full, any
- * excess liquid poured will fall equally to the glass immediately to
- * the left and right of it. When those glasses become full, any excess
- * champagne will fall equally to the left and right of those glasses, and
- * so on.(A glass at the bottom row has it's excess champagne fall on the floor.)
- * Now after pouring some non-negative integer cups of champagne, return how
- * full the j-th glass in the i-th row is (both i and j are 0 indexed.)
- */
-public class ChampagneTower {
-    /**
-     * HINT: fill the first row with max amount at the beginning
-     * Time complexity: O(n^2) n - rows
-     * Space complexity: O(n^2)
-     */
-    public static double champagneTower(int poured, int query_row, int query_glass){
+// 799
+public class ChampagneTower 
+{
+    public static double champagneTower(int poured, int query_row, int query_glass)
+	{
         double[][] res = new double[100][100];
         res[0][0] = poured;
 
         // iterate through all rows and all glasses in each row
-        for (int i = 0 ; i < 100 ; i++){
-            for (int j = 0 ; j <= i ; j++) {
-                if (res[i][j] > 1) {
+        for (int i = 0 ; i < 100 ; i++)
+		{
+            for (int j = 0 ; j <= i ; j++) 
+			{
+                if (res[i][j] > 1) 
+				{
                     double amountLeft = res[i][j] - 1;
-                    if (i != 99) {
+                    if (i != 99) 
+					{
                         res[i + 1][j] += amountLeft / 2;
                         res[i + 1][j + 1] += amountLeft / 2;
                     }
+
                     res[i][j] = 1;
                 }
             }
@@ -39,8 +29,5 @@ public class ChampagneTower {
 
         return res[query_row][query_glass];
     }
-
-    public static void main(String[] args) {
-        System.out.println(champagneTower(4,2,1));
-    }
 }
+

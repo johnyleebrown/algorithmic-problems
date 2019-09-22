482611db06f4b4b02153ebc72e99e1baeb1fd8e7
Grigorii <gregsh9.5@gmail.com>
2019-09-22 15:28:39 +0300

    1202

---
+++ graph/cc/SmallestStringWithSwaps.java
@@ -0,0 +1,64 @@
+// 1202
+class Solution 
+{
+    private PriorityQueue<Character> chars = new PriorityQueue<>();
+    private PriorityQueue<Integer> positions = new PriorityQueue<>();
+    private boolean[] seen;
+    private Map<Integer, List<Integer>> g = new HashMap<>();
+    
+    // collect chars and positions from a connected components
+    // sort the chars and fill the resulting string
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) 
+    {
+        constructGraph(s, pairs);
+
+        seen = new boolean[s.length()];
+        char[] res = new char[s.length()];
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!searchForConnectedComponents(i, s)) continue;
+            while (!chars.isEmpty())
+            {
+                char character = chars.poll();
+                int position = positions.poll();
+                
+                res[position] = character;
+            }
+        }
+        
+        return String.valueOf(res);
+    }   
+    
+    // dfs
+    private boolean searchForConnectedComponents(int v, String s)
+    {
+        if (seen[v]) return false;
+        seen[v] = true;
+        
+        chars.add(s.charAt(v));
+        positions.add(v);
+        
+        for (int w: g.get(v))
+        {
+            searchForConnectedComponents(w, s);
+        }
+        
+        return true;
+    }
+    
+    private void constructGraph(constructGraph)
+    {
+        for (int i = 0; i < s.length(); i++) 
+        {
+            g.put(i, new ArrayList<>());
+        }
+        
+        for (List<Integer> p: pairs)
+        {
+            g.get(p.get(0)).add(p.get(1));
+            g.get(p.get(1)).add(p.get(0));
+        }
+    }
+}
+

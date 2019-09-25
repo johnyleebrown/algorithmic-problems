540bea665137111e706141ad8c782d8be4e8e5ee
Grigorii <gregsh9.5@gmail.com>
2019-09-25 09:11:49 +0300

    1203

--- graph/topological/SortItemsByGroupsRespectingDependencies.java
+++ graph/topological/SortItemsByGroupsRespectingDependencies.java
@@ -1,63 +1,35 @@
 package graph.topological;
 
-import java.util.*;
-
-import static test.Out.sout;
-
-// 1203
+/*
+ * 1203
+ * 
+ * Sort groups, then while sorting items, put them into places of sorted groups.
+ */
 class Solution 
 {
-    private Map<Integer, Set<Integer>> groups = new HashMap<>();
-	private Map<Integer, List<Integer>> groupsWithItems = new HashMap<>();
-	private Map<Integer, List<Integer>> items = new HashMap<>();
-	private int[] answer;
-	private int[] sortedGroups;
-	private int sortGroupsCount;
-    private boolean[] groupsSeen;
-
-	private void init(int n, int m, int[] group, List<List<Integer>> beforeItems)
-	{
-		answer = new int[n];
-		
-		for (int i = 0; i < n; i++)
-		{
-			items.put(i, new ArrayList<>());
-            groups.put(group[i], new HashSet<>());
-			groupsWithItems.put(group[i], new ArrayList<>());
-		}
-
-		sortedGroups = new int[groups.size()];
-		sortGroupsCount = groups.size() - 1;
-        groupsSeen = new boolean[groups.size() + 1];
-        
-		int i = 0;
-		for (List<Integer> l: beforeItems)
-		{
-			int groupOfI = group[i];
-
-			for (int before: l)
-			{
-				items.get(before).add(i);
-				
-				int groupOfBefore = group[before];
-				groups.get(groupOfBefore).add(groupOfI);
-			}
-
-			i++;
-		}
-	}
+    private int groupsTotalSize, sortGroupsCount;
+    private Map<Integer, List<Integer>> groups = new HashMap<>(),
+                                        groupsWithItems = new HashMap<>(), 
+                                        items = new HashMap<>();
+    
+    private int[] answer, sortedGroups;
+    private boolean[] groupsSeen, itemsSeen;
 
     public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) 
     {
 		init(n, m, group, beforeItems);
-
-		if (hasCycle(items) || hasCycle(groups))
+        
+		if (hasCycle(groups, new boolean[groupsTotalSize], new boolean[groupsTotalSize]))
 		{
-			return answer;
+			return new int[]{};
 		}
-		
+        
+		if (hasCycle(items, new boolean[n], new boolean[n]))
+		{
+			return new int[]{};
+		}        
+        
 		sortGroups();
-		//System.out.println(Arrays.toString(sortedGroups));
 
 		sortItems(group);
 
@@ -65,11 +37,51 @@ class Solution
 
 		return answer;
     }
-   	
+   
+    private boolean hasCycle(Map<Integer, List<Integer>> graph,
+                             boolean[] seenOnStack, boolean[] seenOverall)
+    {
+        for (int v: graph.keySet())
+        {
+            if (hasCycleDFS(v, graph, seenOnStack, seenOverall))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean hasCycleDFS(int v, Map<Integer, List<Integer>> graph,
+                                boolean[] seenOnStack, boolean[] seenOverall)
+    {
+        if (seenOnStack[v])
+        {
+            return true;
+        }
+        
+        if (seenOverall[v])
+        {
+            return false;
+        }
+        
+        seenOnStack[v] = seenOverall[v] = true;
+        
+        for (int w: graph.get(v))
+        {
+            if (hasCycleDFS(w, graph, seenOnStack, seenOverall))
+            {
+                return true;
+            }
+        }
+        
+        seenOnStack[v] = false;
+        return false;
+    }
+    
 	private void postProcess()
 	{
 		int i = 0;
-
 		for (int group: sortedGroups)
 		{
 			for (int x: groupsWithItems.get(group))
@@ -94,7 +106,7 @@ class Solution
 
 		for (int w: items.get(v))
 		{
-			sortItemsWithDfs(w);
+			sortItemsWithDfs(w, group);
 		}
 
 		groupsWithItems.get(group[v]).add(0, v);
@@ -110,8 +122,8 @@ class Solution
 
 	private void sortGroupsWithDfs(int v)
 	{
-		if (groupsSeen[v + 1]) return;
-		groupsSeen[v + 1] = true;
+		if (groupsSeen[v]) return;
+		groupsSeen[v] = true;
 
 		for (int w: groups.get(v))
 		{
@@ -119,6 +131,53 @@ class Solution
 		}
 
 		sortedGroups[sortGroupsCount--] = v;
+	}
+    
+    private void init(int n, int m, int[] group, List<List<Integer>> beforeItems)
+	{
+		answer = new int[n];
+        itemsSeen = new boolean[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (group[i] == -1)
+            {
+                group[i] = m++;
+            }
+        }
+        
+        groupsTotalSize = m;
+        groupsSeen = new boolean[m];
+        
+		for (int i = 0; i < n; i++)
+		{
+			items.put(i, new ArrayList<>());
+            groups.put(group[i], new ArrayList<>());
+			groupsWithItems.put(group[i], new ArrayList<>());
+		}
+        
+		sortedGroups = new int[groups.size()];
+		sortGroupsCount = groups.size() - 1;
+        
+		int i = 0;
+		for (List<Integer> l: beforeItems)
+		{
+			int groupOfI = group[i];
+
+			for (int before: l)
+			{
+				items.get(before).add(i);
+				
+				int groupOfBefore = group[before];
+                if (groupOfBefore == groupOfI)
+                {
+                    continue;
+                }
+				groups.get(groupOfBefore).add(groupOfI);
+			}
+
+			i++;
+		}
 	}
 }
 

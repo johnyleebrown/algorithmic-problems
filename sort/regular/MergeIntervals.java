5b19fb171d5a956dc1a4514760d16a721bfaea4f
Grigorii <gregsh9.5@gmail.com>
2019-09-29 12:49:30 +0300

    56

---
+++ sort/regular/MergeIntervals.java
@@ -0,0 +1,64 @@
+// 56
+class Solution 
+{
+    public int[][] merge(int[][] intervals) 
+    {
+        int n = intervals.length;
+        
+		// edges
+		if (n == 0 || intervals[0].length == 0) return new int[][]{};
+        
+		// sort
+		sortArray(intervals);
+        
+        // merging
+		List<int[]> l = new ArrayList<>();
+        mergeIntervals(l, intervals, n);
+
+        // to 2d array
+        return listToArray(l);
+    }
+
+	private void sortArray(int[] intervals)
+	{
+        Arrays.sort(intervals, ((a, b) -> a[0] - b[0]));
+	}
+
+	private void mergeIntervals(List<int[]> l, int[] intervals, int n)
+	{
+		int i = 0;
+		int[] curpair = intervals[0];
+		int end = curpair[1];    
+
+        while (i < n)
+        {
+            curpair = intervals[i];
+            end = curpair[1];    
+            i++;
+            
+			// if intervals overlap, end_A >= start_B, update end with end_B
+			while (i < n && end >= intervals[i][0])
+            {
+                end = Math.max(end, intervals[i][1]);
+                i++;
+            }
+            
+			l.add(new int[]{curpair[0], end});
+        }
+	}
+
+	private int[][] listToArray(List<int[]> l)
+	{
+		int[][] ans = new int[l.size()][2];
+        int k = 0;
+
+        for (int[] ar: l)
+        {
+            ans[k++] = new int[]{ar[0], ar[1]};
+        }
+
+		return ans;
+	}
+
+}
+

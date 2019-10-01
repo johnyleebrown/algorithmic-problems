0f1a13a69ecd896e0d00d4f15e439f355aec2616
Grigorii <gregsh9.5@gmail.com>
2019-10-01 09:24:46 +0300

    280

---
+++ sort/regular/WiggleSort.java
@@ -0,0 +1,52 @@
+/*
+ * 280
+ * Company: Google
+ */
+class WiggleSort
+{
+	// straight-forward, sort and rearange
+	class Solution 
+	{
+		public void wiggleSort(int[] nums) 
+		{
+			Arrays.sort(nums);
+
+			for (int i = 1; i < nums.length - 1; i+=2)
+			{
+				int temp = nums[i + 1];
+				nums[i + 1] = nums[i];
+				nums[i] = temp;
+			}
+		}
+	}
+
+	// one pass swapping
+	class Solution2
+	{
+		public void wiggleSort(int[] nums) 
+		{
+			boolean less = true;
+			
+			for (int i = 0; i < nums.length - 1; i++) 
+			{
+				if (less) 
+				{
+					if (nums[i] > nums[i + 1]) 
+					{
+						swap(nums, i, i + 1);
+					}
+				} 
+				else 
+				{
+					if (nums[i] < nums[i + 1]) 
+					{
+						swap(nums, i, i + 1);
+					}
+				}
+
+				less = !less;
+			}
+		}
+	}
+
+}

fb25dea8460875ef5fb29b01bf47cc88eb4004e8
Grigorii <gregsh9.5@gmail.com>
2019-10-02 08:03:20 +0300

    1080

---
+++ tree/modify/InsufficientNodesInRootToLeafPaths.java
@@ -0,0 +1,27 @@
+/*
+ * 1080
+ * Company: Amazon
+ */
+class Solution
+{
+	public TreeNode sufficientSubset(TreeNode root, int limit)
+	{
+		if (root == null)
+		{
+			return null;
+		}
+
+		if (root.left == null && root.right == null)
+		{
+			// if limit covers the value
+			return root.val >= limit ? root : null;
+		}
+
+		int x = limit - root.val;
+		root.left = sufficientSubset(root.left, x);
+		root.right = sufficientSubset(root.right, x);
+
+		return root.left == null && root.right == null ? null : root;
+	}
+}
+

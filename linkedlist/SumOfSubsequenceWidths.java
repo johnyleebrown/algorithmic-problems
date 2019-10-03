f712e4f9eec11cd1f0383eaa7e748893da9b9e83
Grigorii <gregsh9.5@gmail.com>
2019-10-03 17:28:20 +0300

    cleanup

--- linkedlist/SumOfSubsequenceWidths.java
+++ linkedlist/SumOfSubsequenceWidths.java
@@ -1,21 +1,8 @@
-package Hard.Array;
-
-import java.util.LinkedList;
-
-/**
- * 891
- *
- * Given an array of integers A, consider all non-empty subsequences of A.
- * For any sequence S, let the width of S be the difference between the maximum and minimum element of S.
- * Return the sum of the widths of all subsequences of A.
- * As the answer may be very large, return the answer modulo 10^9 + 7.
- */
-public class SumOfSubsequenceWidths {
-    /**
-     * Time complexity: O()
-     * Space complexity: O()
-     */
-    public int sumSubseqWidths(int[] A) {
+// 891
+public class SumOfSubsequenceWidths 
+{
+    public int sumSubseqWidths(int[] A) 
+	{
         LinkedList<Integer> ll = new LinkedList<>();
         LinkedList<Integer> llsmall = new LinkedList<>();
         int k = A.length - 1;
@@ -28,7 +15,6 @@ public class SumOfSubsequenceWidths {
             ll.addFirst(i);
         }
         for (int i = k; i < A.length; i++) {
-//            System.out.println("min : " + A[llsmall.getLast()] + " max : " + A[ll.getLast()]);
             if (ll.getLast() < i - k + 1)
                 ll.removeLast();
             if (llsmall.getLast() < i - k + 1)
@@ -43,3 +29,4 @@ public class SumOfSubsequenceWidths {
         return ll.getLast() + llsmall.getFirst();
     }
 }
+

c605029604ab820ad62cb556f899571d5831f7da
Grigorii <gregsh9.5@gmail.com>
2019-10-06 21:45:22 +0300

    15

--- twopointers/palindrome/LongestPalindromicSubstring.java
+++ twopointers/palindrome/LongestPalindromicSubstring.java
@@ -6,12 +6,16 @@
  * The idea is to explore each character in both directions, we could 
  * have 2 cases, if the palindrome length is odd or it is even, if it 
  * is odd 2 pointers i and j will be k and k + 1, otherwise they will be k and k.
+ *
+ * O(n^2), O(1).
  */
 class Solution 
 {
-	private int finalI, finalJ, max = Integer.MIN_VALUE;
+	private int finalI;
+	private int finalJ;
+	private int max = Integer.MIN_VALUE;
 
-    public String longestPalindrome(String s) 
+	public String longestPalindrome(String s) 
 	{
 		if (s.length() == 0) 
 		{
@@ -26,22 +30,28 @@ class Solution
 		}
 
 		return s.substring(finalI, finalJ + 1);
-    }
+	}
 
 	private void explore(int baseI, int baseJ, String s)
 	{
 		int i = baseI, j = baseJ;
 		while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j))
 		{
-			if (j - i + 1 > max)
-			{
-				max = j - i + 1;
-				finalI = i;
-				finalJ = j;
-			}
-
 			i--;
 			j++;
 		}		
+        
+        updateMax(i, j);
 	}
+    
+    private void updateMax(int i, int j)
+    {
+        if ((j - 1) - (i + 1) + 1 > max)
+        {
+            max = (j - 1) - (i + 1) + 1;
+            finalI = i + 1;
+            finalJ = j - 1;
+        }        
+    }
 }
+

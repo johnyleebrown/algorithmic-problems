e9a8a65854e1311fa4f286c82c74da7373f8a08a
Grigorii <gregsh9.5@gmail.com>
2019-10-10 23:45:32 +0300

    1110

---
+++ tree/modify/DeleteNodesAndReturnForest.java
@@ -0,0 +1,68 @@
+/*
+ * 1110
+ * Google
+ */
+class Solution
+{
+    private Set<Integer> set = new HashSet<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete)
+    {
+        List<TreeNode> res = new LinkedList<>();
+        if (root == null)
+        {
+            return res;
+        }
+
+        for (int i: to_delete)
+        {
+            set.add(i);
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        if (!set.contains(root.val))
+        {
+            res.add(root);
+        }
+
+        while (!q.isEmpty())
+        {
+            TreeNode x = q.poll();
+
+            if (x.left != null)
+            {
+                TreeNode l = x.left;
+                q.add(l);
+
+                if (set.contains(x.left.val))
+                {
+                    x.left = null;
+                }
+                if (set.contains(x.val) && !set.contains(l.val))
+                {
+                    res.add(l);
+                }
+            }
+
+            if (x.right != null)
+            {
+                TreeNode r = x.right;
+                q.add(r);
+
+                if (set.contains(x.right.val))
+                {
+                    x.right = null;
+                }
+                if (set.contains(x.val) && !set.contains(r.val))
+                {
+                    res.add(r);
+                }
+            }
+        }
+
+        return res;
+    }
+}
+

e5a2f1b578672040cf0f7c90c34aee81870d36ec
Grigorii <gregsh9.5@gmail.com>
2019-10-22 07:35:39 +0300

    551

---
+++ string/regular/StudentAttendanceRecordI.java
@@ -0,0 +1,37 @@
+/*
+ * 551
+ * Google
+ */
+class Solution
+{
+	public boolean checkRecord(String s)
+	{
+		int countA = 0, countL = 0;
+		int charA = 'A', charL ='L';
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (countL > 0 && s.charAt(i) != charL)
+			{
+				countL = 0;
+			}
+
+			if (s.charAt(i) == charA)
+			{
+				countA++;
+			}
+			else if(s.charAt(i) == charL)
+			{
+				countL++;
+			}
+
+			if (countA >1 || countL > 2 )
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+

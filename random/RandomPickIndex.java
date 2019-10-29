bc02d36f320c44a8da67122d503829f2ea753a8d
Grigorii <gregsh9.5@gmail.com>
2019-10-29 18:49:00 +0100

    382

---
+++ random/RandomPickIndex.java
@@ -0,0 +1,39 @@
+/*
+ * 398
+ */
+public class RandomPickIndex
+{
+	/* 
+	 * Reservoir sampling.
+	 * Treating target elements as stream, so for each target element 
+	 * probability to be chosen will be 1/countTargets.
+	 */
+	class Solution
+	{
+		private int[] nums;
+		public Solution(int[] nums)
+		{
+			this.nums = nums;
+		}
+		public int pick(int target)
+		{
+			int choice = -1;
+			int countTargets = 0;
+			for (int i = 0; i < nums.length; i++)
+			{
+				if (nums[i] == target)
+				{
+					countTargets++;
+					Random r = new Random();
+					int x = r.nextInt(countTargets);
+					if (x == 0)
+					{
+						choice = i;
+					}
+				}
+			}
+			return choice;
+		}
+	}
+}
+

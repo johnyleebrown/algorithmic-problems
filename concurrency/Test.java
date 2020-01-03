6a8fc13a75b96262f6df7b85571a347b73f65df1
Grigorii <gregsh9.5@gmail.com>
2020-01-03 20:12:18 +0300

    redo 3

---
+++ concurrency/Test.java
@@ -0,0 +1,10 @@
+package concurrency;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("H");
+    }
+}
---
+++ recursion/PowXN.java
@@ -0,0 +1,23 @@
+package medium.recusrion;
+
+public class PowXN
+{
+	// regulat solution produces SO
+	// multiplying halves of n
+	// extra juice with n less than zero
+	class Solution {
+		public double myPow(double x, int n) 
+		{
+			if (n == 0) return 1;
+			double half = myPow(x, n / 2);
+			if (Math.abs(n) % 2 == 1) return n < 0 ? 1 / x * half * half : x * half * half;
+			return half * half;
+		}
+	}
+/*
+2.00000
+10
+0.00001
+2147483647
+*/
+}

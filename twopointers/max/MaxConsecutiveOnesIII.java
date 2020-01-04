2581200600b6684a47224ad9d8514c1c19785366
Grigorii <gregsh9.5@gmail.com>
2020-01-04 17:30:22 +0300

    1004 redo

---
+++ twopointers/max/MaxConsecutiveOnesIII.java
@@ -0,0 +1,45 @@
+/*
+ * 1004
+ *
+ * Check 424
+ */
+class MaxConsecutiveOnesIII
+{
+	class Solution 
+	{
+		public int longestOnes(int[] A, int K) 
+		{
+			int l = 0;
+			int result = 0;
+
+			// count zeros because we need to replace them
+			int zerosCount = 0; 
+
+			for (int r = 0; r < A.length; r++)
+			{
+				// use right window boundary
+				if (A[r] == 0)
+				{
+					zerosCount++;
+				}
+
+				while (zerosCount > K)
+				{
+					// use left window boundary
+					if (A[l] == 0)
+					{
+						zerosCount--;
+					}
+
+					l++;
+				}
+
+				// update result
+				result = Math.max(result, r - l + 1);
+			}
+
+			return result;
+		}
+	}
+}
+

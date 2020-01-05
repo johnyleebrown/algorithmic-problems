3986354c644a60aed45a3b8714f6580a015fb8d5
Grigorii <gregsh9.5@gmail.com>
2020-01-05 15:40:27 +0300

    76 redo

--- recursion/regular/LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ recursion/regular/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -1,18 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
  * 395
+ * ============
+ * Task.
  *
  * Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every
  * character in T appears no less than k times.
- *
+ * ============
  * Solution.
  *
  * Recursively split on characters that appear less than k times, there can not be any answer containing these chars.
+ * ============
+ * Test cases.
+ *
+ * "aaabb"
+ * 3
+ * "ababbc"
+ * 2
+ * "cababb"
+ * 2
+ * "abcabb"
+ * 2
+ * "cababbbc"
+ * 3
+ * "abcabbbc"
+ * 3
+ * "cabcabbb"
+ * 3
+ * "cabcabbbbc"
+ * 4
+ * "cabccabccc"
+ * 4
+ * "cccaaaaaaaabccabccc"
+ * 8
+ * ============
  */
 public class LongestSubstringWithAtLeastKRepeatingCharacters
 {
-	class Solution
+	/**
+	 * Pulling out good substrings with regex and checking them recursively.
+	 */
+	class Solution1
 	{
 		public int longestSubstring(String s, int k)
 		{
@@ -57,6 +90,69 @@ public class LongestSubstringWithAtLeastKRepeatingCharacters
 			// get the longest string
 			int max = 0;
 			for (String a : ar)
+			{
+				max = Math.max(max, longestSubstring(a, k));
+			}
+
+			return max;
+		}
+	}
+
+	/**
+	 * No regex. Pulling out good substrings and checking them recursively.
+	 */
+	class Solution2
+	{
+		public int longestSubstring(String s, int k)
+		{
+			if (s == null || s.length() == 0)
+			{
+				return 0;
+			}
+			if (k == 0)
+			{
+				return s.length();
+			}
+
+			int[] map = new int[26];
+			for (int i = 0; i < s.length(); i++)
+			{
+				map['z' - s.charAt(i)]++;
+			}
+
+			Set<Character> badChars = new HashSet<>();
+			for (int i = 0; i < 26; i++) if (map[i] > 0 && map[i] < k)
+			{
+				badChars.add((char) ('z' - i));
+			}
+			if (badChars.size() == 0)
+			{
+				return s.length();
+			}
+
+			List<String> ar = new ArrayList<>();
+			int l = 0;
+			for (int r = 0; r < s.length(); r++)
+			{
+				// found bad after good => want to cut the substring
+				if (r > 0 && badChars.contains(s.charAt(r)) && !badChars.contains(s.charAt(r - 1)))
+				{
+					ar.add(s.substring(l, r));
+				}
+				// found good after bad => put l there
+				else if (r > 0 && !badChars.contains(s.charAt(r)) && badChars.contains(s.charAt(r - 1)))
+				{
+					l = r;
+				}
+				// good char is the last one
+				else if (r == s.length() - 1 && !badChars.contains(s.charAt(r)))
+				{
+					ar.add(s.substring(l, r + 1));
+				}
+			}
+
+			int max = 0;
+			for (String a: ar)
 			{
 				max = Math.max(max, longestSubstring(a, k));
 			}
--- twopointers/min/MinimumWindowSubstring.java
+++ twopointers/min/MinimumWindowSubstring.java
@@ -1,58 +1,95 @@
-// 76
+/**
+ * 76
+ *
+ * Task.
+ *
+ * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in
+ * complexity O(n).
+ *
+ * Solution.
+ *
+ * Classic sliding window.
+ */
 public class MinimumWindowSubstring
 {
-	class Solution 
+	class Solution
 	{
-		public String minWindow(String s, String t) 
+		public String minWindow(String s, String t)
 		{
-			if (t.length() > s.length()) return "";
-
-			int start = 0, end = 0, resultstart = 0;
-			int minlen = Integer.MAX_VALUE;
-			int leftOver = t.length();
-
-			int[] map = new int[256];
-			for (int i = 0; i < t.length(); i++) map[t.charAt(i)]++;
-
-			while (end < s.length())
+			if (s == null || s.length() == 0)
 			{
-				char charAtEnd = s.charAt(end++);
-				
-				// if we encounter the char from t
-				// we want to decrease the counter of target chars
-				if (map[charAtEnd] > 0) leftOver--;
+				return "";
+			}
+			if (t == null || t.length() == 0)
+			{
+				return s;
+			}
 
-				// decrease counter as we reached the letter
-				map[charAtEnd]--;
-
-				// if the substring contains all the target chars
-				while (leftOver == 0)
+			// get counts of chars in t string
+			Integer[] mapT = new Integer[256];
+			for (int i = 0; i < t.length(); i++)
+			{
+				// the chars that are not in t will be marked as null
+				// so we could identify them later
+				if (mapT[t.charAt(i)] == null)
 				{
-					// if the length of suitable string is less than min
-					if (end - start < minlen)
+					mapT[t.charAt(i)] = 0;
+				}
+
+				mapT[t.charAt(i)]++;
+			}
+
+			int l = 0;
+			int allCharsFromTInWindowCount = 0;
+			int minL = 0, minR = 0;
+			int minLen = Integer.MAX_VALUE;
+
+			for (int r = 0; r < s.length(); r++)
+			{
+				// if the char at pos r counts as seen
+				if (mapT[s.charAt(r)] != null)
+				{
+					mapT[s.charAt(r)]--;
+
+					if (mapT[s.charAt(r)] >= 0)
 					{
-						// update minlen
-						minlen = end - start;
-						
-						// remember the start of the suitable string
-						resultstart = start;
+						allCharsFromTInWindowCount++;
 					}
-					
-					char charAtStart = s.charAt(start++);
-					
-					// if the letter at start is used in s the
-					// exact amount as we have in t
-					if (map[charAtStart] == 0) leftOver++;
-					
-					// used it and we move further so increment count
-					map[charAtStart]++;
+				}
+
+				// when we covered all chars from t string
+				while (allCharsFromTInWindowCount == t.length())
+				{
+					if (mapT[s.charAt(l)] != null)
+					{
+						// returning the count
+						mapT[s.charAt(l)]++;
+
+						// the count became 1 means that we moved from the char at l
+						// the char at l is no longer a part of the window
+						// and we can not longer minimize the window
+						if (mapT[s.charAt(l)] == 1)
+						{
+							allCharsFromTInWindowCount--;
+						}
+					}
+
+					l++;
+				}
+
+				// if l was ever changed
+				if (l > 0)
+				{
+					if (r - l < minLen)
+					{
+						minL = l;
+						minR = r;
+						minLen = r - l;
+					}
 				}
 			}
 
-			return minlen == Integer.MAX_VALUE 
-				? "" 
-				: s.substring(resultstart, resultstart + minlen);
+			return minLen == Integer.MAX_VALUE ? "" : s.substring(minL - 1, minR + 1);
 		}
 	}
 }
-

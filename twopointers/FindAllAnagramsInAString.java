a2c2129f6fe5c727724244102fc1db7dffc61881
Grigorii <gregsh9.5@gmail.com>
2020-01-08 00:23:46 +0300

    567

--- twopointers/FindAllAnagramsInAString.java
+++ twopointers/FindAllAnagramsInAString.java
@@ -9,6 +9,10 @@ import java.util.List;
  * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s. Strings consists of
  * lowercase English letters only and the length of both strings s and p will not be larger than 20,100. The order of
  * output does not matter.
+ *
+ * Similar.
+ *
+ * 567
  */
 public class FindAllAnagramsInAString
 {

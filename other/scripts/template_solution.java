8f6ebb28d63df549ed4f2c0368ba5a40f95f4bcd
Grigorii <gregsh9.5@gmail.com>
2020-01-19 08:16:04 +0300

    5318

---
+++ other/scripts/template_solution.java
@@ -0,0 +1,27 @@
+/**
+ * $INSERT_PROBLEM_NUMBER
+ *
+ * ======
+ *
+ * Task.
+ *
+ * $INSERT_TASK
+ *
+ * ======
+ *
+ * Companies: $INSERT_COMPANIES
+ *
+ * ======
+ *
+ * Source: $INSERT_SOURCE
+ */
+class $INSERT_CLASS_NAME
+{
+	/**
+	 *
+	 */
+	class Solution
+	{
+
+	}
+}

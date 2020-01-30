15063301aa1eb5523e6fdfcfc8dc35b8818937b2
Grigorii <gregsh9.5@gmail.com>
2020-01-30 18:44:38 +0300

    1332

--- src/array/prefix/SubarraySumsDivisibleByK.java
+++ src/array/prefix/SubarraySumsDivisibleByK.java
@@ -1,30 +1,26 @@
-/*
+package array.prefix;
+
+/**
  * 974
- * Yandex, Amazon
  */
 public class SubarraySumsDivisibleByK
 {
-	/*
-	 * O(n)
-	 * 
-	 */
-	class Solution 
+	class Solution1
 	{
-		public int subarraysDivByK(int[] A, int K) 
+		public int subarraysDivByK(int[] A, int K)
 		{
 			int[] sum = new int[K];
 			sum[0] = 1;
 			int pre = 0, count = 0;
-			for (int i: A)
+			for (int i : A)
 			{
 				pre += i;
 				int rem = pre % K;
 				if (rem < 0) rem += K;
 				count += sum[rem];
-				sum[rem]++; 
+				sum[rem]++;
 			}
 			return count;
 		}
 	}
 }
-

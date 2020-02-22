0203c9aa745f1107a3d2a47932d17f51996ea2c7
Grigorii <gregsh9.5@gmail.com>
2020-02-22 11:02:01 +0300

    tester

--- problems/algorithms/src/util/test/Tester.java
+++ problems/algorithms/src/util/test/Tester.java
@@ -13,9 +13,12 @@ import static util.test.Out.sout;
 public class Tester
 {
 	private static final String NEW_LINE = "\n";
-//	private static final String TESTER_START = "[============== Tester ==============]";
-	private static final String TESTER_S   = "======================================";
-	private static final String TESTER_SEP = "--------------------------------------";
+	private static String TESTER_S;
+	private static String TESTER_SEP;
+	private static final String TEXT_VERDICT = "verdict";
+	private static final int DEFAULT_LINE_LEN = 40;
+	private static final String TEXT_ACC = "[ACCEPTED]";
+	private static final String TEXT_EXEC = "execution";
 
 	private final Method method;
 	private final List<Object> results;
@@ -51,9 +54,27 @@ public class Tester
 		sout(TESTER_SEP);
 	}
 
+	private String createDefaultLongString(char c)
+	{
+		return createLongString(c, DEFAULT_LINE_LEN);
+	}
+
+	private String createLongString(char c, int n)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++)
+		{
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
 	@SuppressWarnings({"unchecked"})
 	public Tester(Object obj)
 	{
+		TESTER_S = createDefaultLongString('=');
+		TESTER_SEP = createDefaultLongString('-');
+
 		classObject = obj;
 //		for (Method m : obj.getClass().getMethods())
 //		{
@@ -138,10 +159,13 @@ public class Tester
 		}
 		if (!nok)
 		{
-			sout("Verdict:   " + "Accepted");
-			sout(TESTER_SEP);
+			String emptiness = createLongString('.', DEFAULT_LINE_LEN - TEXT_VERDICT.length() - TEXT_ACC.length());
+			sout(TEXT_VERDICT + emptiness + TEXT_ACC);
+//			sout(TESTER_SEP);
 		}
-		sout("Execution: " + getAvgExecTimes());
+		String avgTimes = "["+ String.format("%.2f", getAvgExecTimes())+"]";
+		String e2 = createLongString('.', DEFAULT_LINE_LEN - avgTimes.length() - TEXT_EXEC.length());
+		sout(TEXT_EXEC + e2 + avgTimes);
 		sout(TESTER_S);
 	}
 

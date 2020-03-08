86a1b551d4a2792041dada75479376e9b134b2a0
Grigorii <gregsh9.5@gmail.com>
2020-03-08 18:44:44 +0300

    1374

--- problems/algorithms/src/string/regular/CompareStringsByFrequencyOfTheSmallestCharacter.java
+++ problems/algorithms/src/string/regular/CompareStringsByFrequencyOfTheSmallestCharacter.java
@@ -1,51 +1,54 @@
-/*
+package string.regular;
+
+/**
  * 1170
- * Google
  */
-class Solution
+class CompareStringsByFrequencyOfTheSmallestCharacter
 {
-	public int[] numSmallerByFrequency(String[] queries, String[] words) 
+	class Solution
 	{
-		int[] wordScores = new int[words.length];
-		int[] scoreMap = new int[11];
-		for (int i = 0; i < words.length; i++) 
+		public int[] numSmallerByFrequency(String[] queries, String[] words)
 		{
-			wordScores[i] = f(words[i]);
-			scoreMap[wordScores[i]]++;
-		}
-
-		int[] prefixSumScore = new int[11];
-		prefixSumScore[0] = scoreMap[0];
-		for (int i = 1; i < 11; i++) 
-		{
-			prefixSumScore[i] = prefixSumScore[i - 1] + scoreMap[i];
-		}
-
-		int[] ans = new int[queries.length];
-		for (int i = 0; i < queries.length; i++) 
-		{
-			String query = queries[i];
-			int score = f(query);
-			ans[i] = words.length - prefixSumScore[score];
-		}
-
-		return ans;
-	}
-
-	private int f(String word)
-	{
-		int[] freq = new int[26];
-		char minChar = 'z';
-		for (int i = 0; i < word.length(); i++)
-		{
-			char c = word.charAt(i);
-			if (c <= minChar)
+			int[] wordScores = new int[words.length];
+			int[] scoreMap = new int[11];
+			for (int i = 0; i < words.length; i++)
 			{
-				freq[c - 'a']++;
-				minChar = c;
+				wordScores[i] = f(words[i]);
+				scoreMap[wordScores[i]]++;
 			}
+
+			int[] prefixSumScore = new int[11];
+			prefixSumScore[0] = scoreMap[0];
+			for (int i = 1; i < 11; i++)
+			{
+				prefixSumScore[i] = prefixSumScore[i - 1] + scoreMap[i];
+			}
+
+			int[] ans = new int[queries.length];
+			for (int i = 0; i < queries.length; i++)
+			{
+				String query = queries[i];
+				int score = f(query);
+				ans[i] = words.length - prefixSumScore[score];
+			}
+
+			return ans;
 		}
-		return freq[minChar - 'a'];
+
+		private int f(String word)
+		{
+			int[] freq = new int[26];
+			char minChar = 'z';
+			for (int i = 0; i < word.length(); i++)
+			{
+				char c = word.charAt(i);
+				if (c <= minChar)
+				{
+					freq[c - 'a']++;
+					minChar = c;
+				}
+			}
+			return freq[minChar - 'a'];
+		}
 	}
 }
-
--- problems/algorithms/src/string/regular/FindAndReplaceInString.java
+++ problems/algorithms/src/string/regular/FindAndReplaceInString.java
@@ -1,38 +1,42 @@
 package string.regular;
 
-/*
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * 833
- * Company:Google
  */
-class Solution 
+class FindAndReplaceInString
 {
-    public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) 
+	class Solution
 	{
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < sources.length; i++)
-        {
-            if (S.startsWith(sources[i], indexes[i]))
-            {
-                map.put(indexes[i], i);
-            }
-        }
+		public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets)
+		{
+			Map<Integer, Integer> map = new HashMap<>();
+			for (int i = 0; i < sources.length; i++)
+			{
+				if (S.startsWith(sources[i], indexes[i]))
+				{
+					map.put(indexes[i], i);
+				}
+			}
 
-        int i = 0;
-        String ans = "";
-        while (i < S.length())
-        {
-            if (map.containsKey(i))
-            {
-                ans += targets[map.get(i)];
-                i += sources[map.get(i)].length();
-            }
-            else
-            {
-                ans += S.charAt(i);
-                i++;
-            }
-        }
-        return ans;
-    }
+			int i = 0;
+			String ans = "";
+			while (i < S.length())
+			{
+				if (map.containsKey(i))
+				{
+					ans += targets[map.get(i)];
+					i += sources[map.get(i)].length();
+				}
+				else
+				{
+					ans += S.charAt(i);
+					i++;
+				}
+			}
+			return ans;
+		}
+	}
 }
-

ebf7c44632982d082d8cf3287789b3adb1fdfaf0
Grigorii <gregsh9.5@gmail.com>
2020-03-24 01:42:57 +0300

    45

--- problems/algorithms/src/util/contest/FindTheDistanceValueBetweenTwoArrays.java
+++ problems/algorithms/src/util/contest/FindTheDistanceValueBetweenTwoArrays.java
@@ -6,17 +6,17 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
- * $INSERT_PROBLEM_NUMBER
+ * 1385
  *
  * ======
  *
  * Task.
  *
- * $INSERT_TASK
+ * Given two integer arrays arr1 and arr2, and the integer d, return the
+ * distance value between the two arrays.
  *
- * ======
- *
- * Similar: $INSERT_SIMILAR.
+ * The distance value is defined as the number of elements arr1[i] such that
+ * there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
  *
  * ======
  *
@@ -29,30 +29,36 @@ public class FindTheDistanceValueBetweenTwoArrays
 	 */
 	public static class Solution
 	{
-		public int findTheDistanceValue(int[] a, int[] b, int d) {
+		public int findTheDistanceValue(int[] a, int[] b, int d)
+		{
 			TreeSet<Integer> t = new TreeSet<>();
-			for (int i = 0; i < b.length; i++) {
+			for (int i = 0; i < b.length; i++)
+			{
 				t.add(b[i]);
 			}
 			int res = 0;
 			System.out.println(Arrays.toString(b));
-			for (int i = 0; i < a.length; i++) {
-				System.out.println((a[i] - d)+" "+t.ceiling(a[i] - d));
-				System.out.println((a[i] + d)+" "+t.floor(a[i] + d));
-				if (t.ceiling(a[i] - d) == null && t.floor(a[i] + d) == null) {
+			for (int i = 0; i < a.length; i++)
+			{
+				System.out.println((a[i] - d) + " " + t.ceiling(a[i] - d));
+				System.out.println((a[i] + d) + " " + t.floor(a[i] + d));
+				if (t.ceiling(a[i] - d) == null && t.floor(a[i] + d) == null)
+				{
 					res++;
 				}
 			}
 			return res;
 		}
 
-        public Solution(){}
+		public Solution()
+		{
+		}
 	}
 
 	public static void main(String[] args)
 	{
 		new Tester(new Solution())
-				.add(new int[]{4,5,8}, new int[]{10,9,1,8}, 2).expect(2)
+				.add(new int[]{4, 5, 8}, new int[]{10, 9, 1, 8}, 2).expect(2)
 				.run();
 	}
 }
---
+++ problems/algorithms/src/util/contest/LongestHappyPrefix.java
@@ -0,0 +1,50 @@
+package util.contest;
+
+import util.tester.Tester;
+
+/**
+ * 1392
+ *
+ * ======
+ *
+ * Task.
+ *
+ * A string is called a happy prefix if is a non-empty prefix which is also a
+ * suffix (excluding itself).
+ *
+ * Given a string s. Return the longest happy prefix of s .
+ *
+ * Return an empty string if no such prefix exists.
+ *
+ * ======
+ *
+ * Similar: $INSERT_SIMILAR.
+ *
+ * ======
+ *
+ * Source: Leetcode
+ */
+public class LongestHappyPrefix
+{
+	/**
+	 * $INSERT_EXPLANATION.
+	 */
+	public static class Solution
+	{
+		public int solve()
+		{
+			return -1;
+		}
+
+		public Solution()
+		{
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		new Tester(new Solution())
+				.add().expect()
+				.run();
+	}
+}

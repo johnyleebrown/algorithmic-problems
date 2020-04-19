57ca7c9c460a7ba5143afeeaaa3e144aea5763d4
Grigorii <gregsh9.5@gmail.com>
2020-04-19 22:00:47 +0300

    33

--- problems/algorithms/src/bs/regular/SearchInRotatedSortedArray.java
+++ problems/algorithms/src/bs/regular/SearchInRotatedSortedArray.java
@@ -3,47 +3,40 @@ package bs.regular;
 /**
  * 33
  */
-public class SearchInRotatedSortedArray
-{
-	class Solution
-	{
-		public int search(int[] nums, int target)
-		{
-			int lo = 0, hi = nums.length - 1;
+public class SearchInRotatedSortedArray {
+	public static class Solution {
+		public int search(int[] a, int t) {
+			int n = a.length;
+			if (n == 0) {
+				return -1;
+			}
+			if (t < a[0] && t > a[n - 1]) {
+				return -1;
+			}
 
-			while (lo <= hi)
-			{
-				int mid = (lo + hi) / 2;
+			int lo = 0;
+			int hi = n - 1;
 
-				if (nums[mid] == target)
-				{
-					return mid;
-				}
-
-				if (nums[lo] <= nums[mid])
-				{
-					if (target >= nums[lo] && target < nums[mid])
-					{
-						hi = mid - 1;
-					}
-					else
-					{
+			while (hi - lo >= 0) {
+				int mid = lo + (hi - lo) / 2;
+				if (t < a[0]) {//answer is in right part
+					if (a[mid] < a[0]) {
+						if (a[mid] == t) return mid;
+						if (a[mid] < t) lo = mid + 1;
+						else hi = mid - 1;
+					} else {
 						lo = mid + 1;
 					}
-				}
-				else
-				{
-					if (target > nums[mid] && target <= nums[hi])
-					{
-						lo = mid + 1;
-					}
-					else
-					{
+				} else {//answer is in left part
+					if (a[mid] < a[0]) {
 						hi = mid - 1;
+					} else {
+						if (a[mid] == t) return mid;
+						if (a[mid] < t) lo = mid + 1;
+						else hi = mid - 1;
 					}
 				}
 			}
-
 			return -1;
 		}
 	}

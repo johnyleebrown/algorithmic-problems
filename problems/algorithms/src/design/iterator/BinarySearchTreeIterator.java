2e942652116041e4097f41b73ca7554b9fcf0805
Grigorii <gregsh9.5@gmail.com>
2020-04-22 16:19:44 +0300

    173

--- problems/algorithms/src/design/iterator/BinarySearchTreeIterator.java
+++ problems/algorithms/src/design/iterator/BinarySearchTreeIterator.java
@@ -1,81 +1,15 @@
 package design.iterator;
 
 import util.ds.InputReader;
+import util.ds.TreeNode;
 
-import java.util.concurrent.Semaphore;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 173
  */
 public class BinarySearchTreeIterator {
-    public static class Solution2 {
-        public static class BSTIterator {
-            TreeNodeCustom next;
-            Semaphore ss = new Semaphore(1);
-            TreeNodeCustom prev;
-
-            public BSTIterator(TreeNodeCustom root) {
-                next = addLefts(root);
-                prev = null;
-            }
-
-            public int next() throws InterruptedException {
-                int ret = next.val;
-                System.out.println(next.prev == null ? null : next.prev.val);
-                prev = next.prev;
-                if (next.right != null && next.next == null)
-                    next = addLefts(next.right);
-                else
-                    next = next.next;
-                return ret;
-            }
-
-            private void p() {
-
-            }
-
-            private TreeNodeCustom addLefts(TreeNodeCustom cur) {
-                TreeNodeCustom x = cur;
-                while (x.left != null) {
-//                    System.out.println(x.val);
-                    if (x.prev == null) x.prev = x.left;
-                    x.left.next = x;
-                    x = x.left;
-                }
-//                System.out.println(x.val);
-                return x;
-            }
-
-            public boolean hasNext() {
-                return next != null;
-            }
-
-            public int previous() {
-                int ret = prev.val;
-                next = prev.next;
-                prev = prev.prev;
-                return ret;
-            }
-
-            public boolean hasPrevious() {
-                return prev != null;
-            }
-        }
-    }
-
-    /**
-     * TreeNode with a link to prev.
-     */
-    public static class TreeNodeCustom {
-        TreeNodeCustom prev, next;
-        TreeNodeCustom left, right;
-        int val;
-
-        public TreeNodeCustom(int val) {
-            this.val = val;
-        }
-    }
-
     public static void main(String[] args) throws InterruptedException {
         TreeNodeCustom t1 = new TreeNodeCustom(7);
         TreeNodeCustom t1l = new TreeNodeCustom(3);
@@ -90,24 +24,120 @@ public class BinarySearchTreeIterator {
         InputReader in = new InputReader(System.in);
         while (true) {
             int x = in.nextInt();
-//            int y = in.nextInt();
             if (x == 1) {
                 System.out.print("next: ");
-                // DO_THING_1
                 System.out.println(it.next());
             } else if (x == 2) {
                 System.out.print("has next: ");
-                // DO_THING_2
                 System.out.println(it.hasNext());
             } else if (x == 3) {
                 System.out.print("prev: ");
-                // DO_THING_3
                 System.out.println(it.previous());
             } else if (x == 4) {
                 System.out.print("has prev: ");
-                // DO_THING_4
                 System.out.println(it.hasPrevious());
             } else return;
+        }
+    }
+
+    public static class Solution {
+        class BSTIterator {
+            Deque<TreeNode> st;
+
+            public BSTIterator(TreeNode root) {
+                st = new ArrayDeque<>();
+                addLefts(root);
+            }
+
+            private void addLefts(TreeNode r) {
+                while (r != null) {
+                    st.addLast(r);
+                    r = r.left;
+                }
+            }
+
+            public int next() {
+                TreeNode cur = st.removeLast();
+                addLefts(cur.right);
+                return cur.val;
+            }
+
+            public boolean hasNext() {
+                return !st.isEmpty();
+            }
+        }
+    }
+
+    /**
+     * Use prev and next links, otherwise use stack to get next items like in
+     * prev solution.
+     *
+     * First 3 lines:
+     * TreeNodeCustom next = cur.next;
+     * cur = next;
+     * return next.val;
+     *
+     * Then expand it.
+     */
+    public static class Solution2 {
+        public static class BSTIterator {
+            private TreeNodeCustom cur;
+            private Deque<TreeNodeCustom> st;
+
+            public BSTIterator(TreeNodeCustom root) {
+                st = new ArrayDeque<>();
+                addLefts(root);
+                cur = st.removeLast();
+            }
+
+            private void addLefts(TreeNodeCustom cur) {
+                while (cur != null) {
+                    st.add(cur);
+                    cur = cur.left;
+                }
+            }
+
+            public int next() {
+                if (cur.next == null) {
+                    addLefts(cur.right);
+                    cur.next = st.removeLast();
+                }
+
+                TreeNodeCustom next = cur.next;
+                next.prev = cur;
+                cur = next;
+                return next.val;
+            }
+
+            public boolean hasNext() {
+                if (cur.next == null) {
+                    return !st.isEmpty() || cur.right != null;
+                }
+                return true;
+            }
+
+            public int previous() {
+                TreeNodeCustom prev = cur.prev;
+                cur = prev;
+                return prev.val;
+            }
+
+            public boolean hasPrevious() {
+                return cur.prev != null;
+            }
+        }
+    }
+
+    /**
+     * Regular TreeNode with a link to prev and next.
+     */
+    public static class TreeNodeCustom {
+        TreeNodeCustom prev, next;
+        TreeNodeCustom left, right;
+        int val;
+
+        public TreeNodeCustom(int val) {
+            this.val = val;
         }
     }
 }
--- problems/algorithms/src/design/list/DesignSnakeGame.java
+++ problems/algorithms/src/design/list/DesignSnakeGame.java
@@ -1,4 +1,4 @@
-package design;
+package design.list;
 
 import java.util.LinkedList;
 import java.util.List;

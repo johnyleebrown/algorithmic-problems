5b3916941d7b9227a184b4d6e6fd1cb765823b5f
Grigorii <gregsh9.5@gmail.com>
2020-04-28 12:21:20 +0300

    523

---
+++ problems/algorithms/src/array/prefix/subarray/ContinuousSubarraySum.java
@@ -0,0 +1,31 @@
+package array.prefix.subarray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 523
+ */
+public class ContinuousSubarraySum {
+    /**
+     * In this case i keep by the example of 974 - remainder as key and as value
+     * - first index of this remainder so we could see if we can use that prefix
+     * sum with the remainder in the answer.
+     */
+    public static class Solution {
+        public boolean checkSubarraySum(int[] a, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, -1);
+            int sum = 0;
+            for (int i = 0; i < a.length; i++) {
+                sum += a[i];
+                int x = k == 0 ? sum : sum % k;
+                if (i - map.getOrDefault(x, i) > 1) return true;
+                if (!map.containsKey(x)) {
+                    map.put(x, i);
+                }
+            }
+            return false;
+        }
+    }
+}
--- problems/algorithms/src/intervals/readme.md
+++ problems/algorithms/src/intervals/readme.md
@@ -1,4 +1,10 @@
-intervals, sweep-line, segment tree
 #### todo
 - https://leetcode.com/problems/video-stitching/
 - https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
+https://leetcode.com/problems/employee-free-time/
+https://leetcode.com/problems/my-calendar-iii/description/
+https://leetcode.com/problems/my-calendar-ii/description/
+https://leetcode.com/problems/my-calendar-i/description/
+https://leetcode.com/problems/insert-interval/description/
+https://leetcode.com/problems/merge-intervals/description/
+https://leetcode.com/discuss/interview-question/367064/Google-or-Phone-Screen-or-Exclusive-Time-of-Processes

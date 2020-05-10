9454d8c3e10b2a7b7b8896e00185813ba72b0bb4
Grigorii <gregsh9.5@gmail.com>
2020-05-10 20:14:25 +0300

    418

---
+++ problems/algorithms/src/string/parseString/SentenceScreenFitting.java
@@ -0,0 +1,41 @@
+package string.parseString;
+
+/**
+ * 418
+ *
+ * ======
+ *
+ * Task.
+ *
+ * Given a rows x cols screen and a sentence represented by a list of non-empty
+ * words, find how many times the given sentence can be fitted on the screen.
+ *
+ * ======
+ *
+ * Source: Leetcode
+ */
+public class SentenceScreenFitting {
+    /**
+     * Adding length of row, trying to see if we land on space, if not we go
+     * back until we find space and start over.
+     */
+    public static class Solution {
+        public int wordsTyping(String[] sentence, int n, int m) {
+            String s = String.join(" ", sentence) + " ";
+            int i = 0;
+            int len = s.length();
+            for (int j = 0; j < n; j++) {
+                i += m;
+                if (s.charAt(i % len) == ' ') {
+                    i++;
+                } else {
+                    while (i > 0 && s.charAt((i - 1) % len) != ' ') {
+                        i--;
+                    }
+                }
+            }
+
+            return i / s.length();
+        }
+    }
+}

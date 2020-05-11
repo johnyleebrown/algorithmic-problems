07bdfac1a91c87fd8be12740fbe5f69957ee7260
Grigorii <gregsh9.5@gmail.com>
2020-05-11 20:36:17 +0300

    1055

--- problems/algorithms/src/twoPointers/slidingWindow/min/ShortestWayToFormString.java
+++ problems/algorithms/src/twoPointers/slidingWindow/min/ShortestWayToFormString.java
@@ -1,43 +1,104 @@
 package twoPointers.slidingWindow.min;
 
+import java.util.Arrays;
+
 /**
  * 1055
  */
 public class ShortestWayToFormString {
-		public static class Solution {
-				public int shortestWay(String source, String target) {
-						int sourceLen = source.length();
-						int targetLen = target.length();
-						int sourcePointer;
-						int targetPointer = 0;
-						int subsequencesCount = 0;
-						boolean foundSameChar = false;
+    /**
+     * We create inverted index to see what position the letter is in after
+     * current position. Then
+     */
+    public static class Solution1 {
+        public int shortestWay(String S, String T) {
+            int sn = S.length();
+            int[][] pos = invert(S);
+            int sInd = 0;
+            int ans = 0;
 
-						while (targetPointer < targetLen) {
-								sourcePointer = 0;
+            for (char c : T.toCharArray()) {
+                // cant find in whole word
+                if (pos[0][c - 'a'] == -1) {
+                    return -1;
+                }
 
-								while (sourcePointer < sourceLen && targetPointer < targetLen) {
-										if (source.charAt(sourcePointer) == target.charAt(targetPointer)) {
-												foundSameChar = true;
-												sourcePointer++;
-												targetPointer++;
-										}
-										else {
-												sourcePointer++;
-										}
-								}
+                int curLetterPosAfterInd = pos[sInd][c - 'a'];
 
-								if (!foundSameChar) {
-										return -1;
-								}
-								else {
-										foundSameChar = false;
-								}
+                // cant find from sInd to end of S
+                if (curLetterPosAfterInd == -1) {
+                    ans++;
 
-								subsequencesCount++;
-						}
+                    // after we couldn't find it from sInd
+                    // we continue from 0, we have to finish search in
+                    // current iteration
+                    sInd = pos[0][c - 'a'] + 1;
+                } else {
+                    sInd = curLetterPosAfterInd + 1;
+                }
 
-						return subsequencesCount;
-				}
-		}
+                if (sInd == sn) {
+                    sInd = 0;
+                    ans++;
+                }
+
+            }
+
+            return sInd == 0 ? ans : ans + 1;
+        }
+
+        private int[][] invert(String s) {
+            int n = s.length();
+            int[][] pos = new int[n][26];
+
+            // fill the last character
+            Arrays.fill(pos[n - 1], -1);
+            pos[n - 1][s.charAt(n - 1) - 'a'] = n - 1;
+
+            for (int i = n - 2; i >= 0; i--) {
+                pos[i] = Arrays.copyOf(pos[i + 1], 26);
+                pos[i][s.charAt(i) - 'a'] = i;
+            }
+
+            return pos;
+        }
+    }
+
+    /**
+     * Regular.
+     */
+    public static class Solution2 {
+        public int shortestWay(String S, String T) {
+            int ans = 1;
+            int prev = 1;
+            char[] s = S.toCharArray();
+            int sn = s.length;
+            int si = 0;
+            char[] t = T.toCharArray();
+            int tn = t.length;
+            int ti = 0;
+
+            while (ti < tn) {
+                if (si >= sn) {
+                    si = 0;
+                    ans++;
+                }
+                if (prev + 2 == ans) {
+                    return -1;
+                }
+
+                if (s[si] != t[ti]) {
+                    while (si < sn && s[si] != t[ti]) {
+                        si++;
+                    }
+                } else {
+                    prev = ans;
+                    si++;
+                    ti++;
+                }
+            }
+
+            return ans;
+        }
+    }
 }

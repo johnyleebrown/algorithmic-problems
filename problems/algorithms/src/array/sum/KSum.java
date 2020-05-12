06aa972aab7c914804776ff87f06352eaa09900b
Grigorii <gregsh9.5@gmail.com>
2020-05-12 19:03:33 +0300

    18

---
+++ problems/algorithms/src/array/sum/KSum.java
@@ -0,0 +1,15 @@
+package array.sum;
+
+import java.util.List;
+
+/**
+ * KSum
+ */
+public class KSum {
+    /**
+     * Calculates unique subsets of size k where sum equals target.
+     */
+    public List<List<Integer>> getUniqueSubsets(int k, int[] nums, int target) {
+
+    }
+}
--- problems/algorithms/src/array/sum/ThreeSum.java
+++ problems/algorithms/src/array/sum/ThreeSum.java
@@ -22,13 +22,13 @@ public class ThreeSum {
                 int left = i + 1;
                 int right = n - 1;
                 while (left < right) {
-                /*
-                cur = 7
-                1 2 3 4 5 6
-                |         |
-                1 2 3 4 5 6
-                ->|     |<-
-                */
+                    /*
+                    cur = 7
+                    1 2 3 4 5 6
+                    |         |
+                    1 2 3 4 5 6
+                    ->|     |<-
+                    */
                     if (nums[left] + nums[right] == cur) {
                         addList(ans, nums[i], nums[left], nums[right]);
                         while (left + 1 < right && nums[left] == nums[left + 1]) {
@@ -40,26 +40,26 @@ public class ThreeSum {
                         left++;
                         right--;
                     }
-                /*
-                cur = 8, sum 1+6 - too small => pick a bigger value
-                1 2 3 4 5 6
-                |         |
-                1 2 3 4 5 6
-                ->|       |
-                */
+                    /*
+                    cur = 8, sum 1+6 - too small => pick a bigger value
+                    1 2 3 4 5 6
+                    |         |
+                    1 2 3 4 5 6
+                    ->|       |
+                    */
                     else if (nums[left] + nums[right] < cur) {
                         while (left + 1 < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
                         left++;
                     }
-                /*
-                cur = 6, sum 1+6 - too big => pick a smaller value
-                1 2 3 4 5 6
-                |         |
-                1 2 3 4 5 6
-                |       |<-
-                */
+                    /*
+                    cur = 6, sum 1+6 - too big => pick a smaller value
+                    1 2 3 4 5 6
+                    |         |
+                    1 2 3 4 5 6
+                    |       |<-
+                    */
                     else {
                         while (right - 1 > left && nums[right] == nums[right - 1]) {
                             right--;
@@ -75,6 +75,63 @@ public class ThreeSum {
             List<Integer> local = new LinkedList<>();
             for (int i : nums) local.add(i);
             ans.add(local);
+        }
+    }
+
+    /**
+     * Template solution. k = 3.
+     */
+    public static class Solution2 {
+        List<List<Integer>> ans = new LinkedList<>();
+        int k = 3;
+
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+            f(0, 3, nums, new LinkedList<>(), 0);
+            return ans;
+        }
+
+        private void f(int start, int n, int[] nums, List<Integer> others, int cur) {
+            if (n == 2) {
+                int left = start;
+                int right = nums.length - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] == cur) {
+                        addList(others, nums[left], nums[right]);
+                        while (left + 1 < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (right - 1 > left && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < cur) {
+                        while (left + 1 < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                    } else {
+                        while (right - 1 > left && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    }
+                }
+            } else {
+                for (int i = start; i < nums.length - n + 1; i++) {
+                    if (i > 0 && nums[i] == nums[i - 1]) continue;
+                    others.add(nums[i]);
+                    f(i + 1, n - 1, nums, others, cur - nums[i]);
+                    others.remove(others.size() - 1);
+                }
+            }
+        }
+
+        private void addList(List<Integer> local, int... nums) {
+            for (int i : nums) local.add(i);
+            ans.add(new LinkedList<>(local));
+            for (int i : nums) local.remove(local.size() - 1);
         }
     }
 }

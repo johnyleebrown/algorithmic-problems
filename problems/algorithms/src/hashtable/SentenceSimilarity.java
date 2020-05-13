51954dfcc30602faddb18ed8a2b504f66242f4a6
Grigorii <gregsh9.5@gmail.com>
2020-05-13 10:58:04 +0300

    734

--- problems/algorithms/src/hashtable/SentenceSimilarity.java
+++ problems/algorithms/src/hashtable/SentenceSimilarity.java
@@ -1,39 +1,29 @@
 package hashtable;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 734
  */
-public class SentenceSimilarity
-{
-	class Solution
-	{
-		public boolean areSentencesSimilar(String[] words1, String[] words2,
-		                                   List<List<String>> pairs)
-		{
-			if (words1.length != words2.length)
-			{
-				return false;
+public class SentenceSimilarity {
+	public static class Solution {
+		public boolean areSentencesSimilar(String[] w1, String[] w2, List<List<String>> pairs) {
+			if (w1.length != w2.length) return false;
+			Map<String, Set<String>> m = new HashMap<>();
+			for (List<String> p : pairs) {
+				m.putIfAbsent(p.get(0), new HashSet<>());
+				m.get(p.get(0)).add(p.get(1));
+				m.putIfAbsent(p.get(1), new HashSet<>());
+				m.get(p.get(1)).add(p.get(0));
 			}
-
-			Set<String> pairset = new HashSet();
-			for (List<String> pair : pairs)
-			{
-				pairset.add(pair.get(0) + "#" + pair.get(1));
-			}
-
-			for (int i = 0; i < words1.length; ++i)
-			{
-				if (!words1[i].equals(words2[i]) &&
-						!pairset.contains(words1[i] + "#" + words2[i]) &&
-						!pairset.contains(words2[i] + "#" + words1[i]))
-
+			for (int i = 0; i < w1.length; i++) {
+				if (w1[i].equals(w2[i])) {
+					continue;
+				}
+				if ((!m.containsKey(w1[i]) || !m.get(w1[i]).contains(w2[i])) || (!m.containsKey(w2[i]) || !m.get(w2[i]).contains(w1[i]))) {
 					return false;
+				}
 			}
-
 			return true;
 		}
 	}

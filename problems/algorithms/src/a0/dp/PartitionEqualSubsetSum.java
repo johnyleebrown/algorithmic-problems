6a26aa2957b2bebc8e5ce9175907c6b133e21537
Grigorii <gregsh9.5@gmail.com>
2020-05-13 21:12:34 +0300

    416

--- problems/algorithms/src/a0/dp/PartitionEqualSubsetSum.java
+++ problems/algorithms/src/a0/dp/PartitionEqualSubsetSum.java
@@ -1,35 +1,34 @@
 package a0.dp;
 
+import java.util.Arrays;
+
 /**
  * 416
  */
 public class PartitionEqualSubsetSum {
-	public static boolean Solution(int[] set) {
-		int sum = 0;
-		for (int num : set) {
-			sum += num;
-		}
-		if (sum % 2 != 0) {
-			return false;
-		}
-		sum /= 2;
-
-		boolean[][] temp = new boolean[set.length + 1][sum + 1];
-		for (int i = 0; i <= set.length; i++) {
-			temp[i][0] = true;
-		}
-
-		for (int i = 1; i <= set.length; i++) {
-			for (int j = 1; j <= sum; j++) {
-				if (set[i - 1] > j) {
-					temp[i][j] = temp[i - 1][j];
-				}
-				else {
-					temp[i][j] = temp[i - 1][j] || temp[i - 1][j - set[i - 1]];
-				}
+	/**
+	 * Either we add i or not.
+	 */
+	public static class Solution {
+		public boolean canPartition(int[] ar) {
+			int sum = 0;
+			int max = ar[0];
+			for (int i : ar) {
+				max = Math.max(max, i);
+				sum += i;
 			}
+			if (sum % 2 == 1) return false;
+			int target = sum / 2;
+			if (max > target) return false;
+			Arrays.sort(ar); // we go from right to faster gain sum
+			return dfs(ar.length - 1, 0, target, ar);
 		}
 
-		return temp[set.length][sum];
+		private boolean dfs(int i, int sum, int target, int[] ar) {
+			if (i < 0 || sum > target) return false;
+			if (sum == target) return true;
+			return dfs(i - 1, sum + ar[i], target, ar)
+					|| dfs(i - 1, sum, target, ar);
+		}
 	}
 }

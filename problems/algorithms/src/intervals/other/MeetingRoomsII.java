3e5beb13e2470ced77b17c552f979c73ed66cb26
Grigorii <gregsh9.5@gmail.com>
2020-06-28 11:26:57 +0300

    253

--- problems/algorithms/src/intervals/other/MeetingRoomsII.java
+++ problems/algorithms/src/intervals/other/MeetingRoomsII.java
@@ -1,13 +1,39 @@
 package intervals.other;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 /**
  * 253
  */
 public class MeetingRoomsII {
-    class Solution {
+    /**
+     * Priority queue for ends of intervals. We check every time if current
+     * interval is not overlapping with peek interval. If not - we don't need to
+     * keep this interval anymore.
+     */
+    public static class Solution2 {
+        public int minMeetingRooms(int[][] ins) {
+            if (ins.length == 0) return 0;
+            Arrays.sort(ins, (a, b) -> a[0] - b[0]);
+            PriorityQueue<Integer> ends = new PriorityQueue<>();
+            ends.add(ins[0][1]);
+            for (int i = 1; i < ins.length; i++) {
+                if (ins[i][0] >= ends.peek()) {
+                    ends.poll();
+                }
+                ends.add(ins[i][1]);
+            }
+            return ends.size();
+        }
+    }
+
+    /**
+     * We add +1 for start of interval and -1 for end of interval.
+     */
+    public static class Solution1 {
         public int minMeetingRooms(int[][] intervals) {
             Map<Integer, Integer> map = new TreeMap<>();
             for (int[] i : intervals) {
@@ -18,7 +44,8 @@ public class MeetingRoomsII {
             int maxRooms = 0;
             int currentRoomCount = 0;
             for (int c : map.values()) {
-                maxRooms = Math.max(maxRooms, currentRoomCount += c);
+                currentRoomCount += c;
+                maxRooms = Math.max(maxRooms, currentRoomCount);
             }
 
             return maxRooms;

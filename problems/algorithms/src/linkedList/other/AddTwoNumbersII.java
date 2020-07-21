ca0603f513a036092e8a817549d5f32407945102
Grigorii <gregsh9.5@gmail.com>
2020-07-21 22:12:54 +0300

    445

--- problems/algorithms/src/linkedList/other/AddTwoNumbersII.java
+++ problems/algorithms/src/linkedList/other/AddTwoNumbersII.java
@@ -5,61 +5,87 @@ import util.ds.ListNode;
 /**
  * 445
  */
-public class AddTwoNumbersII
-{
-	class Solution
-	{
-		private int leftOver = 0;
+public class AddTwoNumbersII {
 
-		public ListNode addTwoNumbers(ListNode l1, ListNode l2)
-		{
-			int c1 = getLen(l1), c2 = getLen(l2);
-			if (c1 == 0) return l2;
-			if (c2 == 0) return l1;
-			ListNode ans = sum1(c1 > c2 ? l1 : l2, c1 > c2 ? l2 : l1, Math.abs(c1 - c2));
-			if (leftOver != 0)
-			{
-				ListNode head = new ListNode(1);
-				head.next = ans;
-				return head;
+	/**
+	 * - Get lengths of lists to know the offset to align heads
+	 * - From ends do sum op, leftover can be calculated after the new node is returned
+	 */
+	public static class Solution1 {
+		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			int len1 = calcLength(l1), len2 = calcLength(l2);
+			int d = Math.abs(len1 - len2);
+			// sending in 3rd param a longer list of 2
+			return len1 > len2 ? h(d, d, l1, l2) : h(d, d, l2, l1);
+		}
+
+		/**
+		 * Assuming 3rd param is a bigger list
+		 */
+		ListNode h(int base, int offset, ListNode l1, ListNode l2) {
+
+			if (l1 == null) return null;
+
+			ListNode next = h(base, offset - 1, l1.next, offset <= 0 ? l2.next : l2);
+
+			// calc leftover from new next node
+			int leftover = 0;
+			if (next != null && next.val > 9) {
+				leftover = 1;
+				next.val %= 10;
 			}
-			return ans;
+
+			int smaller = offset > 0 ? 0 : l2.val;
+			int val = l1.val + smaller + leftover;
+			ListNode ret = new ListNode(val);
+			ret.next = next;
+
+			if (base == offset && ret.val > 9) {
+				ListNode extra = new ListNode(1);
+				ret.val %= 10;
+				extra.next = ret;
+				return extra;
+			}
+
+			return ret;
 		}
 
-		// for parts where we have either l1 or l2 elements
-		private ListNode sum1(ListNode lx, ListNode lother, int c)
-		{
-			if (lx == null) return null;
-			if (c == 0) return sum2(lx, lother);
-			ListNode y = sum1(lx.next, lother, c - 1);
-			int x = lx.val + leftOver;
-			leftOver = x / 10;
-			ListNode head = new ListNode(x % 10);
-			head.next = y;
-			return head;
+		int calcLength(ListNode l) {
+			int ret = 0;
+			while (l != null) {
+				ret++;
+				l = l.next;
+			}
+			return ret;
 		}
+	}
 
-		// for parts where we have l1 and l2 elements
-		private ListNode sum2(ListNode l1, ListNode l2)
-		{
-			if (l1 == null && l2 == null) return null;
-			ListNode y = sum2(l1.next, l2.next);
-			int x = l1.val + l2.val + leftOver;
-			leftOver = x / 10;
-			ListNode head = new ListNode(x % 10);
-			head.next = y;
-			return head;
-		}
-
-		private int getLen(ListNode l1)
-		{
-			int c = 0;
-			while (l1 != null)
-			{
-				c++;
+	/**
+	 * For small numbers.
+	 */
+	public static class Solution2 {
+		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			long i = 0;
+			while (l1 != null) {
+				i = i * 10 + l1.val;
 				l1 = l1.next;
 			}
-			return c;
+			long j = 0;
+			while (l2 != null) {
+				j = j * 10 + l2.val;
+				l2 = l2.next;
+			}
+			long res = i + j;
+			if (res == 0) return new ListNode(0);
+			ListNode prev = null;
+			while (res != 0) {
+				long x = res % 10;
+				ListNode n = new ListNode((int) x);
+				n.next = prev;
+				prev = n;
+				res /= 10;
+			}
+			return prev;
 		}
 	}
 }

93df8732da7716ea1b9dd64a4880ebaaf4ff41c6
Grigorii <gregsh9.5@gmail.com>
2020-07-27 20:17:18 +0300

    200

--- problems/algorithms/src/graph/dfs/NumberOfIslands.java
+++ problems/algorithms/src/graph/dfs/NumberOfIslands.java
@@ -4,42 +4,41 @@ package graph.dfs;
  * 200
  */
 public class NumberOfIslands {
-    public class Solution {
-        private int n, m;
-        private int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+	public static class Solution {
+		private int n, m;
+		private int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-        public int numIslands(char[][] grid) {
-            int count = 0;
-            n = grid.length;
+		public int numIslands(char[][] ar) {
+			n = ar.length;
+			if (n == 0) return 0;
+			m = ar[0].length;
+			if (m == 0) return 0;
+			boolean[][] seen = new boolean[n][m];
+			int c = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (dfs(i, j, seen, ar)) {
+						c++;
+					}
+				}
+			}
+			return c;
+		}
 
-            if (n == 0) {
-                return 0;
-            }
+		private boolean dfs(int i, int j, boolean[][] seen, char[][] ar) {
+			if (!isValid(i, j) || seen[i][j] || ar[i][j] != '1') {
+				return false;
+			}
+			seen[i][j] = true;
+			for (int[] d : dirs) {
+				dfs(i + d[0], j + d[1], seen, ar);
+			}
+			return true;
+		}
 
-            m = grid[0].length;
-
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++) {
-                    if (grid[i][j] == '1') {
-                        dfs(grid, i, j);
-                        count++;
-                    }
-                }
-
-            return count;
-        }
-
-        private void dfs(char[][] grid, int i, int j) {
-            if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') {
-                return;
-            }
-
-            grid[i][j] = '0';
-
-            for (int[] dir : dirs) {
-                dfs(grid, i + dir[0], j + dir[1]);
-            }
-        }
-    }
+		private boolean isValid(int i, int j) {
+			return i >= 0 && j >= 0 && i < n && j < m;
+		}
+	}
 }
 

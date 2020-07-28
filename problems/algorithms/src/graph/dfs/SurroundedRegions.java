90527c2dd6761197fb2d9260b9095bdd62c121ce
Grigorii <gregsh9.5@gmail.com>
2020-07-28 20:41:58 +0300

    130

--- problems/algorithms/src/graph/dfs/SurroundedRegions.java
+++ problems/algorithms/src/graph/dfs/SurroundedRegions.java
@@ -4,59 +4,51 @@ package graph.dfs;
  * 130
  */
 public class SurroundedRegions {
-    class Solution {
-        private int[][] pattern = new int[][]{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+	/**
+	 * Color recursively O connected to the border to 3rd color(i.e. '*'), then just color O->X
+	 * and *->O.
+	 */
+	public static class Solution {
+		private static final int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-        public void solve(char[][] board) {
-            if (board.length == 0 || board[0].length == 0) {
-                return;
-            }
+		public void solve(char[][] ar) {
+			int n = ar.length;
+			if (n == 0) {
+				return;
+			}
+			int m = ar[0].length;
+			if (m == 0) {
+				return;
+			}
 
-            int n = board.length, m = board[0].length;
-            boolean[][] seen = new boolean[n][m];
+			for (int j = 0; j < m; j++) {
+				dfs(0, j, ar, n, m); // top
+				dfs(n - 1, j, ar, n, m); // bottom
+			}
+			for (int i = 1; i < n; i++) {
+				dfs(i, m - 1, ar, n, m); // right
+				dfs(i, 0, ar, n, m); // left
+			}
 
-            for (int j = 0; j < m; j++) {
-                // top
-                setThirdColor(0, j, board, seen, n, m);
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (ar[i][j] == 'O') {
+						ar[i][j] = 'X';
+					} else if (ar[i][j] == '*') {
+						ar[i][j] = 'O';
+					}
+				}
+			}
+		}
 
-                // bottom
-                setThirdColor(n - 1, j, board, seen, n, m);
-            }
-            for (int i = 1; i < n; i++) {
-                // right
-                setThirdColor(i, m - 1, board, seen, n, m);
-
-                // left
-                setThirdColor(i, 0, board, seen, n, m);
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (board[i][j] == 'O') {
-                        board[i][j] = 'X';
-                    } else if (board[i][j] == '*') {
-                        board[i][j] = 'O';
-                    }
-                }
-            }
-        }
-
-        private void setThirdColor(int i, int j, char[][] board, boolean[][] seen, int n, int m) {
-            if (i < 0 || j < 0 || i >= n || j >= m) {
-                return;
-            }
-
-            if (board[i][j] == 'O' && !seen[i][j]) {
-                board[i][j] = '*';
-                dfs(i, j, board, seen, n, m);
-            }
-        }
-
-        private void dfs(int old_i, int old_j, char[][] board, boolean[][] seen, int n, int m) {
-            for (int[] cell : pattern) {
-                int i = old_i + cell[0], j = old_j + cell[1];
-                setThirdColor(i, j, board, seen, n, m);
-            }
-        }
-    }
+		private void dfs(int i, int j, char[][] ar, int n, int m) {
+			if (i < 0 || j < 0 || i >= n || j >= m || ar[i][j] != 'O') {
+				return;
+			}
+			ar[i][j] = '*';
+			for (int[] cell : dirs) {
+				dfs(i + cell[0], j + cell[1], ar, n, m);
+			}
+		}
+	}
 }

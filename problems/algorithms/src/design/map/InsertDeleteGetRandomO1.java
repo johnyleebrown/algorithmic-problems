14ba71fac6dff47431c592f6694414e30bb79656
Grigorii <gregsh9.5@gmail.com>
2020-08-23 16:00:58 +0300

    380

--- problems/algorithms/src/design/map/InsertDeleteGetRandomO1.java
+++ problems/algorithms/src/design/map/InsertDeleteGetRandomO1.java
@@ -22,43 +22,52 @@ import java.util.*;
  * Source: Leetcode
  */
 public class InsertDeleteGetRandomO1 {
-    /**
-     * Use map to keep value and index in list. Remove is O(1). Use list to rand
-     * val in O(1). Removing value from list can tale up to O(n) if it is not
-     * the last one. We remove tha last one and set it to index of val in list
-     * we want to remove.
-     */
-    class Solution {
-        class RandomizedSet {
-            Map<Integer, Integer> m = new HashMap<>();//val and ind in l
-            List<Integer> l = new ArrayList<>();
-            Random r = new Random();
+	/**
+	 * Use map to keep value and index in list. Remove is O(1). Use list to rand
+	 * val in O(1). Removing value from list can tale up to O(n) if it is not
+	 * the last one. We remove tha last one and set it to index of val in list
+	 * we want to remove.
+	 */
+	public static class Solution {
+		public static class RandomizedSet {
 
-            public RandomizedSet() {
-            }
+			Random r;
+			List<Integer> l; // vals
+			Map<Integer, Integer> m; // val-pos
 
-            public boolean insert(int val) {
-                if (m.containsKey(val)) return false;
-                l.add(val);
-                m.put(val, l.size() - 1);
-                return true;
-            }
+			public RandomizedSet() {
+				r = new Random();
+				l = new ArrayList<>();
+				m = new HashMap<>();
+			}
 
-            public boolean remove(int val) {
-                if (!m.containsKey(val)) return false;
-                int ind = m.remove(val);
-                int last = l.get(l.size() - 1);
-                l.remove(l.size() - 1);
-                if (ind != l.size()) {
-                    m.put(last, ind);
-                    l.set(ind, last);
-                }
-                return true;
-            }
+			public boolean insert(int val) {
+				if (m.containsKey(val)) {
+					return false;
+				}
+				m.put(val, l.size());
+				l.add(val);
+				return true;
+			}
 
-            public int getRandom() {
-                return l.get(r.nextInt(l.size()));
-            }
-        }
-    }
+			public boolean remove(int val) {
+				if (!m.containsKey(val)) {
+					return false;
+				}
+				int pos = m.get(val);
+				int val2 = l.get(l.size() - 1);
+				m.remove(val);
+				l.remove(l.size() - 1);
+				if (pos != l.size()) {
+					l.set(pos, val2);
+					m.put(val2, pos);
+				}
+				return true;
+			}
+
+			public int getRandom() {
+				return l.get(r.nextInt(l.size()));
+			}
+		}
+	}
 }

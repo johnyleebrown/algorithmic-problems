d7fa193d7ed5b579573044af102fba6bc7db62d9
Grigorii <gregsh9.5@gmail.com>
2020-08-28 11:31:31 +0300

    91

--- src/main/java/dp/linear/back/DecodeWays.java
+++ src/main/java/dp/linear/back/DecodeWays.java
@@ -4,22 +4,63 @@ package dp.linear.back;
  * 91
  */
 public class DecodeWays {
-    public static class Solution {
-        public static int solution(String s) {
-            int n = s.length();
-            if (n == 0) return 0;
-            if (n == 1 && s.charAt(0) != '0') return 1;
-            if (s.charAt(0) == '0') return 0;
-            int[] dp = new int[n + 1];
-            dp[0] = 1;
-            dp[1] = s.charAt(1) == '0' ? 0 : 1;
-            for (int i = 2; i <= n; i++) {
-                if (i < n && s.charAt(i) == '0') continue;
-                int x = Integer.parseInt(s.substring(i - 2, i)) < 27 ? dp[i - 2] : 0;
-                // either we add nothing, or we add a[i-2] if the last two digits form a letter
-                dp[i] = dp[i - 1] + x;
-            }
-            return dp[n];
-        }
-    }
+	/**
+	 * State transition: dp[i]=dp[i-2]+dp[i-1]
+	 */
+	public static class Solution {
+		public int numDecodings(String s) {
+			int n = s.length();
+			int[] dp = new int[n + 1];
+			dp[0] = 1;
+			dp[1] = s.charAt(0) == '0' ? 0 : 1;
+			for (int i = 2; i <= n; i++) {
+				boolean isNumber = isSubstringANumber(s, i - 2, i - 1);
+				if (isNumber) {
+					dp[i] += dp[i - 2];
+				}
+				if (s.charAt(i - 1) != '0') {
+					dp[i] += dp[i - 1];
+				}
+			}
+			return dp[n];
+		}
+
+		private boolean isSubstringANumber(String s, int i, int j) {
+			if (s.charAt(i) == '0') return false;
+			return Integer.parseInt(String.valueOf(s.charAt(i)) + s.charAt(j)) < 27;
+		}
+	}
+
+	/**
+	 * Longer version of s1.
+	 */
+	public static class Solution2 {
+		public int numDecodings(String s) {
+			int n = s.length();
+			if (s.charAt(0) == '0') return 0;
+			int[] dp = new int[n + 1];
+			dp[0] = 1;
+			dp[1] = 1;
+			for (int i = 2; i <= n; i++) {
+				boolean isNumber = isSubstringANumber(s, i - 2, i - 1);
+				if (!isNumber && s.charAt(i - 1) == '0') {
+					return 0;
+				}
+				if (isNumber) {
+					if (s.charAt(i - 1) == '0')
+						dp[i] = dp[i - 2];
+					else
+						dp[i] = dp[i - 1] + dp[i - 2];
+				} else {
+					dp[i] = dp[i - 1];
+				}
+			}
+			return dp[n];
+		}
+
+		private boolean isSubstringANumber(String s, int i, int j) {
+			if (s.charAt(i) == '0') return false;
+			return Integer.parseInt(String.valueOf(s.charAt(i)) + String.valueOf(s.charAt(j))) < 27;
+		}
+	}
 }

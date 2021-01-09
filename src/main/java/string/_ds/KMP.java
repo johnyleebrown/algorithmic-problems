6bcb57eabb2f76dfc1f64b6543b8943e33a78391
Grigorii <gregsh9.5@gmail.com>
2021-01-09 17:47:58 +0800

    KMP

--- src/main/java/string/_ds/KMP.java
+++ src/main/java/string/_ds/KMP.java
@@ -4,62 +4,68 @@ package string._ds;
  * This implementation uses a version of the Knuth-Morris-Pratt substring search
  * algorithm.
  *
- * http://e-maxx.ru/algo/prefix_function
- * https://habr.com/en/post/191454/
- * https://algoprog.ru/material/25514
+ * Explanation:
  *
- * problems:
+ * https://www.youtube.com/watch?v=bH45DuZjBWw&list=PLrS21S1jm43ie9vkDOu3zZqlTtPd1pd0t
+ *
+ * Problems:
  *
  * https://leetcode.com/problems/find-all-good-strings/
  * https://leetcode.com/problems/shortest-palindrome/
  * https://leetcode.com/problems/longest-happy-prefix/
  */
 public class KMP {
-    private final int R;       // the radix
-    private int[][] dfa;       // the KMP automoton
-
-    private char[] pattern;    // either the character array for the pattern
-    private String pat;        // or the pattern string
 
     /**
-     * Preprocesses the pattern string.
+     * Prefix array is an array of numbers where each number corresponds to the prefix number for
+     * the substring ending at that position. Prefix number is the length of the largest
+     * substring that is both a prefix and a suffix of a string.
+     * For "ababbaba" the largest substring that is a prefix and a suffix is "aba".
+     * For "ab" it is "ab".
+     * For "a" it is "a".
+     * For "ababb" it is empty.
+     * @param pattern pattern that we want to search for in text
+     * @return the prefix array
      */
-    public KMP(String pattern) {
-        this.R = 256;
-        this.pat = pattern;
+    public int[] getPrefixArray(String pattern) {
+        int[] p = new int[pattern.length() + 1]; // prefix array
+        p[0] = -1; // default value
 
-        // build DFA from pattern
-        int m = pattern.length();
-        dfa = new int[R][m];
-        dfa[pattern.charAt(0)][0] = 1;
-        for (int x = 0, j = 1; j < m; j++) {
-            for (int c = 0; c < R; c++)
-                dfa[c][j] = dfa[c][x];     // Copy mismatch cases.
-            dfa[pattern.charAt(j)][j] = j + 1;   // Set match case.
-            x = dfa[pattern.charAt(j)][x];     // Update restart state.
+        for (int i = 1; i <= pattern.length(); i++) {
+            int k = p[i - 1];
+            while (k >= 0) {
+                if (pattern.charAt(i - 1) == pattern.charAt(k)) {
+                    break;
+                }
+                k = p[k];
+            }
+            p[i] = k + 1;
         }
+
+        return p;
     }
 
     /**
-     * Returns the index of the first occurrrence of the pattern string in the
-     * text string.
-     *
-     * Simulate operation of DFA on text.
+     * @param p prefix array from the method {@link #getPrefixArray(String)}
+     * @param pattern pattern that we are searching for in text
+     * @param text the text that the pattern is being searched in
+     * @return -1 if the pattern was not found in text, otherwise return the index of the last
+     * matched letter from pattern in text
      */
-    public int search(String text) {
-        int m = pat.length();
-        int n = text.length();
-        int i;
-        int j;
-
-        for (i = 0, j = 0; i < n && j < m; i++)
-            j = dfa[text.charAt(i)][j];
-
-        // found
-        if (j == m)
-            return i - m;
-
-        // not found
-        return n;
+    public int searchText(int[] p, String pattern, String text) {
+        int k = -1;
+        for (int i = 0; i < text.length(); i++) {
+            while (k >= 0) {
+                if (text.charAt(i) == pattern.charAt(k)) {
+                    break;
+                }
+                k = p[k];
+            }
+            k++;
+            if (k == pattern.length()) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/main/java/util/utils/Generator.java
+++ src/main/java/util/utils/Generator.java
@@ -74,4 +74,18 @@ public class Generator {
 			return a;
 		}
 	}
+
+	public static String createRandomReadableString(int size) {
+		return createRandomReadableStringBuilder(size).toString();
+	}
+
+	public static StringBuilder createRandomReadableStringBuilder(int size) {
+		Random r = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			// adding 33 cuz first 33 are not visible
+			sb.append((char) (r.nextInt(255 - 33) + 33));
+		}
+		return sb;
+	}
 }

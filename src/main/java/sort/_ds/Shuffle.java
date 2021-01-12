663e098902141dc7d68251accc3207aecef0f6b5
Grigorii <gregsh9.5@gmail.com>
2021-01-13 04:04:50 +0800

    681

--- src/main/java/sort/_ds/Shuffle.java
+++ src/main/java/sort/_ds/Shuffle.java
@@ -5,10 +5,8 @@ package sort._ds;
  * elements in uniformly random order, under the assumption that Math.random()
  * generates independent and uniformly distributed numbers between 0 and 1.
  */
-public class Shuffle
-{
-	public static void shuffleArray(Object[] a)
-	{
+public class Shuffle {
+	public static void shuffleArray(Object[] a) {
 		int n = a.length;
 		for (int i = 0; i < n; i++) {
 			// choose index uniformly in [0, i]
@@ -19,8 +17,7 @@ public class Shuffle
 		}
 	}
 
-	public static void shuffleIntArray(int[] a)
-	{
+	public static void shuffleIntArray(int[] a) {
 		int n = a.length;
 		for (int i = 0; i < n; i++) {
 			// choose index uniformly in [0, i]

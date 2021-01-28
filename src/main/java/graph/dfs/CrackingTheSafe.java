cf05cf8db0e5653d714058c94fcc9513755dd365
Grigorii <gregsh9.5@gmail.com>
2021-01-29 01:22:54 +0800

    753

--- src/main/java/graph/dfs/CrackingTheSafe.java
+++ src/main/java/graph/dfs/CrackingTheSafe.java
@@ -1,9 +1,6 @@
 package graph.dfs;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 753. Cracking the Safe
@@ -47,10 +44,23 @@ import java.util.Set;
  * https://leetcode.com/problems/cracking-the-safe/
  */
 public class CrackingTheSafe {
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		s.crackSafe(3, 9);
-	}
+	/*
+	TODO
+	Guaranteed to open the box - permutation of all combinations
+	How to make a shortest continuous string - combine the permutations
+	All permutations of k^n has an overlap of n-1 (000 and 001 overlap 2 numbers = 00)
+	So we need an overlap of minimum length which is basically n numbers of first permutation
+	and + 1 last number of each permutation, with the size n + k^n - 1
+	We can create a graph where each node is a permutation, nodes are connected if they have an
+	overlap of n-1 (the one we need)
+	In other words an overlap is when a suffix of one string equals to the prefix of another, of
+	size n-1 in our case
+	After that what we need to do is to find such path in a graph that will result in the answer
+	This path should go through each node once because we cant stop at node 111 and continue with
+	 000, these nodes are not overlapping, so we wont have a minimum password
+	Such path that goes through each node once is called Hamiltonian path, it is an NP complete
+	problem but one of the solutions is a brute force by doing dfs
+	 */
 
 	/**
 	 * @formatter:off
@@ -123,6 +133,7 @@ public class CrackingTheSafe {
 
 			// can't go further
 			if (count == 0 && vertexCount != 0) {
+				System.out.println(cur);
 				ans.deleteCharAt(ans.length() - 1);
 				seen.remove(cur);
 				vertexCount++;
@@ -144,4 +155,90 @@ public class CrackingTheSafe {
 			}
 		}
 	}
+
+	/**
+	 * Cleaner
+	 */
+	public static class Solution2 {
+
+		public String crackSafe(int n, int k) {
+			if (n == 1) {
+				return computeForK(k);
+			}
+
+			Map<String, String> combToSuffix = new HashMap<>();
+			Map<String, List<String>> prefixToCombs = new HashMap<>();
+			gen(0, k, n, new char[n], combToSuffix, prefixToCombs);
+
+			StringBuilder ans = new StringBuilder();
+			String first = combToSuffix.entrySet().iterator().next().getKey();
+			dfs(first, new HashSet<>(), combToSuffix.size(), ans, combToSuffix, prefixToCombs);
+
+			return ans.toString();
+		}
+
+		private String computeForK(int k) {
+			char[] c = new char[k];
+			for (int i = 0; i < k; i++) c[i] = (char) (i + '0');
+			return String.valueOf(c);
+		}
+
+		private void gen(int j, int k, int n, char[] comb,
+		                 Map<String, String> combToSuffix,
+		                 Map<String, List<String>> prefixToCombs) {
+			if (j == n) {
+				String c = String.valueOf(comb);
+				String suffix = String.valueOf(comb, 1, n - 1);
+				String prefix = String.valueOf(comb, 0, n - 1);
+
+				combToSuffix.put(c, suffix);
+				prefixToCombs.putIfAbsent(prefix, new ArrayList<>());
+				prefixToCombs.get(prefix).add(c);
+			} else {
+				for (int i = 0; i < k; i++) {
+					comb[j] = (char) (i + '0');
+					gen(j + 1, k, n, comb, combToSuffix, prefixToCombs);
+				}
+			}
+		}
+
+		private boolean dfs(String cur, Set<String> seen, int vertexCount, StringBuilder ans,
+		                    Map<String, String> combToSuffix,
+		                    Map<String, List<String>> prefixToCombs) {
+
+			updateAnswer(ans, cur);
+			seen.add(cur);
+
+			if (vertexCount == 1) {
+				return true;
+			}
+
+			String suf = combToSuffix.get(cur);
+
+			for (String comb : prefixToCombs.get(suf)) {
+				if (!seen.contains(comb)) {
+					if (dfs(comb, seen, vertexCount - 1, ans, combToSuffix, prefixToCombs)) {
+						return true;
+					}
+				}
+			}
+
+			seen.remove(cur);
+			ans.deleteCharAt(ans.length() - 1);
+
+			return false;
+		}
+
+		private void updateAnswer(StringBuilder ans, String cur) {
+			// first time = fill n symbols
+			if (ans.length() == 0) {
+				for (int i = 0; i < cur.length(); i++) ans.append(cur.charAt(i));
+			}
+			// all other times just take the last symbol
+			else {
+				ans.append(cur.charAt(cur.length() - 1));
+			}
+		}
+	}
+
 }

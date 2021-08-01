98efff7f88dec45f8bbd10dcbbeeb827d9c2a756
Grigorii <gregsh9.5@gmail.com>
2021-08-02 00:59:23 +0300

    487

--- src/main/java/twoPointers/slidingWindow/max/MaxConsecutiveOnesII.java
+++ src/main/java/twoPointers/slidingWindow/max/MaxConsecutiveOnesII.java
@@ -1,32 +1,58 @@
 package twoPointers.slidingWindow.max;
 
 /**
- * 487
+ * 487. Max Consecutive Ones II
+ *
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array if you can
+ * flip at most one 0.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,0,1,1,0]
+ * Output: 4
+ * Explanation: Flip the first zero will get the maximum number of consecutive 1s. After flipping,
+ * the maximum number of consecutive 1s is 4.
+ * Example 2:
+ *
+ * Input: nums = [1,0,1,1,0,1]
+ * Output: 4
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * nums[i] is either 0 or 1.
+ *
+ *
+ * Follow up: What if the input numbers come in one by one as an infinite stream? In other words,
+ * you can't store all numbers coming from the stream as it's too large to hold in memory. Could you
+ * solve it efficiently?
+ *
+ * https://leetcode.com/problems/max-consecutive-ones-ii/
  */
 public class MaxConsecutiveOnesII {
 	static class Solution {
 		public int findMaxConsecutiveOnes(int[] nums) {
-			int start = 0;
-			int end = 0;
+			int l = 0;
 			int zeros = 0;
-			int maxLen = Integer.MIN_VALUE;
+			int max = 0;
 
-			while (end < nums.length) {
-				if (nums[end] == 0) {
+			for (int r = 0; r < nums.length; r++) {
+				if (nums[r] == 0) {
 					zeros++;
 				}
 
-				if (zeros > 1) {
-					if (nums[start++] == 0) {
+				while (zeros == 2) {
+					if (nums[l] == 0) {
 						zeros--;
 					}
+					l++;
 				}
 
-				maxLen = Math.max(maxLen, end - start + 1);
-				end++;
+				max = Math.max(max, r - l + 1);
 			}
 
-			return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
+			return max;
 		}
 	}
 }

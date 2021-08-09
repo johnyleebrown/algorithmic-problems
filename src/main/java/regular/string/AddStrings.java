3ed396fe45f7094f0e9f36bd43c5b972b9ce6cdc
Grigorii <gregsh9.5@gmail.com>
2021-08-09 12:23:52 +0300

    415

--- src/main/java/regular/string/AddStrings.java
+++ src/main/java/regular/string/AddStrings.java
@@ -1,19 +1,31 @@
 package regular.string;
 
 /**
- * 415
+ * 415. Add Strings https://leetcode.com/problems/add-strings/
  */
 public class AddStrings {
 	public static class Solution {
 		public String addStrings(String num1, String num2) {
-			StringBuilder sb = new StringBuilder();
-			int carry = 0;
-			for (int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--) {
-				int x = i < 0 ? 0 : num1.charAt(i) - '0';
-				int y = j < 0 ? 0 : num2.charAt(j) - '0';
-				sb.append((x + y + carry) % 10);
-				carry = (x + y + carry) / 10;
+			if (num1.length() == 1 && num1.charAt(0) == '0') {
+				return num2;
 			}
+			if (num2.length() == 1 && num2.charAt(0) == '0') {
+				return num1;
+			}
+			StringBuilder sb = new StringBuilder();
+			int i = num1.length() - 1, j = num2.length() - 1;
+			int leftover = 0;
+			while (i >= 0 || j >= 0) {
+				int iValue = i < 0 ? 0 : num1.charAt(i) - '0';
+				int jValue = j < 0 ? 0 : num2.charAt(j) - '0';
+				int sum = iValue + jValue + leftover;
+				leftover = sum / 10;
+				int res = sum % 10;
+				sb.append(res);
+				i--;
+				j--;
+			}
+			if (leftover == 1) sb.append(leftover);
 			return sb.reverse().toString();
 		}
 	}

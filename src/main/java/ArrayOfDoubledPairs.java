b4a894f7c55f0a30c8b4331ed1578a76f3a3eea2
Grigorii <gregsh9.5@gmail.com>
2021-08-12 09:33:43 +0300

    954

--- src/main/java/ArrayOfDoubledPairs.java
+++ src/main/java/ArrayOfDoubledPairs.java
@@ -30,4 +30,71 @@ public class ArrayOfDoubledPairs {
 			return c == 0;
 		}
 	}
+
+	/**
+	 * Count pairs from the minimum
+	 */
+	public static class Solution2 {
+		public boolean canReorderDoubled(int[] arr) {
+			int[] counts = new int[1000001];
+			int[] countsNegative = new int[1000001];
+			int min = Integer.MAX_VALUE;
+			int minNegative = Integer.MAX_VALUE;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] >= 0) {
+					counts[arr[i]]++;
+					min = Math.min(min, arr[i]);
+				} else {
+					int x = Math.abs(arr[i]);
+					countsNegative[x]++;
+					minNegative = Math.min(minNegative, x);
+				}
+			}
+			for (int i = 0; i < counts.length; i++) {
+				while (--counts[i] >= 0) {
+					int x = i * 2;
+					if (counts[x] <= 0) return false;
+					counts[x]--;
+				}
+				while (--countsNegative[i] >= 0) {
+					int x = i * 2;
+					if (countsNegative[x] <= 0) return false;
+					countsNegative[x]--;
+				}
+			}
+			return true;
+		}
+	}
+
+	public static class Solution3 {
+		public boolean canReorderDoubled(int[] arr) {
+			int[] counts = new int[1000001];
+			int[] countsNegative = new int[1000001];
+			int min = Integer.MAX_VALUE;
+			int minNegative = Integer.MAX_VALUE;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] >= 0) {
+					counts[arr[i]]++;
+					min = Math.min(min, arr[i]);
+				} else {
+					int x = Math.abs(arr[i]);
+					countsNegative[x]++;
+					minNegative = Math.min(minNegative, x);
+				}
+			}
+			for (int i = Math.min(min, minNegative); i < counts.length; i++) {
+				if (counts[i] > 0) {
+					int other = i * 2;
+					if (counts[other] < counts[i]) return false;
+					counts[other] -= counts[i];
+				}
+				if (countsNegative[i] > 0) {
+					int otherNegative = i * 2;
+					if (countsNegative[otherNegative] < countsNegative[i]) return false;
+					countsNegative[otherNegative] -= countsNegative[i];
+				}
+			}
+			return true;
+		}
+	}
 }

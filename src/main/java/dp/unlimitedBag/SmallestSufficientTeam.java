812f291332438f7f5eae208440cd2768f703b557
Grigorii <gregsh9.5@gmail.com>
2021-11-16 10:53:19 +0300

    199

---
+++ src/main/java/dp/unlimitedBag/SmallestSufficientTeam.java
@@ -0,0 +1,117 @@
+package dp.unlimitedBag;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * $INSERT_PROBLEM_NUMBER $INSERT_LINK
+ */
+public class SmallestSufficientTeam {
+
+  /**
+   * max sum - smallest number of coins задача набрать мин людей на amount
+   * amount в данном случае это сет где все 1-цы нужен перебор от 0 выбранных до
+   * всех выбранных это amount coins это люди человек это скил
+   */
+  public static class Solution {
+
+    public int[] smallestSufficientTeam(String[] req_skills,
+        List<List<String>> people) {
+      // max sum - smallest number of coins
+      // задача набрать мин людей на amount
+      // amount в данном случае это сет где все 1-цы
+      // нужен перебор от 0 выбранных до всех выбранных
+      // это amount
+      // coins это люди
+      // человек это скил
+      int n = req_skills.length;
+      Map<String, Integer> skillsPos = new HashMap<>();
+      for (int i = 0; i < n; i++) {
+        skillsPos.put(req_skills[i], i);
+      }
+      int max = ((~0) >>> (32 - n));
+      // amount, number people
+      Map<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < people.size(); i++) {
+        List<String> l = people.get(i);
+        int x = l.size() == 0 ? 0 : getMaskForSkillSet(l, skillsPos);
+        map.put(i, x);
+      }
+      int p = people.size();
+      int[][] dp = new int[max + 1][p + 1];
+      for (int i = 1; i <= max; i++) {
+        dp[i][0] = Integer.MAX_VALUE;
+      }
+      for (int i = 1; i <= max; i++) {
+        for (int j = 1; j <= p; j++) {
+          if (people.get(j - 1).size() == 0) {
+            dp[i][j] = dp[i][j - 1];
+          } else {
+            int mask = map.get(j - 1);
+            int unset = unsetMask(i, mask);
+
+            if (contains(i, mask) && dp[unset][j] != Integer.MAX_VALUE) {
+              dp[i][j] = Math.min(dp[i][j - 1], dp[unset][j] + 1);
+            } else {
+              dp[i][j] = dp[i][j - 1];
+            }
+          }
+
+        }
+      }
+      // for (int i = 0; i <= max; i++) {
+      //     System.out.println();
+      //     for (int j = 0; j <= p; j++) {
+      //         if (dp[i][j] == Integer.MAX_VALUE) {
+      //             System.out.print("max, ");
+      //         } else System.out.print(dp[i][j]+", ");
+      //     }
+      // }
+      int totalSize = dp[max][p];
+      // trace
+      int i = max, j = p;
+      int[] ansAr = new int[totalSize];
+      int k = 0;
+      while (i >= 0 && j >= 0 && dp[i][j] != 0) {
+        if (people.get(j - 1).size() == 0) {
+          j--;
+        } else {
+          int mask = map.get(j - 1);
+          int unset = unsetMask(i, mask);
+          if (contains(i, mask) && dp[unset][j] != Integer.MAX_VALUE) {
+            if (dp[i][j - 1] < dp[unset][j] + 1) {
+              j--;
+            } else {
+              i = unset;
+              ansAr[k++] = j - 1;
+            }
+          } else {
+            j--;
+          }
+        }
+
+      }
+      return ansAr;
+    }
+
+    int unsetMask(int a, int mask) {
+      return a ^ mask;
+    }
+
+    boolean contains(int a, int b) {
+      return (b & a) == b;
+    }
+
+    int getMaskForSkillSet(List<String> skillSet,
+        Map<String, Integer> skillsPos) {
+      int res = 0;
+      for (String sk : skillSet) {
+        int pos = skillsPos.get(sk);
+        res |= (1 << pos);
+      }
+      return res;
+    }
+  }
+}
--- src/main/java/graph/shortest_paths/bfs/tree/BinaryTreeRightSideView.java
+++ src/main/java/graph/shortest_paths/bfs/tree/BinaryTreeRightSideView.java
@@ -2,6 +2,7 @@ package graph.shortest_paths.bfs.tree;
 
 import commons.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -10,33 +11,73 @@ import java.util.Queue;
  * 199
  */
 public class BinaryTreeRightSideView {
-	/**
-	 * Level order traversal but we insert to the list only
-	 * the first element of the level if it is not null.
-	 */
-	class Solution {
-		public List<Integer> rightSideView(TreeNode root) {
-			List<Integer> resultList = new LinkedList<>();
-			if (root == null) return resultList;
 
-			Queue<TreeNode> q = new LinkedList<>();
-			q.add(root);
+  /**
+   * Level order traversal but we insert to the list only the first element of
+   * the level if it is not null.
+   */
+  public static class Solution1 {
 
-			while (!q.isEmpty()) {
-				resultList.add(q.peek().val);
+    public List<Integer> rightSideView(TreeNode root) {
+      List<Integer> resultList = new LinkedList<>();
+      if (root == null) {
+        return resultList;
+      }
 
-				int size = q.size();
-				while (--size >= 0) {
-					TreeNode node = q.poll();
-					if (node == null) continue;
+      Queue<TreeNode> q = new LinkedList<>();
+      q.add(root);
 
-					if (node.right != null) q.add(node.right);
-					if (node.left != null) q.add(node.left);
-				}
-			}
+      while (!q.isEmpty()) {
+        resultList.add(q.peek().val);
 
-			return resultList;
-		}
-	}
+        int size = q.size();
+        while (--size >= 0) {
+          TreeNode node = q.poll();
+          if (node == null) {
+            continue;
+          }
+
+          if (node.right != null) {
+            q.add(node.right);
+          }
+          if (node.left != null) {
+            q.add(node.left);
+          }
+        }
+      }
+
+      return resultList;
+    }
+  }
+
+  /**
+   * @trick [bfs with dfs] this logic works because we go first to the right
+   * child it means these nodes that we will visit coming from the right child
+   * are more important, so they will be first to witness a condition when level
+   * == ans.size
+   *
+   * the other way of doing this would be adding answer at positions:
+   * map.put(level, root.val), then converting this to the list
+   */
+  public static class Solution2 {
+
+    public List<Integer> rightSideView(TreeNode root) {
+      List<Integer> ans = new ArrayList<>();
+      rightSideViewHelper(root, ans, true, 1);
+      return ans;
+    }
+
+    private void rightSideViewHelper(TreeNode root, List<Integer> ans,
+        boolean shouldAdd, int level) {
+      if (root == null) {
+        return;
+      }
+      if (shouldAdd && ans.size() + 1 == level) {
+        ans.add(root.val);
+      }
+      boolean hasRightChild = root.right != null;
+      rightSideViewHelper(root.right, ans, true, level + 1);
+      rightSideViewHelper(root.left, ans, !hasRightChild, level + 1);
+    }
+  }
 }
-

49d69962dbf6c7bf8378a299973eefa47910aaec
Grigorii <gregsh9.5@gmail.com>
2021-11-20 07:41:33 +0300

    489

--- src/main/java/graph/dfs/RobotRoomCleaner.java
+++ src/main/java/graph/dfs/RobotRoomCleaner.java
@@ -1,6 +1,5 @@
 package graph.dfs;
 
-import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,51 +7,60 @@ import java.util.Set;
  * 489
  */
 public class RobotRoomCleaner {
-	class Solution {
-		int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-		Set<Point> s;
 
-		public void cleanRoom(Robot r) {
-			s = new HashSet<>();
-			dfs(r, 0, 0, 0);
-		}
+  class Solution {
 
-		private void dfs(Robot r, int dir, int i, int j) {
-			if (!s.add(new Point(i, j))) return;
-			r.clean();
-			for (int k = 0; k < 4; k++) {
-				int actual = (dir + k) % 4;
-				if (r.move()) {
-					int ni = i + dirs[actual][0];
-					int nj = j + dirs[actual][1];
-					dfs(r, actual, ni, nj);
-					goBack(r);
-				}
-				r.turnRight();
-			}
-		}
+    // 0,1,2,3 - u,r,b,l - i,j
+    int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    Robot r;
 
-		void goBack(Robot r) {
-			r.turnRight();
-			r.turnRight();
-			r.move();
-			r.turnRight();
-			r.turnRight();
-		}
+    public void cleanRoom(Robot robot) {
+      r = robot;
+      Set<Long> seen = new HashSet<>();
+      dfs(0, 0, 0, seen);
+    }
 
-		private class Robot {
-			void clean() {
-			}
+    private void dfs(int curDir, int i, int j, Set<Long> seen) {
+      if (isSeen(i, j, seen)) return;
+      r.clean();
+      for (int k = curDir, c = 0; c < 4; k = (k + 1) % 4, c++) {
+        if (r.move()) {
+          int newI = dirs[k][0] + i;
+          int newJ = dirs[k][1] + j;
+          dfs(k, newI, newJ, seen);
+          moveBack();
+        }
+        r.turnRight();
+      }
+    }
 
-			private boolean move() {
-				return false;
-			}
+    private boolean isSeen(int i, int j, Set<Long> seen) {
+      long hash = i * 201L + j * 101L;
+      return !seen.add(hash);
+    }
 
-			private void turnRight() {
-			}
+    private void moveBack() {
+      r.turnLeft();
+      r.turnLeft();
+      r.move();
+      r.turnLeft();
+      r.turnLeft();
+    }
 
-			private void turnLeft() {
-			}
-		}
-	}
+    class Robot {
+
+      void clean() {
+      }
+
+      private boolean move() {
+        return false;
+      }
+
+      private void turnRight() {
+      }
+
+      private void turnLeft() {
+      }
+    }
+  }
 }

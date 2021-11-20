fed109f33797634e5168951148fa9dedd1f2af42
Grigorii <gregsh9.5@gmail.com>
2021-11-20 19:39:34 +0300

    1209

--- src/main/java/stack/RemoveAllAdjacentDuplicatesInStringII.java
+++ src/main/java/stack/RemoveAllAdjacentDuplicatesInStringII.java
@@ -57,4 +57,28 @@ public class RemoveAllAdjacentDuplicatesInStringII {
       }
     }
   }
+
+  /**
+   * Solution with replacements (2 pointers) - we move second pointer value
+   * closer to i, so we could compare values - to see if we have dups.
+   */
+  public static class Solution2 {
+
+    public String removeDuplicates(String s, int k) {
+      int i = 0, n = s.length();
+      int count[] = new int[n];
+      char[] stack = s.toCharArray();
+      for (int j = 0; j < n; ++j, ++i) {
+        stack[i] = stack[j];
+        count[i] = 1;
+        if (i > 0 && stack[i - 1] == stack[j]) {
+          count[i] = count[i - 1] + 1;
+        }
+        if (count[i] == k) {
+          i -= k;
+        }
+      }
+      return new String(stack, 0, i);
+    }
+  }
 }

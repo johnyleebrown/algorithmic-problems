8e6a7b280d86d50f5740f8dc9093a56e3ad07b62
Grigorii <gregsh9.5@gmail.com>
2021-12-07 17:47:23 +0300

    173

--- src/main/java/design/iterator/BinarySearchTreeIterator.java
+++ src/main/java/design/iterator/BinarySearchTreeIterator.java
@@ -9,114 +9,117 @@ import java.util.Deque;
  * 173
  */
 public class BinarySearchTreeIterator {
-    public static class Solution {
-        class BSTIterator {
-            Deque<TreeNode> st;
 
-            public BSTIterator(TreeNode root) {
-                st = new ArrayDeque<>();
-                addLefts(root);
-            }
+  public static class Solution {
 
-            private void addLefts(TreeNode r) {
-                while (r != null) {
-                    st.addLast(r);
-                    r = r.left;
-                }
-            }
+    class BSTIterator {
 
-            public int next() {
-                TreeNode cur = st.removeLast();
-                addLefts(cur.right);
-                return cur.val;
-            }
+      Deque<TreeNode> st;
 
-            public boolean hasNext() {
-                return !st.isEmpty();
-            }
+      public BSTIterator(TreeNode root) {
+        st = new ArrayDeque<>();
+        addLefts(root);
+      }
+
+      private void addLefts(TreeNode r) {
+        while (r != null) {
+          st.addLast(r);
+          r = r.left;
         }
+      }
+
+      public int next() {
+        TreeNode cur = st.removeLast();
+        addLefts(cur.right);
+        return cur.val;
+      }
+
+      public boolean hasNext() {
+        return !st.isEmpty();
+      }
     }
+  }
 
-    /**
-     * Use prev and next links, otherwise use stack to get next items like in
-     * prev solution.
-     *
-     * First 3 lines:
-     * TreeNodeCustom next = cur.next;
-     * cur = next;
-     * return next.val;
-     *
-     * Then expand it.
-     */
-    public static class Solution2 {
-        public static class BSTIterator {
-            private TreeNodeCustom cur;
-            private Deque<TreeNodeCustom> st;
-            private TreeNodeCustom root;
+  /**
+   * Use prev and next links, otherwise use stack to get next items like in prev
+   * solution.
+   *
+   * First 3 lines: TreeNodeCustom next = cur.next; cur = next; return next.val;
+   *
+   * Then expand it.
+   */
+  public static class Solution2 {
 
-            public BSTIterator(TreeNodeCustom root) {
-                st = new ArrayDeque<>();
-                this.root = root;
-            }
+    public static class BSTIterator {
 
-            public int next() {
-                if (cur == null) {
-                    addLefts(root);
-                    TreeNodeCustom next = st.removeLast();
-                    cur = next;
-                    return next.val;
-                }
+      private TreeNodeCustom cur;
+      private Deque<TreeNodeCustom> st;
+      private TreeNodeCustom root;
 
-                if (cur.next == null) {
-                    addLefts(cur.right);
-                    cur.next = st.removeLast();
-                }
+      public BSTIterator(TreeNodeCustom root) {
+        st = new ArrayDeque<>();
+        this.root = root;
+      }
 
-                TreeNodeCustom next = cur.next;
-                next.prev = cur;
-                cur = next;
-                return next.val;
-            }
-
-            private void addLefts(TreeNodeCustom node) {
-                while (node != null) {
-                    st.add(node);
-                    node = node.left;
-                }
-            }
-
-            public boolean hasNext() {
-                if (cur == null) {
-                    return root != null;
-                }
-                if (cur.next == null) {
-                    return !st.isEmpty() || cur.right != null;
-                }
-                return true;
-            }
-
-            public int previous() {
-                TreeNodeCustom prev = cur.prev;
-                cur = prev;
-                return prev.val;
-            }
-
-            public boolean hasPrevious() {
-                return cur.prev != null;
-            }
+      public int next() {
+        if (cur == null) {
+          addLefts(root);
+          TreeNodeCustom next = st.removeLast();
+          cur = next;
+          return next.val;
         }
-    }
 
-    /**
-     * Regular TreeNode with a link to prev and next.
-     */
-    public static class TreeNodeCustom {
-        TreeNodeCustom prev, next;
-        TreeNodeCustom left, right;
-        int val;
-
-        public TreeNodeCustom(int val) {
-            this.val = val;
+        if (cur.next == null) {
+          addLefts(cur.right);
+          cur.next = st.removeLast();
         }
+
+        TreeNodeCustom next = cur.next;
+        next.prev = cur;
+        cur = next;
+        return next.val;
+      }
+
+      private void addLefts(TreeNodeCustom node) {
+        while (node != null) {
+          st.add(node);
+          node = node.left;
+        }
+      }
+
+      public boolean hasNext() {
+        if (cur == null) {
+          return root != null;
+        }
+        if (cur.next == null) {
+          return !st.isEmpty() || cur.right != null;
+        }
+        return true;
+      }
+
+      public int previous() {
+        TreeNodeCustom prev = cur.prev;
+        cur = prev;
+        return prev.val;
+      }
+
+      public boolean hasPrevious() {
+        return cur.prev != null;
+      }
     }
+  }
+
+  /**
+   * Regular TreeNode with a link to prev and next.
+   */
+  public static class TreeNodeCustom {
+
+    TreeNodeCustom prev, next;
+    TreeNodeCustom left, right;
+    int val;
+
+    public TreeNodeCustom(int val) {
+      this.val = val;
+    }
+  }
 }

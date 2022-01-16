942f24bc77a4c0aee4b2784105d4a042a7465ac1
Grigorii <gregsh9.5@gmail.com>
2022-01-16 08:02:34 +0300

    2140

--- src/main/java/dp/subset/SolvingQuestionsWithBrainpower.java
+++ src/main/java/dp/subset/SolvingQuestionsWithBrainpower.java
@@ -13,14 +13,20 @@ public class SolvingQuestionsWithBrainpower {
 		long[] map;
 
 		public long mostPoints(int[][] q) {
-			map = new long[q.length + 1]; return dfs(0, q);
+			map = new long[q.length + 1];
+			return dfs(0, q);
 		}
 
 		long dfs(int i, int[][] q) {
-			if (i >= q.length) return 0; if (map[i] != 0) {
+			if (i >= q.length) return 0;
+			if (map[i] != 0) {
 				return map[i];
-			} long res = 0; res = Math.max(res, q[i][0] + dfs(i + q[i][1] + 1, q));
-			res = Math.max(res, dfs(i + 1, q)); map[i] = res; return res;
+			}
+			long res = 0;
+			res = Math.max(res, q[i][0] + dfs(i + q[i][1] + 1, q));
+			res = Math.max(res, dfs(i + 1, q));
+			map[i] = res;
+			return res;
 		}
 	}
 }

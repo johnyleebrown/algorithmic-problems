0d362a26bad2d75f479071dd8e7b67034fc78229
Grigorii <gregsh9.5@gmail.com>
2022-01-25 14:52:40 +0300

    973

--- src/main/java/sort/heap/KClosestPointsToOrigin.java
+++ src/main/java/sort/heap/KClosestPointsToOrigin.java
@@ -8,62 +8,85 @@ public class KClosestPointsToOrigin {
   /**
    * Heap O(n*logk)
    */
-  public class Solution2 {
+  public static class Solution {
 
-    public int[][] kClosest(int[][] points, int K) {
-      int n = points.length;
-      int[][] x = new int[K][2];
+    public int[][] kClosest(int[][] points, int k) {
+      // use max heap
+      // [1] the biggest of smallest
+      MaxHeap h = new MaxHeap(k);
+      for (int[] p : points) {
+        if (h.n == k) {
+          h.replaceMax(p);
+        } else {
+          h.add(p);
+        }
+      }
+      return h.getSnapshot();
+    }
 
-      for (int i = 0; i < K; i++) {
-        x[i] = points[i];
-        swim(x, i);
+    static class MaxHeap {
+
+      int cap, n = 0;
+      int[][] h;
+
+      MaxHeap(int cap) {
+        this.cap = cap;
+        h = new int[cap + 1][];
       }
 
-      for (int i = K; i < points.length; i++) {
-        if (greater(x[0], points[i])) {
-          x[0] = points[i];
-          sink(x, 0, K);
+      void add(int[] p) {
+        h[++n] = p;
+        swim(n);
+      }
+
+      void replaceMax(int[] p) {
+        if (!less(p, h[1])) return;
+        h[1] = p;
+        sink(1);
+      }
+
+      void swim(int i) {
+        while (i > 1 && less(h[i / 2], h[i])) {
+          exch(i, i / 2);
+          i = i / 2;
         }
       }
 
-      return x;
-    }
-
-    // min heap - using greater instead of less
-    private void sink(int[][] a, int k, int n) {
-      while (2 * k < n) {
-        int j = 2 * k;
-
-        if (j + 1 < n && greater(a[j + 1], a[j])) {
-          j++;
+      void sink(int i) {
+        while (i * 2 <= n) {
+          int j = i * 2;
+          if (j + 1 <= n && less(h[j], h[j + 1])) j++;
+          if (!less(h[i], h[j])) break;
+          exch(i, j);
+          i = j;
         }
+      }
 
-        if (!greater(a[j], a[k])) {
-          break;
+      boolean less(int[] p1, int[] p2) {
+        return dist(p1) < dist(p2);
+      }
+
+      boolean larger(int[] p1, int[] p2) {
+        return dist(p1) > dist(p2);
+      }
+
+      int dist(int[] p) {
+        return p[0] * p[0] + p[1] * p[1];
+      }
+
+      void exch(int i, int j) {
+        int[] t = h[i];
+        h[i] = h[j];
+        h[j] = t;
+      }
+
+      int[][] getSnapshot() {
+        int[][] ans = new int[cap][];
+        for (int i = 0; i < cap; i++) {
+          ans[i] = h[i + 1];
         }
-
-        exch(a, k, j);
-
-        k = j;
+        return ans;
       }
-    }
-
-    // moving element up, towards the start of the array
-    private void swim(int[][] a, int k) {
-      while (k > 0 && greater(a[k], a[k / 2])) {
-        exch(a, k, k / 2);
-        k /= 2;
-      }
-    }
-
-    private boolean greater(int[] a, int[] b) {
-      return a[0] * a[0] + a[1] * a[1] > b[0] * b[0] + b[1] * b[1];
-    }
-
-    private void exch(int[][] a, int i, int j) {
-      int[] temp = a[i];
-      a[i] = a[j];
-      a[j] = temp;
     }
   }
 }

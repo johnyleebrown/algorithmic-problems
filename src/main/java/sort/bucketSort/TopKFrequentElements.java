d36334f48d6663ba749533ca00475e1ed80224b9
Grigorii <gregsh9.5@gmail.com>
2022-02-01 19:55:18 +0300

    347

--- src/main/java/sort/bucketSort/TopKFrequentElements.java
+++ src/main/java/sort/bucketSort/TopKFrequentElements.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * 347
  */
 public class TopKFrequentElements {
+
 	class Solution {
+
 		public List<Integer> topKFrequent(int[] a, int k) {
 			Map<Integer, Integer> count = new HashMap<>();
 			for (int i : a) {
@@ -38,6 +41,46 @@ public class TopKFrequentElements {
 			}
 
 			return ans;
+		}
+	}
+
+	class Solution2 {
+
+		public int[] topKFrequent(int[] nums, int k) {
+			PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
+				return a.count - b.count;
+			});
+			Map<Integer, Node> m = new HashMap<>();
+			for (int i : nums) {
+				m.putIfAbsent(i, new Node(1, i));
+				Node x = m.get(i);
+				pq.remove(x);
+				x.incCount();
+				pq.add(x);
+				if (pq.size() > k) {
+					pq.poll();
+				}
+			}
+			int[] ans = new int[k];
+			int i = 0;
+			for (Node n : pq) {
+				ans[i++] = n.num;
+			}
+			return ans;
+		}
+
+		class Node {
+
+			int count, num;
+
+			Node(int c, int num) {
+				this.count = c;
+				this.num = num;
+			}
+
+			void incCount() {
+				this.count++;
+			}
 		}
 	}
 }

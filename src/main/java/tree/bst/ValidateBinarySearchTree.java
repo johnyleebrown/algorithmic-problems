bba3bdd00a52e299469e21db282cac908a9f4f3f
Grigorii <gregsh9.5@gmail.com>
2022-02-08 20:22:36 +0300

    98

--- src/main/java/tree/bst/ValidateBinarySearchTree.java
+++ src/main/java/tree/bst/ValidateBinarySearchTree.java
@@ -5,22 +5,50 @@ import commons.TreeNode;
 /**
  * 98
  */
-public class ValidateBinarySearchTree
-{
-	private static class Solution
-	{
-		public boolean isValidBST(TreeNode cur)
-		{
-			return f(cur, null, null);
+public class ValidateBinarySearchTree {
+
+	/**
+	 * Top down
+	 */
+	private static class Solution {
+
+		public boolean isValidBST(TreeNode cur) {
+			return dfs(cur, null, null);
 		}
 
-		private boolean f(TreeNode cur, Integer min, Integer max)
-		{
-			if (cur == null)
-				return true;
-			if (max != null && cur.val >= max || min != null && cur.val <= min)
-				return false;
-			return f(cur.left, min, cur.val) && f(cur.right, cur.val, max);
+		private boolean dfs(TreeNode cur, Integer min, Integer max) {
+			if (cur == null) return true;
+			if (max != null && cur.val >= max || min != null && cur.val <= min) return false;
+			return dfs(cur.left, min, cur.val) && dfs(cur.right, cur.val, max);
+		}
+	}
+
+	/**
+	 * Bottom up
+	 */
+	public static class Solution2 {
+
+		public boolean isValidBST(TreeNode root) {
+			Long[] ans = isValidBSTHelper(root);
+			return ans[0] != null;
+		}
+
+		Long[] isValidBSTHelper(TreeNode root) {
+			if (root == null) {
+				return new Long[]{Long.MAX_VALUE, Long.MIN_VALUE};
+			}
+			Long[] left = isValidBSTHelper(root.left);
+			Long[] right = isValidBSTHelper(root.right);
+			// in subtree is not valid
+			if (left[0] == null || right[0] == null) {
+				return new Long[]{null, null};
+			}
+			// root is not valid
+			if (root.val <= left[1] || root.val >= right[0]) {
+				return new Long[]{null, null};
+			}
+			return new Long[]{Math.min(left[0], root.val),
+					Math.max(right[1], root.val)};
 		}
 	}
 }
--- src/main/java/tree/validate/ValidateBinaryTreeNodes.java
+++ src/main/java/tree/validate/ValidateBinaryTreeNodes.java
@@ -2,51 +2,36 @@ package tree.validate;
 
 /**
  * 1361
- *
- * ======
- *
- * Task.
- *
- * You have n binary tree nodes numbered from 0 to n - 1 where node i has two
- * children leftChild[i] and rightChild[i], return true if and only if all the
- * given nodes form exactly one valid binary tree.
- *
- * If node i has no left child then leftChild[i] will equal -1, similarly for
- * the right child.
- *
- * Note that the nodes have no values and that we only use the node numbers in
- * this problem.
- *
- * ======
- *
- * Source: Leetcode
  */
 public class ValidateBinaryTreeNodes {
-    /**
-     * SF. Tree traversal.
-     */
-    public static class Solution {
-        int n;
 
-        public boolean validateBinaryTreeNodes(int n, int[] l, int[] r) {
-            this.n = n;
-            int i = 0;
-            for (; i < n; i++)
-                if (l[i] == 0 || r[i] == 0)
-                    break;
-            return dfs(i == n ? 0 : i, l, r, new boolean[n]) && this.n == 0;
-        }
+	/**
+	 * SF. Tree traversal.
+	 */
+	public static class Solution {
 
-        private boolean dfs(int cur, int[] l, int[] r, boolean[] seen) {
-            if (cur == -1)
-                return true;
-            if (seen[cur])
-                return false;
-            n--;
-            seen[cur] = true;
-            boolean left = dfs(l[cur], l, r, seen);
-            boolean right = dfs(r[cur], l, r, seen);
-            return left && right;
-        }
-    }
+		int n;
+
+		public boolean validateBinaryTreeNodes(int n, int[] l, int[] r) {
+			this.n = n;
+			int i = 0;
+			for (; i < n; i++) {
+				if (l[i] == 0 || r[i] == 0)
+					break;
+			}
+			return dfs(i == n ? 0 : i, l, r, new boolean[n]) && this.n == 0;
+		}
+
+		private boolean dfs(int cur, int[] l, int[] r, boolean[] seen) {
+			if (cur == -1)
+				return true;
+			if (seen[cur])
+				return false;
+			n--;
+			seen[cur] = true;
+			boolean left = dfs(l[cur], l, r, seen);
+			boolean right = dfs(r[cur], l, r, seen);
+			return left && right;
+		}
+	}
 }
